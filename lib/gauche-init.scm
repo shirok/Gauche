a67fc987f9ad77337b677e8cba4196e58f88e545
Shiro Kawai <shiro@acm.org>
2001-02-06 08:33:07 +0000

    system default script
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@516 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ lib/gauche-init.scm
@@ -0,0 +1,38 @@
+;;;
+;;; gauche-init.scm - initialize standard environment
+;;;
+;;;  Copyright(C) 2000-2001 by Shiro Kawai (shiro@acm.org)
+;;;
+;;;  Permission to use, copy, modify, ditribute this software and
+;;;  accompanying documentation for any purpose is hereby granted,
+;;;  provided that existing copyright notices are retained in all
+;;;  copies and that this notice is included verbatim in all
+;;;  distributions.
+;;;  This software is provided as is, without express or implied
+;;;  warranty.  In no circumstances the author(s) shall be liable
+;;;  for any damages arising out of the use of this software.
+;;;
+;;;  $Id: gauche-init.scm,v 1.1 2001-02-06 08:33:07 shiro Exp $
+;;;
+
+;;
+;;
+;;
+
+;;
+;; Macro
+;;
+
+(define define-macro
+  (%make-macro-transformer
+   'define-macro
+   (lambda (_ spec . body)
+     (unless (and (pair? spec)
+                  (symbol? (car spec))
+                  (not (null? (body))))
+       (error "badly formed define-macro: ~s" (list* _ spec body)))
+     `(define ,(car spec)
+        (%make-macro-transformer
+         ',(car spec)
+         (lambda ,spec ,@body))))))
+
