f6d8faa4c60dc15c078b5953275af11057c28d70
Shiro Kawai <shiro@acm.org>
2001-02-08 13:22:12 +0000

    commented out unused insns (they interfere with teh use of local label address in vm.c)
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@534 c205566b-fb4e-0410-ab5c-8f0660156277

--- src/gauche/vminsn.h
+++ src/gauche/vminsn.h
@@ -12,7 +12,7 @@
  *  warranty.  In no circumstances the author(s) shall be liable
  *  for any damages arising out of the use of this software.
  *
- *  $Id: vminsn.h,v 1.10 2001-02-06 12:04:55 shiro Exp $
+ *  $Id: vminsn.h,v 1.11 2001-02-08 13:22:12 shiro Exp $
  */
 
 /* DEFINSN(symbol, name, # of parameters) */
@@ -187,12 +187,11 @@ DEFINSN(SCM_VM_MEMV, "MEMV", 0)
 DEFINSN(SCM_VM_EQ, "EQ?", 0)
 DEFINSN(SCM_VM_EQV, "EQV?", 0)
 DEFINSN(SCM_VM_APPEND, "APPEND", 1)
-DEFINSN(SCM_VM_NCONC, "NCONC", 1)
 DEFINSN(SCM_VM_NOT, "NOT", 0)
 DEFINSN(SCM_VM_NULLP, "NULL?", 0)
-DEFINSN(SCM_VM_NOT_NULLP, "NOT-NULL?", 0)
-DEFINSN(SCM_VM_FOR_EACH, "FOR-EACH", 1)
-DEFINSN(SCM_VM_MAP, "MAP", 1)
+/*DEFINSN(SCM_VM_NOT_NULLP, "NOT-NULL?", 0)*/
+/*DEFINSN(SCM_VM_FOR_EACH, "FOR-EACH", 1)*/
+/*DEFINSN(SCM_VM_MAP, "MAP", 1)*/
 
 DEFINSN(SCM_VM_VEC, "VEC", 1)
 DEFINSN(SCM_VM_APP_VEC, "APP-VEC", 1)
