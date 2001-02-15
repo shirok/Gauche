a65cf7118c5b8d50cd7658aecffd7f3b932cf0a6
Shiro Kawai <shiro@acm.org>
2001-02-15 08:57:32 +0000

    added callcc-test1
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@587 c205566b-fb4e-0410-ab5c-8f0660156277

--- test/dynwind.scm
+++ test/dynwind.scm
@@ -1,6 +1,7 @@
 
 (define c #f)
 
+;; An example in R5RS
 (define (dynwind-test1)
   (let ((path '()))
     (let ((add (lambda (s) (set! path (cons s path)))))
@@ -14,9 +15,22 @@
           (c 'talk2)
           (reverse path)))))
 
+;; Test for handler stack.  "***1" and "***3" should be displayed only once.
 (define (dynwind-test2)
   (dynamic-wind
    (lambda () (display "*** 1\n"))
    dynwind-test1
    (lambda () (display "*** 3\n"))))
 
+;; Test for continuation with the saved environment is altered.
+;; should print 2^1 to 2^10
+(define (callcc-test1)
+  (let ((w (let ((v 1))
+             (set! v (+ (call-with-current-continuation
+                         (lambda (c0) (set! c c0) v))
+                        v))
+             (display v) (newline)
+             v)))
+    (if (< w 1024) (c w))))
+
+             
