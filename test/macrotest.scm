cfd293807d3a74cf82fa73c882e4b43933bb9947
Shiro Kawai <shiro@acm.org>
2001-03-01 20:04:29 +0000

    adapted to test.scm
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@701 c205566b-fb4e-0410-ab5c-8f0660156277

--- test/macrotest.scm
+++ test/macrotest.scm
@@ -1,7 +1,23 @@
 ;;
-;; derived expression types, from R5RS section 7.3
+;; testing macro expansion
 ;;
 
+(add-load-path "../lib")
+(require "test")
+
+;; strip off syntactic information from identifiers in the macro output.
+(define (unident form)
+  (cond
+   ((identifier? form) (identifier->symbol form))
+   ((pair? form) (cons (unident (car form)) (unident (cdr form))))
+   ((vector? form)
+    (list->vector (map unident (vector->list form))))
+   (else form)))
+
+(define-macro (test-macro msg expect form)
+  `(test ,msg ',expect (lambda () (unident (%macro-expand ,form)))))
+
+;; taken from R5RS section 7.3
 (define-syntax %cond
   (syntax-rules (else =>)
     ((cond (else result1 result2 ...))
@@ -24,4 +40,46 @@
      (if test (begin result1 result2 ...) (%cond clause1 clause2 ...)))
     ))
 
-           
+(define-syntax %letrec
+  (syntax-rules ()
+    ((_ ((var1 init1) ...) body ...)
+     (%letrec "generate_temp_names"
+              (var1 ...)
+              ()
+              ((var1 init1) ...)
+              body ...))
+    ((_ "generate_temp_names" () (temp1 ...) ((var1 init1) ...) body ...)
+     (let ((var1 <undefined>) ...)
+       (let ((temp1 init1) ...)
+         (set! var1 temp1) ...
+         body ...)))
+    ((_ "generate_temp_names" (x y ...) (temp ...) ((var1 init1) ...) body ...)
+     (%letrec "generate_temp_names"
+              (y ...)
+              (newtemp temp ...)
+              ((var1 init1) ...)
+              body ...))))
+
+;; test code
+
+
+(test-macro "%cond" (begin a) (%cond (else a)))
+(test-macro "%cond" (begin a b c) (%cond (else a b c)))
+(test-macro "%cond" (let ((temp a)) (if temp (b temp))) (%cond (a => b)))
+(test-macro "%cond" (let ((temp a)) (if temp (b temp) (%cond c))) (%cond (a => b) c))
+(test-macro "%cond" (let ((temp a)) (if temp (b temp) (%cond c d))) (%cond (a => b) c d))
+(test-macro "%cond" (let ((temp a)) (if temp (b temp) (%cond c d e))) (%cond (a => b) c d e))
+(test-macro "%cond" a (%cond (a)))
+(test-macro "%cond" (let ((temp a)) (if temp temp (%cond b))) (%cond (a) b))
+(test-macro "%cond" (let ((temp a)) (if temp temp (%cond b c))) (%cond (a) b c))
+(test-macro "%cond" (if a (begin b)) (%cond (a b)))
+(test-macro "%cond" (if a (begin b c d)) (%cond (a b c d)))
+(test-macro "%cond" (if a (begin b c d) (%cond e f g)) (%cond (a b c d) e f g))
+
+;; test for higiene
+(test "%cond" '(if a (begin => b))
+      (lambda () (let ((=> #f)) (unident (%macro-expand (%cond (a => b)))))))
+(test "%cond" '(if else (begin z))
+      (lambda () (let ((else #t)) (unident (%macro-expand (%cond (else z)))))))
+
+(newline)
