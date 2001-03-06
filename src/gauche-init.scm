0214411c63ae7c59bd7b2f118c61c87b6fd17c94
Shiro Kawai <shiro@acm.org>
2001-03-06 08:54:30 +0000

    added select-module stmt macro stuff moved to macro.c
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@765 c205566b-fb4e-0410-ab5c-8f0660156277

--- src/gauche-init.scm
+++ src/gauche-init.scm
@@ -12,32 +12,10 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: gauche-init.scm,v 1.10 2001-02-28 23:49:49 shiro Exp $
+;;;  $Id: gauche-init.scm,v 1.11 2001-03-06 08:54:30 shiro Exp $
 ;;;
 
-;;
-;; Macro
-;;
-
-(define DEFINE-MACRO
-  (%make-macro-transformer
-   'define-macro
-   (lambda (_ spec . body)
-     (unless (and (pair? spec)
-                  (symbol? (car spec))
-                  (not (null? body)))
-       (error "badly formed define-macro: ~s" (list* _ spec body)))
-     `(define ,(car spec)
-        (%make-macro-transformer
-         ',(car spec)
-         (lambda ,spec ,@body))))))
-
-(define-macro (DEFINE-SYNTAX name synrule)
-  (unless (and (pair? synrule) (eq? (car synrule) 'syntax-rules))
-    (error "syntax specification required, but got: ~s" synrule))
-  (unless (symbol? name)
-    (error "symbol required as the macro name, but got: ~s" name))
-  `(define ,name (%syntax-rules ,name ,@(cdr synrule))))
+(select-module gauche)
 
 ;;
 ;; Some useful aliases
@@ -45,7 +23,10 @@
 
 (define CALL/CC call-with-current-continuation)
 
-;; will be implemented in C later.
+;;
+;; Auxiliary stuff for R5RS
+;;
+
 (define (CALL-WITH-VALUES producer consumer)
   (receive vals (producer) (apply consumer vals)))
 
@@ -76,3 +57,6 @@
 (define-macro (AUTOLOAD file . vars)
   (cons 'begin (map (lambda (v) `(define ,v (%make-autoload ',v ,file)))
                     vars)))
+
+
+
