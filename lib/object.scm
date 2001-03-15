cd28d6593c4c91a1bfc336c5bf1045580ef41a5d
Shiro Kawai <shiro@acm.org>
2001-03-15 09:46:00 +0000

    added.  nothing in it yet.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@898 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ lib/object.scm
@@ -0,0 +1,23 @@
+;;;
+;;; object.scm - object system
+;;;
+;;;  Copyright(C) 2000-2001 by Shiro Kawai (shiro@acm.org)
+;;;
+;;;  Permission to use, copy, modify, distribute this software and
+;;;  accompanying documentation for any purpose is hereby granted,
+;;;  provided that existing copyright notices are retained in all
+;;;  copies and that this notice is included verbatim in all
+;;;  distributions.
+;;;  This software is provided as is, without express or implied
+;;;  warranty.  In no circumstances the author(s) shall be liable
+;;;  for any damages arising out of the use of this software.
+;;;
+;;;  $Id: object.scm,v 1.1 2001-03-15 09:46:00 shiro Exp $
+;;;
+
+(select-module gauche)
+
+;;;
+;;; To be implemented ...
+;;;
+
