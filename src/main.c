984eac6f06c95771a297ffa8a6768712bdc9d6a6
Shiro Kawai <shiro@acm.org>
2001-03-15 06:47:21 +0000

    expose argv[0] as *program-name*.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@895 c205566b-fb4e-0410-ab5c-8f0660156277

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
  *  warranty.  In no circumstances the author(s) shall be liable
  *  for any damages arising out of the use of this software.
  *
- *  $Id: main.c,v 1.14 2001-03-11 09:45:13 shiro Exp $
+ *  $Id: main.c,v 1.15 2001-03-15 06:47:21 shiro Exp $
  */
 
 #include <unistd.h>
@@ -34,6 +34,8 @@ int main(int argc, char **argv)
         }
     }
     Scm_Init(load_initfile ? "gauche-init.scm" : NULL);
+    SCM_DEFINE(Scm_UserModule(), "*program-name*",
+               Scm_MakeString(argv[0], -1, -1));
 
     if (debug) Scm_VM()->debugCompile = TRUE;
     if (!enable_inline) Scm_VM()->enableInline = FALSE;
@@ -47,6 +49,8 @@ int main(int argc, char **argv)
         SCM_DEFINE(Scm_UserModule(), "*argv*", av);
         Scm_Load(argv[optind]);
         exit(0);
+    } else {
+        SCM_DEFINE(Scm_UserModule(), "*argv*", SCM_NIL);
     }
 
     Scm_Repl(SCM_MAKE_STR("gosh> "),
