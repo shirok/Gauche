2303233fc1f381702bf17b3f5a36efae5afe1426
Shiro Kawai <shiro@acm.org>
2001-03-16 07:11:06 +0000

    fixed a bug that didn't exit loop when EOF read.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@909 c205566b-fb4e-0410-ab5c-8f0660156277

--- src/repl.c
+++ src/repl.c
@@ -12,7 +12,7 @@
  *  warranty.  In no circumstances the author(s) shall be liable
  *  for any damages arising out of the use of this software.
  *
- *  $Id: repl.c,v 1.7 2001-02-19 14:48:49 shiro Exp $
+ *  $Id: repl.c,v 1.8 2001-03-16 07:11:06 shiro Exp $
  */
 
 #include "gauche.h"
@@ -20,6 +20,7 @@
 void Scm_Repl(ScmObj prompt, ScmPort *in, ScmPort *out)
 {
     ScmObj c, v;
+    int eofread = FALSE;
     
     for (;;) {
         ScmObj p = Scm_Eval(prompt, SCM_NIL);
@@ -27,13 +28,14 @@ void Scm_Repl(ScmObj prompt, ScmPort *in, ScmPort *out)
             Scm_Write(p, SCM_OBJ(out), SCM_PRINT_DISPLAY);
             SCM_FLUSH(out);
             v = Scm_Read(SCM_OBJ(in));
-            if (SCM_EOFP(v)) break;
+            if (SCM_EOFP(v)) { eofread = TRUE; break; }
             Scm_Eval(v, SCM_NIL);
             SCM_FOR_EACH(c, Scm_VMGetResult(Scm_VM())) {
                 Scm_Printf(out, "%S\n", SCM_CAR(c));
             }
         }
         SCM_POP_ERROR_HANDLER;
+        if (eofread) return;
     }
 }
 
