8cf7525e56531d6838b71852814e63c38390117d
Shiro Kawai <shiro@acm.org>
2001-03-20 09:58:05 +0000

    added a couple of macros experimentally.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@943 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/object.scm
+++ lib/object.scm
@@ -12,7 +12,7 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: object.scm,v 1.3 2001-03-17 09:22:37 shiro Exp $
+;;;  $Id: object.scm,v 1.4 2001-03-20 09:58:05 shiro Exp $
 ;;;
 
 (select-module gauche)
@@ -20,6 +20,40 @@
 ;;;
 ;;; This module is A WORK IN PROGRESS.   Don't expect this to work.
 ;;;
+
+(define-syntax define-class
+  (syntax-rules ()
+    ((_ ?name ?supers (?slot ...) ?options ...)
+     (define ?name
+       (make <class>
+             :supers ?supers
+             :slots (list (%parse-slot-definition ?slot) ...)
+             ?options ...
+             :name '?name)))))
+
+(define-syntax %parse-slot-definition
+  (syntax-rules ()
+    ((_ (?name ?options ...))
+     (%parse-slot-definition "parse options" ?name (?options ...) ()))
+    ((_ "parse options" ?name () (?rest ...))
+     (?name ?rest ...))
+    ((_ "parse options" ?name (:initform ?form ?options ...) (?rest ...))
+     (%parse-slot-definition "parse options"
+                             ?name (?options ...)
+                             (?rest ... :init-thunk (lambda () ?form))))
+    ((_ "parse options" ?name (:init-form ?form ?options ...) (?rest ...))
+     (%parse-slot-definition "parse options"
+                             ?name (?options ...)
+                             (?rest ... :init-thunk (lambda () ?form))))
+    ((_ "parse options" ?name (?key ?value ?options ...) (?rest ...))
+     (%parse-slot-definition "parse options"
+                             ?name (?options ...)
+                             (?rest ... ?key ?value)))
+    ((_ "parse options" ?name (?stray) (?rest ...))
+     (error "bad slot definition (uneven keyword list) for slot ~s" '?name))
+    ((_ ?name)
+     '?name)
+    ))
 
 (define (class-name class) (slot-ref class 'name))
 (define (class-precedence-list class) (slot-ref class 'cpl))
