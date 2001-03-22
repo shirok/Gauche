04d31f83bb6c78a2aa9ab0aaf67106150b40c4d9
Shiro Kawai <shiro@acm.org>
2001-03-22 10:38:10 +0000

    finish define-generic and define-method.   still working on define-class.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@956 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/object.scm
+++ lib/object.scm
@@ -12,7 +12,7 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: object.scm,v 1.5 2001-03-21 19:31:50 shiro Exp $
+;;;  $Id: object.scm,v 1.6 2001-03-22 10:38:10 shiro Exp $
 ;;;
 
 (select-module gauche)
@@ -21,20 +21,102 @@
 ;;; This module is A WORK IN PROGRESS.   Don't expect this to work.
 ;;;
 
-;;; Bootstrapping "make"
+;; Bootstrapping "make"
+;;   We already have generic-function for "make" defined in C.  Just wanted
+;;   to make a method specialized for <class>, i.e. the most common "make".
+;;   However, we can't say (make <method> ...) before we have make method.
+;;   So we have to "hard wire" the method creation.
 
-(define (%make class . initargs)
-  (initialize (allocate-instance class initargs) initargs))
+(let ((%make (lambda (class . initargs)
+               (let ((obj (allocate-instance class initargs)))
+                 (initialize obj initargs)
+                 obj)))
+      (body  (lambda (class initargs next-method)
+               (let ((obj (allocate-instance class initargs)))
+                 (initialize obj initargs)
+                 obj))))
+  (add-method! make
+               (%make <method>
+                      :generic make
+                      :specializers (list <class>)
+                      :lambda-list '(class . initargs)
+                      :body body)))
 
 
+;;----------------------------------------------------------------
+;; Generic function
+;;
 
+(define-syntax define-generic
+  (syntax-rules ()
+    ((_ ?name)
+     (define ?name (make <generic> :name '?name)))))
+
+;;----------------------------------------------------------------
+;; Method
+;;
+
+(define-syntax define-method
+  (syntax-rules ()
+    ;; main expansion
+    ((_ "args" ?name () (?body ...) (?specs ...) ?lambda-list (?body-arg ...))
+     (begin
+       (define ?name
+         (if (symbol-bound? '?name)
+             (if (is-a? ?name <generic>)
+                 ?name
+                 (error "define-method: ~s is not bound to a generic function"
+                        '?name))
+             (make <generic> :name '?name)))
+       (add-method! ?name
+                    (make <method>
+                      :generic ?name
+                      :specializers (list ?specs ...)
+                      :lambda-list  '?lambda-list
+                      :body (lambda (?body-arg ... next-method)
+                              ?body ...)))))
+    ;; terminating case
+    ((_ "args" ?name ((?arg ?class)) ?body
+        (?spec ...) (?llist ...) (?barg ...))
+     (define-method "args" ?name () ?body
+       (?spec ... ?class) (?llist ... ?arg) (?barg ... ?arg)))
+    ((_ "args" ?name (?arg) ?body
+        (?spec ...) (?llist ...) (?barg ...))
+     (define-method "args" ?name () ?body
+       (?spec ... <top>) (?llist ... ?arg) (?barg ... ?arg)))
+    ;; normal case
+    ((_ "args" ?name ((?arg ?class) . ?args) ?body
+        (?spec ...) (?llist ...) (?barg ...))
+     (define-method "args" ?name ?args ?body
+       (?spec ... ?class) (?llist ... ?arg) (?barg ... ?arg)))
+    ((_ "args" ?name (?arg . ?args) ?body
+        (?spec ...) (?llist ...) (?barg ...))
+     (define-method "args" ?name ?args ?body
+       (?spec ... <top>) (?llist ... ?arg) (?barg ... ?arg)))
+    ;; &rest arg.  we need special case for single &rest arg.
+    ((_ "args" ?name ?arg ?body 
+        () () ())
+     (define-method "args" ?name () ?body
+       () ?arg (?arg)))
+    ((_ "args" ?name ?arg ?body 
+        (?spec ...) (?llist ...) (?barg ...))
+     (define-method "args" ?name () ?body
+       (?spec ...) (?llist ... . ?arg) (?barg ... ?arg)))
+    ;; entry
+    ((_ ?name ?args ?body ...)
+     (define-method "args" ?name ?args (?body ...) () () ()))
+    ))
+
+;;----------------------------------------------------------------
+;; Class
+;;
 
 (define-syntax define-class
   (syntax-rules ()
-    ((_ ?name ?supers (?slot ...) ?options ...)
+    ((_ ?name (?super ...) (?slot ...) ?options ...)
      (define ?name
        (make <class>
-             :supers ?supers
+             :supers (list ?super ...)
              :slots (list (%parse-slot-definition ?slot) ...)
              ?options ...
              :name '?name)))))
@@ -63,16 +145,49 @@
      '?name)
     ))
 
+(define-method initialize ((class <class>) initargs)
+  (let ((name   (get-keyword :name   initargs #f))
+        (slots  (get-keyword :slots  initargs '()))
+        (supers (let ((s (get-keyword :supers initargs '())))
+                  (if (null? s) (list <object>) s))))
+    (slot-set! class 'name name)
+    (slot-set! class 'direct-supers supers)
+    (slot-set! class 'cpl (compute-cpl class))
+    (slot-set! class 'direct-slots
+               (map (lambda (s) (if (pair? s) s (list s))) slots))
+    (slot-set! class 'slots (compute-slots class))
+    ))
+
+(define-method compute-slots ((class <class>))
+  (let ((cpl (slot-ref class 'cpl))
+        (slots '()))
+    (for-each (lambda (c)
+                (for-each (lambda (slot)
+                            (unless (assq (car slot) slots)
+                              (set! slots (cons slot slots))))
+                          (slot-ref c 'direct-slots)))
+              cpl)
+    (reverse slots)))
+
+;;----------------------------------------------------------------
+;; Introspection routines
+;;
+
 (define (class-name class) (slot-ref class 'name))
 (define (class-precedence-list class) (slot-ref class 'cpl))
 (define (class-direct-supers class) (slot-ref class 'direct-supers))
 (define (class-direct-slots class) (slot-ref class 'direct-slots))
 (define (class-slots class) (slot-ref class 'slots))
 
+;; more to come ...
+
+;;----------------------------------------------------------------
+;; Handy for debug (just for now)
+;;
 
 (define (%inspect obj)
   (if (is-a? obj <object>)
       (for-each (lambda (slot)
-                  (format #t "~s: ~s\n" slot (slot-ref obj slot)))
+                  (format #t "~s: ~s\n" (car slot) (slot-ref obj (car slot))))
                 (class-slots (class-of obj)))
       obj))
