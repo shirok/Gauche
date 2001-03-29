37801ebe108af68c6b9aec484456d6cb5175a9cb
Shiro Kawai <shiro@acm.org>
2001-03-29 09:07:50 +0000

    This commit was manufactured by cvs2svn to create tag 'release0_2_14'.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/tags/release0_2_14@1033 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ test/srfi.scm
@@ -0,0 +1,118 @@
+;;
+;; Test for SRFIs
+;;
+
+;; $Id: srfi.scm,v 1.3 2001-03-24 10:09:37 shiro Exp $
+
+(add-load-path "../lib")
+(use gauche.test)
+
+(test-start "SRFIs")
+
+;;-----------------------------------------------------------------------
+(test-section "srfi-2")
+(use srfi-2)
+
+(define (srfi-2-look-up key alist)
+  (and-let* ((x (assq key alist))) (cdr x)))
+(test "and-let*" 3
+      (lambda () (srfi-2-look-up 'c '((a . 1) (b . 2) (c . 3)))))
+(test "and-let*" #f
+      (lambda () (srfi-2-look-up 'd '((a . 1) (b . 2) (c . 3)))))
+(test "and-let*" 3
+      (lambda ()
+        (let ((x 3))
+          (and-let* (((positive? x))
+                     (y x))
+                    y))))
+(test "and-let*" #f
+      (lambda ()
+        (let ((x -3))
+          (and-let* (((positive? x))
+                     (y x))
+                    y))))
+
+;;-----------------------------------------------------------------------
+(test-section "srfi-17")
+(use srfi-17)
+
+(define x (cons 1 2))
+(test "(setter car)" '((3 3) . 2)
+      (lambda () (set! (car x) (list 3 3)) x))
+(test "(setter cdr)" '((3 3) 4 5)
+      (lambda () (set! (cdr x) (list 4 5)) x))
+(test "(setter caar)" '(((8 9) 3) 4 5)
+      (lambda () (set! (caar x) (list 8 9)) x))
+(test "(setter cadr)" '(((8 9) 3) (7 6) 5)
+      (lambda () (set! (cadr x) (list 7 6)) x))
+(test "(setter cdar)" '(((8 9) 4 5) (7 6) 5)
+      (lambda () (set! (cdar x) (list 4 5)) x))
+(test "(setter cddr)" '(((8 9) 4 5) (7 6) 11 12)
+      (lambda () (set! (cddr x) (list 11 12)) x))
+(test "(setter caaar)" '((((13 14) 9) 4 5) (7 6) 11 12)
+      (lambda () (set! (caaar x) (list 13 14)) x))
+(test "(setter caadr)" '((((13 14) 9) 4 5) ((0 1) 6) 11 12)
+      (lambda () (set! (caadr x) (list 0 1)) x))
+(test "(setter cadar)" '((((13 14) 9) (2 3) 5) ((0 1) 6) 11 12)
+      (lambda () (set! (cadar x) (list 2 3)) x))
+(test "(setter caddr)" '((((13 14) 9) (2 3) 5) ((0 1) 6) (4 5) 12)
+      (lambda () (set! (caddr x) (list 4 5)) x))
+(test "(setter cdaar)" '((((13 14) 5 6) (2 3) 5) ((0 1) 6) (4 5) 12)
+      (lambda () (set! (cdaar x) (list 5 6)) x))
+(test "(setter cdadr)" '((((13 14) 5 6) (2 3) 5) ((0 1) 7 8) (4 5) 12)
+      (lambda () (set! (cdadr x) (list 7 8)) x))
+(test "(setter cddar)" '((((13 14) 5 6) (2 3) 9 10) ((0 1) 7 8) (4 5) 12)
+      (lambda () (set! (cddar x) (list 9 10)) x))
+(test "(setter cdddr)" '((((13 14) 5 6) (2 3) 9 10) ((0 1) 7 8) (4 5) -1 -2)
+      (lambda () (set! (cdddr x) (list -1 -2)) x))
+(test "(setter caaaar)" '(((((1 3) 14) 5 6) (2 3) 9 10) ((0 1) 7 8) (4 5) -1 -2)
+      (lambda () (set! (caaaar x) (list 1 3)) x))
+(test "(setter caaadr)" '(((((1 3) 14) 5 6) (2 3) 9 10) (((2 3) 1) 7 8) (4 5) -1 -2)
+      (lambda () (set! (caaadr x) (list 2 3)) x))
+(test "(setter caadar)" '(((((1 3) 14) 5 6) ((0 1) 3) 9 10) (((2 3) 1) 7 8) (4 5) -1 -2)
+      (lambda () (set! (caadar x) (list 0 1)) x))
+(test "(setter caaddr)" '(((((1 3) 14) 5 6) ((0 1) 3) 9 10) (((2 3) 1) 7 8) ((0 1) 5) -1 -2)
+      (lambda () (set! (caaddr x) (list 0 1)) x))
+(test "(setter cadaar)" '(((((1 3) 14) (0 1) 6) ((0 1) 3) 9 10) (((2 3) 1) 7 8) ((0 1) 5) -1 -2)
+      (lambda () (set! (cadaar x) (list 0 1)) x))
+(test "(setter cadadr)" '(((((1 3) 14) (0 1) 6) ((0 1) 3) 9 10) (((2 3) 1) (0 1) 8) ((0 1) 5) -1 -2)
+      (lambda () (set! (cadadr x) (list 0 1)) x))
+(test "(setter caddar)" '(((((1 3) 14) (0 1) 6) ((0 1) 3) (0 1) 10) (((2 3) 1) (0 1) 8) ((0 1) 5) -1 -2)
+      (lambda () (set! (caddar x) (list 0 1)) x))
+(test "(setter cadddr)" '(((((1 3) 14) (0 1) 6) ((0 1) 3) (0 1) 10) (((2 3) 1) (0 1) 8) ((0 1) 5) (0 1) -2)
+      (lambda () (set! (cadddr x) (list 0 1)) x))
+(test "(setter cdaaar)" '(((((1 3) 0 1) (0 1) 6) ((0 1) 3) (0 1) 10) (((2 3) 1) (0 1) 8) ((0 1) 5) (0 1) -2)
+      (lambda () (set! (cdaaar x) (list 0 1)) x))
+(test "(setter cdaadr)" '(((((1 3) 0 1) (0 1) 6) ((0 1) 3) (0 1) 10) (((2 3) 0 1) (0 1) 8) ((0 1) 5) (0 1) -2)
+      (lambda () (set! (cdaadr x) (list 0 1)) x))
+(test "(setter cdadar)" '(((((1 3) 0 1) (0 1) 6) ((0 1) 0 1) (0 1) 10) (((2 3) 0 1) (0 1) 8) ((0 1) 5) (0 1) -2)
+      (lambda () (set! (cdadar x) (list 0 1)) x))
+(test "(setter cdaddr)" '(((((1 3) 0 1) (0 1) 6) ((0 1) 0 1) (0 1) 10) (((2 3) 0 1) (0 1) 8) ((0 1) 0 1) (0 1) -2)
+      (lambda () (set! (cdaddr x) (list 0 1)) x))
+(test "(setter cddaar)" '(((((1 3) 0 1) (0 1) 0 1) ((0 1) 0 1) (0 1) 10) (((2 3) 0 1) (0 1) 8) ((0 1) 0 1) (0 1) -2)
+      (lambda () (set! (cddaar x) (list 0 1)) x))
+(test "(setter cddadr)" '(((((1 3) 0 1) (0 1) 0 1) ((0 1) 0 1) (0 1) 10) (((2 3) 0 1) (0 1) 0 1) ((0 1) 0 1) (0 1) -2)
+      (lambda () (set! (cddadr x) (list 0 1)) x))
+(test "(setter cdddar)" '(((((1 3) 0 1) (0 1) 0 1) ((0 1) 0 1) (0 1) 0 1) (((2 3) 0 1) (0 1) 0 1) ((0 1) 0 1) (0 1) -2)
+      (lambda () (set! (cdddar x) (list 0 1)) x))
+(test "(setter cddddr)" '(((((1 3) 0 1) (0 1) 0 1) ((0 1) 0 1) (0 1) 0 1) (((2 3) 0 1) (0 1) 0 1) ((0 1) 0 1) (0 1) 0 1)
+      (lambda () (set! (cddddr x) (list 0 1)) x))
+
+(define x '#(1 2 3 4 5))
+(test "(setter vector-ref)" '#(1 2 3 #f 5)
+      (lambda () (set! (vector-ref x 3) #f) x))
+
+(define x "abcde")
+(test "(setter string-ref)" "abcQe"
+      (lambda () (set! (string-ref x 3) #\Q) x))
+
+(define (set-kar! p v) (set-car! p v))
+(define kar (getter-with-setter (lambda (p) (car p)) set-kar!))
+
+(define x (cons 1 2))
+(test "(setter kar)" '(3 . 2) (lambda () (set! (kar x) 3) x))
+
+;; see it works as the normal set!
+(test "set!" '#f (lambda () (set! x #f) x))
+
+(test-end)
