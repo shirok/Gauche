e49edfcd606eabcc9990c22d460e775f1231539e
Shiro Kawai <shiro@acm.org>
2001-04-03 10:05:35 +0000

    some more coding.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1120 c205566b-fb4e-0410-ab5c-8f0660156277

--- src/char.c
+++ src/char.c
@@ -12,7 +12,7 @@
  *  warranty.  In no circumstances the author(s) shall be liable
  *  for any damages arising out of the use of this software.
  *
- *  $Id: char.c,v 1.7 2001-04-01 23:18:03 shiro Exp $
+ *  $Id: char.c,v 1.8 2001-04-03 10:05:35 shiro Exp $
  */
 
 #include "gauche.h"
@@ -28,6 +28,26 @@ static int charset_compare(ScmObj x, ScmObj y);
 SCM_DEFINE_BUILTIN_CLASS(Scm_CharSetClass,
                          charset_print, charset_compare, NULL,
                          SCM_CLASS_DEFAULT_CPL);
+
+/* masks */
+#if SIZEOF_LONG == 4
+#define MASK_BIT_SHIFT  5
+#define MASK_BIT_MASK   0x1f
+#elif SIZEOF_LONG == 8
+#define MASK_BIT_SHIFT  6
+#define MASK_BIT_MASK   0x3f
+#elif SIZEOF_LONG == 16    /* maybe, in some future ... */
+#define MASK_BIT_SHIFT  7
+#define MASK_BIT_MASK   0x7f
+#else
+#error need to set SIZEOF_LONG
+#endif
+
+#define MASK_INDEX(ch)       ((ch) >> MASK_BIT_SHIFT)
+#define MASK_BIT(ch)         (1L << ((ch) & MASK_BIT_MASK))
+#define MASK_ISSET(cs, ch)   (cs->mask[MASK_INDEX(ch)] & MASK_BIT(ch))
+#define MASK_SET(cs, ch)     (cs->mask[MASK_INDEX(ch)] |= MASK_BIT(ch))
+#define MASK_RESET(cs, ch)   (cs->mask[MASK_INDEX(ch)] &= ~MASK_BIT(ch))
 
 /* printer */
 static int charset_print(ScmObj obj, ScmPort *out, int mode)
@@ -77,6 +97,33 @@ ScmObj Scm_CopyCharSet(ScmCharSet *src)
     }
     if (rd) rd->next = NULL;
     return SCM_OBJ(dst);
+}
+
+/* modification */
+
+ScmObj Scm_CharSetAdd(ScmCharSet *cs, ScmChar from, ScmChar to)
+{
+    int i;
+    struct ScmCharSetRange *lo, *lop, *hi, *hip, *n;
+    
+    if (to <= from) return;
+    if (from < SCM_CHARSET_MASK_CHARS) {
+        if (to < SCM_CHARSET_MASK_CHARS) {
+            for (i=from; i<=to; i++) MASK_SET(cs, i);
+            return SCM_OBJ(cs);
+        }
+        for (i=from; i<SCM_CHARSET_MASK_CHARS; i++)  MASK_SET(cs, i);
+        from = SCM_CHARSET_MASK_CHARS;
+    }
+    if (cs->ranges == NULL) {
+        cs->ranges = SCM_NEW(struct ScmCharSetRange);
+        cs->ranges->next = NULL;
+        cs->ranges->lo = from;
+        cs->ranges->hi = to;
+        return SCM_OBJ(cs);
+    }
+    /* WRITE ME */
+    return SCM_OBJ(cs);
 }
 
 /*
