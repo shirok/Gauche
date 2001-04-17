9856f46d8203039d2a728a348458d7b868f10766
Shiro Kawai <shiro@acm.org>
2001-04-17 09:57:13 +0000

    more tests
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1232 c205566b-fb4e-0410-ab5c-8f0660156277

--- test/regexp.scm
+++ test/regexp.scm
@@ -2,7 +2,7 @@
 ;; testing regexp
 ;;
 
-;; $Id: regexp.scm,v 1.2 2001-04-16 09:33:25 shiro Exp $
+;; $Id: regexp.scm,v 1.3 2001-04-17 09:57:13 shiro Exp $
 
 (use gauche.test)
 (use srfi-1)
@@ -16,45 +16,179 @@
 ;; regexp package.
 
 (define (match&list rx input nmatch)
-  (map (lambda (n) (rxmatch-substring (rxmatch rx input) n))
-       (iota nmatch)))
+  (cond ((rxmatch rx input)
+         => (lambda (match)
+              (map (lambda (n) (rxmatch-substring match n))
+                   (iota nmatch))))
+        (else #f)))
 
 ;;-------------------------------------------------------------------------
 (test-section "basics")
 
 (test "a" '("a")
       (lambda () (match&list #/a/ "a" 1)))
+(test "a" #f
+      (lambda () (match&list #/a/ "A" 1)))
+(test "a" '("a")
+      (lambda () (match&list #/a/ "ba" 1)))
+(test "a" '("a")
+      (lambda () (match&list #/a/ "bac" 1)))
+(test "a" #f      ;; input null str
+      (lambda () (match&list #/a/ "" 1)))
+(test "a" '("a")  ;; input includes NUL character
+      (lambda () (match&list #/a/ (string (integer->char 0) #\a #\b) 1)))
 (test "abc" '("abc")
       (lambda () (match&list #/abc/ "abc" 1)))
+(test "abc" #f
+      (lambda () (match&list #/abc/ "abbc" 1)))
 (test "abc" '("abc")
-      (lambda () (match&list #/abc|de/ "abc" 1)))
-(test "abc" '("a")
+      (lambda () (match&list #/abc/ "babcd" 1)))
+(test "abc|de" '("abc")
+      (lambda () (match&list #/abc|de/ "dabce" 1)))
+(test "abc|de" '("de")
+      (lambda () (match&list #/abc|de/ "abdec" 1)))
+(test "abc|de" #f
+      (lambda () (match&list #/abc|de/ "abe" 1)))
+(test "a|b|c" '("a")
       (lambda () (match&list #/a|b|c/ "abc" 1)))
+(test "a|b|c" '("b")
+      (lambda () (match&list #/a|b|c/ "bac" 1)))
+(test "a|b|c" #f
+      (lambda () (match&list #/a|b|c/ "def" 1)))
+(test "|abc" '("")
+      (lambda () (match&list #/|abc/ "abc" 1)))
+(test "abc|" '("abc")
+      (lambda () (match&list #/abc|/ "abc" 1)))
+(test "abc|" '("")
+      (lambda () (match&list #/abc|/ "abd" 1)))
 
 ;;-------------------------------------------------------------------------
 (test-section "parens")
 
-(test "abc" '("abc" "b")
+(test "a(b)c" '("abc" "b")
       (lambda () (match&list #/a(b)c/ "abc" 2)))
-(test "abc" '("a(")
+(test "a((b)(c))" '("abc" "bc" "b" "c")
+      (lambda () (match&list #/a((b)(c))/ "abc" 4)))
+(test "a((((b))))c" '("abc" "b" "b" "b" "b")
+      (lambda () (match&list #/a((((b))))c/ "abc" 5)))
+(test "a((((b))))c" '#f
+      (lambda () (match&list #/a((((b))))c/ "a(b)c" 5)))
+(test "a\\(" '("a(")
       (lambda () (match&list #/a\(/ "a(" 1)))
-(test "abc" '("ab" "")
+(test "a()b" '("ab" "")
       (lambda () (match&list #/a()b/ "ab" 2)))
+(test "a()()b" '("ab" "" "")
+      (lambda () (match&list #/a()()b/ "ab" 3)))
+(test "(we|wee|week|frob)(knights|night|day)"
+      '("weeknights" "wee" "knights")
+      (lambda () (match&list #/(we|wee|week|frob)(knights|night|day)/
+                             "weeknights" 3)))
+
+;;-------------------------------------------------------------------------
+(test-section "simple meta")
+
+(test "a.c" '("abc")
+      (lambda () (match&list #/a.c/ "abc" 1)))
+(test "a.." '("abc")
+      (lambda () (match&list #/a../ "abc" 1)))
+(test "a.." #f
+      (lambda () (match&list #/a../ "ab" 1)))
+(test "..." #f
+      (lambda () (match&list #/.../ "ab" 1)))
+(test "." '("a")
+      (lambda () (match&list #/./ "abc" 1)))
+(test "." #f
+      (lambda () (match&list #/./ "" 1)))
 
 ;;-------------------------------------------------------------------------
 (test-section "anchors")
 
-;; not supported yet
+(test "^abc" '("abc")
+      (lambda () (match&list #/^abc/ "abcd" 1)))
+(test "^abc" #f
+      (lambda () (match&list #/^abc/ "aabcd" 1)))
+(test "^^" '("^")
+      (lambda () (match&list #/^^/ "^^abc" 1)))
+(test "^^" #f
+      (lambda () (match&list #/^^/ "a^^c" 1)))
+(test "^abc|def" '("abc")
+      (lambda () (match&list #/^abc|def/ "abc" 1)))
+(test "^abc|def" #f
+      (lambda () (match&list #/^abc|def/ "zabc" 1)))
+(test "^abc|def" '("def")
+      (lambda () (match&list #/^abc|def/ "zabcdef" 1)))
+(test "abc|^def" '("def")
+      (lambda () (match&list #/abc|^def/ "defabc" 1)))
+(test "abc|^def" '("abc")
+      (lambda () (match&list #/abc|^def/ "abcdef" 1)))
+(test "abc|^def" '("def")
+      (lambda () (match&list #/abc|^def/ "defabbc" 1)))
+(test "abc|^def" #f
+      (lambda () (match&list #/abc|^def/ "adefbc" 1)))
+(test "^(abc|def)" '("abc" "abc")
+      (lambda () (match&list #/^(abc|def)/ "abc" 2)))
+(test "^(abc|def)" #f
+      (lambda () (match&list #/^(abc|def)/ "aabc" 2)))
+(test "(^abc|def)" '("abc" "abc")
+      (lambda () (match&list #/(^abc|def)/ "abcdef" 2)))
+(test "(^abc|def)" '("def" "def")
+      (lambda () (match&list #/(^abc|def)/ "^abcdef" 2)))
+(test "a(^bc|def)" '("a^bc" "^bc")
+      (lambda () (match&list #/a(^bc|def)/ "a^bcdef" 2)))
+(test "a(^bc|def)" #f
+      (lambda () (match&list #/a(^bc|def)/ "abcdef" 2)))
+(test "^" '("")
+      (lambda () (match&list #/^/ "hoge" 1)))
+(test "$" '("")
+      (lambda () (match&list #/$/ "hoge" 1)))
+(test "abc$" '("abc")
+      (lambda () (match&list #/abc$/ "bcabc" 1)))
+(test "abc$" #f
+      (lambda () (match&list #/abc$/ "abcab" 1)))
+(test "^abc$" '("abc")
+      (lambda () (match&list #/^abc$/ "abc" 1)))
+(test "abc$$" #f
+      (lambda () (match&list #/abc$$/ "abc" 1)))
+(test "abc$$" '("abc$")
+      (lambda () (match&list #/abc$$/ "abc$" 1)))
+(test "$$" '("$")
+      (lambda () (match&list #/$$/ "abc$" 1)))
+(test "^$" '("")
+      (lambda () (match&list #/^$/ "" 1)))
+(test "^$" #f
+      (lambda () (match&list #/^$/ "a" 1)))
+(test "^^$$" '("^$")
+      (lambda () (match&list #/^^$$/ "^$" 1)))
+(test "abc$|def" '("abc")
+      (lambda () (match&list #/abc$|def/ "abc" 1)))
+(test "abc$|def" '("def")
+      (lambda () (match&list #/abc$|def/ "defabc" 1)))
+(test "^abc|def$" '("abc")
+      (lambda () (match&list #/^abc|def$/ "abcdef" 1)))
+(test "^abc|def$" #f
+      (lambda () (match&list #/^abc|def$/ "defabc" 1)))
+(test "^abc|def$" #f
+      (lambda () (match&list #/^abc|def$/ "defabc" 1)))
+(test "(^abc|def$)" '("def" "def")
+      (lambda () (match&list #/(^abc|def$)/ "aaadef" 2)))
+(test "(^abc|def$)$" #f
+      (lambda () (match&list #/(^abc|def$)$/ "aaadef" 1)))
+(test "(^abc|def$)$" '("def$")
+      (lambda () (match&list #/(^abc|def$)$/ "aaadef$" 1)))
+(test "(abc$|def)$" #f
+      (lambda () (match&list #/(abc$|def)$/ "aaabc" 1)))
+(test "(abc$|def)$" '("abc$")
+      (lambda () (match&list #/(abc$|def)$/ "aaabc$" 1)))
 
 ;;-------------------------------------------------------------------------
 (test-section "backslash escape")
 
-(test "a.c" '("abc")
-      (lambda () (match&list #/a.c/ "abc" 1)))
-(test "a[bc]d" '("abd")
-      (lambda () (match&list #/a[bc]d/ "abd" 1)))
 (test "a\\*c" '("a*c")
       (lambda () (match&list #/a\*c/ "a*c" 1)))
+(test "a\\.c" '("a.c")
+      (lambda () (match&list #/a\.c/ "a.c" 1)))
+(test "a\\.c" #f
+      (lambda () (match&list #/a\.c/ "abc" 1)))
 (test "a\\\\b" '("a\\b")
       (lambda () (match&list #/a\\b/ "a\\b" 1)))
 (test "a\\\\\\*b" '("a\\*b")
@@ -87,5 +221,84 @@
       (lambda () (match&list #/ab?c/ "abc" 1)))
 (test "ab?c" '("ac")
       (lambda () (match&list #/ab?c/ "abbaac" 1)))
+(test "a.*c" '("abc")
+      (lambda () (match&list #/a.*c/ "abc" 1)))
+(test "a.*c" '("aabcabcabcabcc")
+      (lambda () (match&list #/a.*c/ "zaabcabcabcabcczab" 1)))
+(test "a(b*|c)d" '("abbd" "bb")
+      (lambda () (match&list #/a(b*|c)d/ "abbd" 2)))
+(test "a(b*|c)d" '("ad" "")
+      (lambda () (match&list #/a(b*|c)d/ "ad" 2)))
+(test "a(b*|c)d" '("acd" "c")
+      (lambda () (match&list #/a(b*|c)d/ "acd" 2)))
+(test "a(b*|c)d" #f
+      (lambda () (match&list #/a(b*|c)d/ "abcd" 2)))
+(test "a.*c" '("ac")
+      (lambda () (match&list #/a.*c/ "bacbababbbbadbaba" 1)))
+(test "a.*c" #f
+      (lambda () (match&list #/a.*c/ "abaaaabababbadbabdba" 1)))
+
+;;-------------------------------------------------------------------------
+(test-section "character class")
+
+(test "a[bc]d" '("abd")
+      (lambda () (match&list #/a[bc]d/ "abd" 1)))
+(test "a[bc]d" '("acd")
+      (lambda () (match&list #/a[bc]d/ "acd" 1)))
+(test "a[bc]d" #f
+      (lambda () (match&list #/a[bc]d/ "aed" 1)))
+(test "a[a-z]d" '("aed")
+      (lambda () (match&list #/a[a-z]d/ "aed" 1)))
+(test "a[a-z]d" #f
+      (lambda () (match&list #/a[a-z]d/ "aEd" 1)))
+(test "a[-a-z]d" '("a-d")
+      (lambda () (match&list #/a[-a-z]d/ "a-d" 1)))
+(test "a[a-z-]d" '("a-d")
+      (lambda () (match&list #/a[a-z-]d/ "a-d" 1)))
+(test "a[a-z-]d" '("afd")
+      (lambda () (match&list #/a[a-z-]d/ "afd" 1)))
+(test "a[az-]d" '("a-d")
+      (lambda () (match&list #/a[az-]d/ "a-d" 1)))
+(test "a[a-]d" '("a-d")
+      (lambda () (match&list #/a[az-]d/ "a-d" 1)))
+(test "a[az-]d" #f
+      (lambda () (match&list #/a[az-]d/ "afd" 1)))
+(test "a[az-]d" '("azd")
+      (lambda () (match&list #/a[az-]d/ "azd" 1)))
+(test "a[^ab]c" '("acc")
+      (lambda () (match&list #/a[^ab]c/ "abacc" 1)))
+(test "a[Bc]*d" '("aBccBd")
+      (lambda () (match&list #/a[Bc]*d/ "aBccBd" 1)))
+(test "[a]b[c]" '("abc")
+      (lambda () (match&list #/[a]b[c]/ "abc" 1)))
+(test "[abc]b[abc]" '("abc")
+      (lambda () (match&list #/[abc]b[abc]/ "abc" 1)))
+(test "a[bc]d" '("abd")
+      (lambda () (match&list #/a[bc]d/ "xyzaaabcaababdacd" 1)))
+(test "a[ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab](wee|week)(knights|night)"
+      '("aaaaabaaaabaaaabaaaabweeknights" "wee" "knights")
+      (lambda ()
+        (match&list #/a[ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab][ab](wee|week)(knights|night)/
+                    "aaaaabaaaabaaaabaaaabweeknights" 3)))
+(test "[ab][cd][ef][gh][ij][kl][mn]"
+      '("acegikm")
+      (lambda ()
+        (match&list #/[ab][cd][ef][gh][ij][kl][mn]/
+                    "xacegikmoq" 1)))
+(test "[ab][cd][ef][gh][ij][kl][mn][op]"
+      '("acegikmo")
+      (lambda ()
+        (match&list #/[ab][cd][ef][gh][ij][kl][mn][op]/
+                    "xacegikmoq" 1)))
+(test "[ab][cd][ef][gh][ij][kl][mn][op][qr]"
+      '("acegikmoq")
+      (lambda ()
+        (match&list #/[ab][cd][ef][gh][ij][kl][mn][op][qr]/
+                    "xacegikmoqy" 1)))
+(test "[ab][cd][ef][gh][ij][kl][mn][op][q]"
+      '("acegikmoq")
+      (lambda ()
+        (match&list #/[ab][cd][ef][gh][ij][kl][mn][op][q]/
+                    "xacegikmoqy" 1)))
 
 (test-end)
