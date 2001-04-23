ccd403fc35af1b1747c9931ae6f4ba4c3e28a800
Shiro Kawai <shiro@acm.org>
2001-04-23 07:10:09 +0000

    added some complex-related functions
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1286 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/gauche/numerical.scm
+++ lib/gauche/numerical.scm
@@ -12,7 +12,7 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: numerical.scm,v 1.1 2001-04-23 06:30:46 shiro Exp $
+;;;  $Id: numerical.scm,v 1.2 2001-04-23 07:10:09 shiro Exp $
 ;;;
 
 (select-module gauche)
@@ -62,11 +62,49 @@
           ((null? (cdr args)) (car args))
           (else (recn (car args) (cdr args))))))
 
+;;
+;; Numerator and denominator
+;;
+
+;; in Gauche, rational number is just the same as integer, i.e. N/1
+
+(define (numerator q)
+  (unless (integer? q) (error "integer required, but got: %S" q))
+  q)
+
+(define (denominator q)
+  (unless (integer? q) (error "integer required, but got: %S" q))
+  (if (exact? q) 1 1.0))
+
+;;
+;; Complex numbers
+;;
+
+(define (make-polar r t)
+  (check-arg real? r)
+  (check-arg real? t)
+  (make-rectangular (* r (cos t)) (* r (sin t))))
+
+(define (real-part z)
+  (cond ((real? z) z)
+        ((number? z) (car (%complex->real/imag z)))
+        (else (error "number required, but got: %S" z))))
+
+(define (imag-part z)
+  (cond ((real? z) (if (exact? z) 0 0.0))
+        ((number? z) (cdr (%complex->real/imag z)))
+        (else (error "number required, but got: %S" z))))
 
 ;; Insert R5RS functions into scheme module
 
 (with-module scheme
   (define gcd (with-module gauche gcd))
-  (define lcm (with-module gauche lcm)))
+  (define lcm (with-module gauche lcm))
+  (define numerator (with-module gauche numerator))
+  (define denominator (with-module gauche denominator))
+  (define make-polar (with-module gauche make-polar))
+  (define real-part (with-module gauche real-part))
+  (define imag-part (with-module gauche imag-part))
+  )
 
 (provide "gauche/numerical")
