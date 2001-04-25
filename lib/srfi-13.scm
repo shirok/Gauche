52fe75b3b0ee9c5e4262b2215b7abc2349043c7c
Shiro Kawai <shiro@acm.org>
2001-04-25 07:31:53 +0000

    added autoloaders and %get-char-pred macro
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1299 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/srfi-13.scm
+++ lib/srfi-13.scm
@@ -12,7 +12,7 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: srfi-13.scm,v 1.1 2001-04-23 09:35:55 shiro Exp $
+;;;  $Id: srfi-13.scm,v 1.2 2001-04-25 07:31:53 shiro Exp $
 ;;;
 
 ;; Srfi-13 is a large set of functions, so I splitted it up to a number of
@@ -69,6 +69,60 @@
           ))
 (select-module srfi-13)
 
+;; utility macro to handle char/char-set/pred match.
+(define-syntax %get-char-pred
+  (syntax-rules ()
+    ((_ char/char-set/pred)
+     (cond ((char? char/char-set/pred)
+            (lambda (x) (char=? char/char-set/pred x)))
+           ((char-set? char/char-set/pred)
+            (lambda (x) (char-set-contains? char/char-set/pred x)))
+           ((procedure? char/char-set/pred) char/char-set/pred)
+           (else (error "argument needs to be either a character, a char-set, or a procedure: %S" char/char-set/pred))))
+    ))
+
+(autoload "srfi-13/pred"      string-null? string-every string-any)
+(autoload "srfi-13/generator" string-tabulate reverse-list->string)
+(autoload "srfi-13/selector"  substring/shared string-copy!
+                              string-pad string-pad-right
+                              string-trim string-trim-right string-trim-both)
+(autoload "srfi-13/comparer"  string-compare string-compare-ci
+                              string= string<> string<
+                              string<= string> string>=
+                              string-ci= string-ci<> string-ci<
+                              stirng-ci<= string-ci> string-ci>=
+                              string-hash string-hash-ci)
+(autoload "srfi-13/prefix"    string-prefix-length string-suffix-length
+                              string-prefix-length-ci string-suffix-length-ci
+                              string-prefix? string-suffix?
+                              string-prefix-ci? string-suffix-ci?)
+(autoload "srfi-13/searcher"  string-index string-index-right
+                              string-skip string-skip-right
+                              string-count
+                              string-contains string-contains-ci)
+(autoload "srfi-13/casemap"   string-titlecase string-titlecase!
+                              string-upcase string-upcase!
+                              string-downcase string-downcase!)
+(autoload "srfi-13/revapp"    string-reverse string-reverse!
+                              string-concatenate string-concatenate/shared
+                              string-append/shared
+                              string-concatenate-reverse
+                              string-concatenate-reverse/shared)
+(autoload "srfi-13/folder"    string-map string-map!
+                              string-fold string-fold-right
+                              string-unfold string-unfold-right
+                              string-for-each string-for-each-index)
+(autoload "srfi-13/rotator"   xsubstring string-xcopy!)
+(autoload "srfi-13/misc"      string-replace string-tokenize)
+(autoload "srfi-13/filter"    string-filter string-delete)
+(autoload "srfi-13/internal"  string-parse-start+end
+                              string-parse-final-start+end
+                              let-string-start+end
+                              check-substring-spec
+                              substring-spec-ok?)
+(autoload "srfi-13/kmp"       make-kmp-restart-vector
+                              kmp-step
+                              string-kmp-partial-search)
 
 (provide "srfi-13")
 
