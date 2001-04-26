db9f62841318d8358eccb09247d8d6c86744f4f2
Shiro Kawai <shiro@acm.org>
2001-04-26 07:20:59 +0000

    added
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1308 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ src/gauche/char_sjis.h
@@ -0,0 +1,64 @@
+/*
+ * char-sjis.h
+ *
+ *  Copyright(C) 2000-2001 by Shiro Kawai (shiro@acm.org)
+ *
+ *  Permission to use, copy, modify, distribute this software and
+ *  accompanying documentation for any purpose is hereby granted,
+ *  provided that existing copyright notices are retained in all
+ *  copies and that this notice is included verbatim in all
+ *  distributions.
+ *  This software is provided as is, without express or implied
+ *  warranty.  In no circumstances the author(s) shall be liable
+ *  for any damages arising out of the use of this software.
+ *
+ *  $Id: char_sjis.h,v 1.1 2001-04-26 07:20:59 shiro Exp $
+ */
+
+#ifndef SCM_CHAR_ENCODING_BODY
+
+#define SCM_CHAR_ENCODING_NAME "sjis"
+
+#define SCM_CHAR_NFOLLOWS(byte)                 \
+    ((byte) < 0x80? 0 :                         \
+     ((byte) < 0xa0? 1 :                        \
+      ((byte) < 0xe0? 0 : 1)))
+
+#define SCM_CHAR_NBYTES(ch) (((ch) > 0x0ff) ? 2 : 1)
+
+#define SCM_CHAR_MAX_BYTES     2
+
+#define SCM_CHAR_GET(cp, ch)                                                \
+    do {                                                                    \
+        (ch) = *(cp);                                                       \
+        if ((unsigned char)(ch) >= 0x80) {                                  \
+           if ((unsigned char)(ch) < 0xa0 || (unsigned char)(ch) >= 0xe0) { \
+               (ch) = ((ch) << 8) + (unsigned char)*(cp+1);                 \
+        }                                                                   \
+    } while (0)
+
+#define SCM_CHAR_PUT(cp, ch)                    \
+    do {                                        \
+        if (ch > 0xff) {                        \
+            (cp)[0] = (ch >> 8) & 0xff;         \
+            (cp)[1] = ch & 0xff;                \
+        } else {                                \
+            (cp)[0] = ch & 0xff;                \
+        }                                       \
+    } while (0)
+
+#define SCM_CHAR_BACKWARD(cp, start, result)                                \
+    do {                                                                    \
+        (result) = (cp);                                                    \
+        while ((result) >= (start) && (cp)-(result)<= SCM_CHAR_MAX_BYTES) { \
+            if ((result) + SCM_CHAR_NFOLLOWS(*(result)) + 1 == (cp)) {      \
+                break;                                                      \
+            }                                                               \
+            (result)--;                                                     \
+        }                                                                   \
+        if ((result) < (start)) (result) = NULL;                            \
+    } while (0)
+
+#else  /* !SCM_CHAR_ENCODING_BODY */
+
+#endif /* !SCM_CHAR_ENCODING_BODY */
