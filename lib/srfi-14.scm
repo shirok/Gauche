03e1bec0d99edad3ae050771eaa5a86d4e40538a
Shiro Kawai <shiro@acm.org>
2001-04-26 07:06:30 +0000

    srfi-14 splitted to subfiles
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1306 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/srfi-14.scm
+++ lib/srfi-14.scm
@@ -12,7 +12,7 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: srfi-14.scm,v 1.4 2001-04-20 08:32:26 shiro Exp $
+;;;  $Id: srfi-14.scm,v 1.5 2001-04-26 07:06:30 shiro Exp $
 ;;;
 
 ;; Basic operators are built in the Gauche kernel.  This module
@@ -241,179 +241,23 @@
 ;; Querying
 ;;
 
-(define (char-set-size cs)
-  (let ((count 0))
-    (for-each (lambda (range)
-                (set! count (+ count (- (cdr range) (car range)) 1)))
-              (%char-set-ranges cs))
-    count))
-
-(define (char-set-count pred cs)
-  (char-set-fold (lambda (c r) (if (pred c) (+ r 1) r)) 0 cs))
-
-(define (char-set->list cs)
-  (char-set-fold cons '() cs))
-
-(define (char-set->string cs)
-  (list->string (char-set-fold cons '() cs)))
-
-;; char-set-contains? : native
-
-(define (char-set-every pred cs)
-  (let loop ((cursor (char-set-cursor cs)))
-    (cond ((end-of-char-set? cursor))
-          ((pred (char-set-ref cs cursor))
-           (loop (char-set-cursor-next cs cursor)))
-          (else #f))))
-
-(define (char-set-any pred cs)
-  (let loop ((cursor (char-set-cursor cs)))
-    (cond ((end-of-char-set? cursor) #f)
-          ((pred (char-set-ref cs cursor)) #t)
-          (else (loop (char-set-cursor-next cs cursor))))))
+(autoload "srfi-14/query" char-set-size char-set-count
+                          char-set->list char-set->string
+                          char-set-every char-set-any)
 
 ;;-------------------------------------------------------------------
 ;; Algebra
 ;;
 
-;; auxiliary functions for range operation other than adjoin and union.
-;; (adjoin and union can use native functions).  These can also be defined
-;; in C, but I don't see the needs of performance yet.
-(define (%char-set-sub-range ranges from to)
-  (let loop ((ranges ranges)
-             (result '()))
-    (if (null? ranges)
-        (reverse result)
-        (let ((lo (caar ranges))
-              (hi (cdar ranges))
-              (next (cdr ranges)))
-          (if (<= lo from hi)
-              (if (<= lo to hi)
-                  (if (= lo from)
-                      (if (= to hi)
-                          (loop next result)
-                          (loop next (acons (+ to 1) hi result)))
-                      (if (= to hi)
-                          (loop next (acons lo (- from 1) result))
-                          (loop next (list* (cons (+ to 1) hi)
-                                            (cons lo (- from 1))
-                                            result))))
-                  (if (= lo from)
-                      (loop next result)
-                      (loop next (acons lo (- from 1) result))))
-              (if (<= lo to hi)
-                  (if (= to hi)
-                      (loop next result)
-                      (loop next (acons (+ to 1) hi result)))
-                  (if (and (< from lo) (< hi to))
-                      (loop next result)
-                      (loop next (acons lo hi result)))))
-          ))
-    ))
-
-(define (%char-set-sub-chars range chars)
-  (let loop ((range range)
-             (chars chars))
-    (if (null? chars)
-        range
-        (let ((code (char->integer (car chars))))
-          (loop (%char-set-sub-range range code code) (cdr chars))))))
-
-(define (%ranges->char-set ranges)
-  (let ((base (char-set)))
-    (for-each (lambda (range)
-                (%char-set-add-range! base (car range) (cdr range)))
-              ranges)
-    base))
-
-(define (char-set-adjoin base . chars)
-  (if (null? chars) base (%char-set-add-chars! (char-set-copy base) chars)))
-
-(define (char-set-adjoin! base . chars)
-  (%char-set-add-chars! base chars))
-
-(define (char-set-delete base . chars)
-  (%ranges->char-set (%char-set-sub-chars (%char-set-ranges base) chars)))
-
-(define char-set-delete! char-set-delete)
-
-(define (char-set-complement cs)
-  (%char-set-complement! (char-set-copy cs)))
-
-(define (char-set-complement! cs) (%char-set-complement! cs))
-
-(define (char-set-union . charsets)
-  (if (null? charsets)
-      char-set:empty
-      (let loop ((base (char-set-copy (car charsets)))
-                 (rest (cdr charsets)))
-        (if (null? rest)
-            base
-            (loop (%char-set-add! base (car rest)) (cdr rest))))))
-
-(define (char-set-union! base . charsets)
-  (if (null? charsets)
-      base
-      (let loop ((base base) (rest charsets))
-        (if (null? rest)
-            base
-            (loop (%char-set-add! base (car rest)) (cdr rest))))))
-
-(define (%char-set-intersection2 x y)
-  (char-set-difference x (char-set-complement y)))
-
-(define (char-set-intersection . charsets)
-  (cond ((null? charsets) char-set:full)
-        ((null? (cdr charsets)) (car charsets))
-        (else (apply char-set-intersection
-                     (%char-set-intersection2 (car charsets) (cadr charsets))
-                     (cddr charsets)))))
-
-(define (char-set-intersection! base . charsets)
-  (if (null? charsets)
-      base
-      (apply char-set-intersection base charsets)))
-
-(define (char-set-difference base . charsets)
-  (define (char-range-sub2 ranges sub)
-    (let loop ((ranges ranges) (sub sub))
-      (if (null? sub)
-          ranges
-          (loop (%char-set-sub-range ranges (caar sub) (cdar sub))
-                (cdr sub)))))
-
-  (let loop ((ranges (%char-set-ranges base))
-             (sets charsets))
-    (if (null? sets)
-        (%ranges->char-set ranges)
-        (loop (char-range-sub2 ranges (%char-set-ranges (car sets)))
-              (cdr sets))))
-  )
-(define char-set-difference! char-set-difference)
-
-(define (%char-set-xor2 x y)
-  (char-set-difference (char-set-union x y)
-                       (%char-set-intersection2 x y)))
-
-(define (char-set-xor . charsets)
-  (cond ((null? charsets) char-set:empty)
-        ((null? (cdr charsets)) (car charsets))
-        (else (apply char-set-xor
-                     (%char-set-xor2 (car charsets) (cadr charsets))
-                     (cddr charsets)))))
-
-(define (char-set-xor! base . charsets)
-  (if (null? charsets)
-      base
-      (apply char-set-xor base charsets)))
-
-(define (char-set-diff+intersection base . charsets)
-  (values (apply char-set-difference base charsets)
-          (char-set-intersection base (apply char-set-union charsets))))
-
-(define (char-set-diff+intersection! cs1 cs2 . charsets)
-  (values (apply char-set-difference! cs1 cs2 charsets)
-          (char-set-intersection! cs1 (apply char-set-union! cs2 charsets))))
+(autoload "srfi-14/set"   char-set-adjoin char-set-adjoin!
+                          char-set-delete char-set-delete!
+                          char-set-complement char-set-complement!
+                          char-set-union char-set-union!
+                          char-set-intersection char-set-intersection!
+                          char-set-difference char-set-difference!
+                          char-set-xor char-set-xor!
+                          char-set-diff+intersection
+                          char-set-diff+intersection!)
 
 ;;-------------------------------------------------------------------
 ;; Predefined charsets
