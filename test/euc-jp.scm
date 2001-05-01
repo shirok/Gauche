9bcc32260c6465b5b206949c4aefee91ed6d087f
Shiro Kawai <shiro@acm.org>
2001-05-01 10:27:26 +0000

    added string builtin tests
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1336 c205566b-fb4e-0410-ab5c-8f0660156277

--- test/euc-jp.scm
+++ test/euc-jp.scm
@@ -1,12 +1,45 @@
 ;; this test only works when the core system is compiled with euc-jp.
 
-;; $Id: euc-jp.scm,v 1.3 2001-04-27 07:56:40 shirok Exp $
+;; $Id: euc-jp.scm,v 1.4 2001-05-01 10:27:26 shirok Exp $
 
 (use gauche.test)
 
 (test-start "EUC-JP")
 
-;; string-pointer
+;;-------------------------------------------------------------------
+(test-section "string builtins")
+
+(test "string" "いろhにほへt"
+      (lambda () (string #\い #\ろ #\h #\に #\ほ #\へ #\t)))
+(test "list->string" "いろhにほへt"
+      (lambda () (list->string '(#\い #\ろ #\h #\に #\ほ #\へ #\t))))
+(test "make-string" "へへへへへ" (lambda () (make-string 5 #\へ)))
+(test "make-string" "" (lambda () (make-string 0 #\へ)))
+
+(test "string->list" '(#\い #\ろ #\h #\に #\ほ #\へ #\t)
+      (lambda () (string->list "いろhにほへt")))
+
+(test "string-ref" #\ろ (lambda () (string-ref "いろは" 1)))
+(define x (string-copy "いろはにほ"))
+(test "string-set!" "いろZにほ" (lambda () (string-set! x 2 #\Z) x))
+
+(test "string-join" "ふぅ ばぁ ばず"
+      (lambda () (string-join '("ふぅ" "ばぁ" "ばず"))))
+(test "string-join" "ふぅ！ばぁ！ばず"
+      (lambda () (string-join '("ふぅ" "ばぁ" "ばず") "！")))
+(test "string-join" "ふぅ→←ばぁ→←ばず"
+      (lambda () (string-join '("ふぅ" "ばぁ" "ばず") "→←" 'infix)))
+(test "string-join" ""
+      (lambda () (string-join '() "→←")))
+(test "string-join" "ふぅ！ばぁ！ばず！"
+      (lambda () (string-join '("ふぅ" "ばぁ" "ばず") "！" 'suffix)))
+(test "string-join" "！ふぅ！ばぁ！ばず"
+      (lambda () (string-join '("ふぅ" "ばぁ" "ばず") "！" 'prefix)))
+(test "string-join" "ふぅ！ばぁ！ばず"
+      (lambda () (string-join '("ふぅ" "ばぁ" "ばず") "！" 'strict-infix)))
+
+;;-------------------------------------------------------------------
+(test-section "string-pointer")
 (define sp #f)
 (test "make-string-pointer" #t
       (lambda ()
