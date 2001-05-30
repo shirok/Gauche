b97640b21c6d60f23c428c3dcd971e3d4959fe2d
Shiro Kawai <shiro@acm.org>
2001-05-30 09:01:17 +0000

    use cpp flag HAVE_ICONV_H to avoid the compilation problem without iconv
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1549 c205566b-fb4e-0410-ab5c-8f0660156277

--- ext/charconv/charconv.c
+++ ext/charconv/charconv.c
@@ -12,12 +12,20 @@
  *  warranty.  In no circumstances the author(s) shall be liable
  *  for any damages arising out of the use of this software.
  *
- *  $Id: charconv.c,v 1.4 2001-05-29 08:42:24 shirok Exp $
+ *  $Id: charconv.c,v 1.5 2001-05-30 09:01:17 shirok Exp $
  */
 
-#include <iconv.h>
 #include <errno.h>
 #include <gauche.h>
+
+#ifdef HAVE_ICONV_H
+#include <iconv.h>
+#else
+typedef void *iconv_t;
+iconv_t iconv_open(const char *tocode, const char *fromcode);
+int iconv_close(iconv_t handle);
+int iconv(iconv_t handle, const char **inbuf, size_t *inroom, char **outbuf, size_t *outroom);
+#endif /* !HAVE_ICONV_H */
 
 #define DEFAULT_CONVERSION_BUFFER_SIZE 1024
 
@@ -31,16 +39,31 @@ typedef struct conv_info_rec {
     char *outptr;
 } conv_info;
 
+/* Called when the parent port needs more data in its buffer, i.e. 
+   in conv_info.outbuf. */
 static int conv_input_filler(char *buf, int len, void *data)
 {
     conv_info *info = (conv_info*)data;
-    int inleft, outleft, converted;
+    size_t inleft, outleft, converted;
     const char *inbuf = info->inbuf;
     char *outbuf = info->outbuf;
 
+    /* fill the input buffer */
+    
     inleft = info->inptr - info->inbuf;
     outleft = info->outptr - info->outbuf;
     converted = iconv(info->handle, &inbuf, &inleft, &outbuf, &outleft);
+    
+    if (converted == (size_t)-1) {
+        if (errno == EINVAL) {
+            /* conversion stopped due to an incomplete character at the
+               end of the input buffer. */
+        } else if (errno == E2BIG) {
+            /* output buffer is full. */
+        } else {
+            /* it's likely that input contains invalid sequence. */
+        }
+    }
     return 0;
 }
 
@@ -81,7 +104,6 @@ ScmObj Scm_MakeInputConversionPort(ScmPort *fromPort,
                                 (void*)cinfo);
 }
 
-
 /* Initialization */
 extern void Scm_Init_convlib(ScmModule *module);
 
@@ -89,3 +111,24 @@ void Scm_Init_charconv(void)
 {
     Scm_Init_convlib(Scm_GaucheModule());
 }
+
+#ifndef HAVE_ICONV_H
+/* substitution of iconv for the platforms where it is not available */
+
+/* for now, we only have dummy functions */
+iconv_t iconv_open(const char *tocode, const char *fromcode)
+{
+    return (iconv_t)-1;
+}
+
+int iconv_close(iconv_t handle)
+{
+    return 0;
+}
+
+int iconv(iconv_t handle, const char **inbuf, size_t *inroom, char **outbuf, size_t *outroom)
+{
+    return 0;
+}
+#endif /* !HAVE_ICONV_H */
+
