bf9a1c5449c28598e01b19214ab7a06e4e565ccf
Shiro Kawai <shiro@acm.org>
2001-06-24 23:06:40 +0000

    added port->* procedures to be autoloaded from gauche/with.scm
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1715 c205566b-fb4e-0410-ab5c-8f0660156277

--- src/gauche-init.scm
+++ src/gauche-init.scm
@@ -12,7 +12,7 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: gauche-init.scm,v 1.33 2001-06-19 19:46:17 shirok Exp $
+;;;  $Id: gauche-init.scm,v 1.34 2001-06-24 23:06:40 shirok Exp $
 ;;;
 
 (select-module gauche)
@@ -77,8 +77,9 @@
 
 (with-module gauche
   (autoload "gauche/with" with-output-to-string call-with-output-string
-                          with-input-from-string call-with-input-string))
-
+                          with-input-from-string call-with-input-string
+                          port->string port->list
+                          port->string-list port->sexp-list))
 (with-module gauche
   (autoload "gauche/numerical" gcd lcm numerator denominator
                                make-polar real-part imag-part
