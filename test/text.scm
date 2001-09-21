2a9c1de99e32b1ed64421ae4e8407e25837d07f0
Shiro Kawai <shiro@acm.org>
2001-09-21 20:16:05 +0000

    added more tests to tr
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1950 c205566b-fb4e-0410-ab5c-8f0660156277

--- test/text.scm
+++ test/text.scm
@@ -56,5 +56,22 @@
       (lambda () (string-tr "Hello, World!" "A-Za-z" "a-z?*0")))
 (test "repeat" "h???!, w!!??!"
       (lambda () (string-tr "Hello, World!" "A-Za-z" "a-z?*13!*13")))
+(test "repeat - error" #t
+      (lambda ()
+        (with-error-handler
+         (lambda (e) #t)
+         (lambda () (string-tr "Hello, World!" "A*10" "a-z?*13!*13")))))
+(test "delete" ", !"
+      (lambda ()
+        (string-tr "Hello, World!" "A-Za-z" "" :delete #t)))
+(test "delete" "H, W!"
+      (lambda ()
+        (string-tr "Hello, World!" "a-z" "" :delete #t)))
+(test "delete" "h, w!"
+      (lambda ()
+        (string-tr "Hello, World!" "A-Za-z" "a-z" :delete #t)))
+'(test "complement" "Hello??World?"
+      (lambda ()
+        (string-tr "Hello, World!" "A-Za-z" "?*" :complement #t)))
 
 (test-end)
