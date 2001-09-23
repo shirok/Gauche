75fab5bc52488f00902b27f6edf571025afde812
Shiro Kawai <shiro@acm.org>
2001-09-23 05:05:14 +0000

    adapted to string-scan
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@1961 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/srfi-13/searcher.scm
+++ lib/srfi-13/searcher.scm
@@ -12,7 +12,7 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: searcher.scm,v 1.3 2001-06-29 20:32:47 shirok Exp $
+;;;  $Id: searcher.scm,v 1.4 2001-09-23 05:05:14 shirok Exp $
 ;;;
 
 ;; Say `(use srfi-13)' and this file will be autoloaded on demand.
@@ -69,18 +69,15 @@
   (check-arg string? s1)
   (check-arg string? s2)
   (let-optionals* args (start1 end1 start2 end2)
-    (let ((%string-contains (with-module gauche string-contains))
-          (str1 (%maybe-substring s1 start1 end1))
+    (let ((str1 (%maybe-substring s1 start1 end1))
           (str2 (%maybe-substring s2 start2 end2)))
-      (%string-contains str1 str2))))
+      (string-scan str1 str2))))
 
 ;; not tuned (maybe to be moved to native)
 (define (string-contains-ci s1 s2 . args)
   (check-arg string? s1)
   (check-arg string? s2)
   (let-optionals* args (start1 end1 start2 end2)
-    (let ((%string-contains (with-module gauche string-contains))
-          (str1 (string-upcase (%maybe-substring s1 start1 end1)))
+    (let ((str1 (string-upcase (%maybe-substring s1 start1 end1)))
           (str2 (string-upcase (%maybe-substring s2 start2 end2))))
-      (%string-contains str1 str2))))
-
+      (string-scan str1 str2))))
