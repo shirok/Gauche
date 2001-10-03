ed16585cbace9654ce4122123e4891ae772e31ba
Shiro Kawai <shiro@acm.org>
2001-10-03 09:17:38 +0000

    unwrap-syntax is moved to common-macros
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2037 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/gauche/macroutil.scm
+++ lib/gauche/macroutil.scm
@@ -2,25 +2,15 @@
 ;;; Handy macro expander
 ;;;
 
-;;; $Id: macroutil.scm,v 1.1 2001-03-24 09:50:16 shiro Exp $
+;;; $Id: macroutil.scm,v 1.2 2001-10-03 09:17:38 shirok Exp $
 
 ;;
 ;; These routines are handy to check macro expansion process interactively.
 ;;
 
 (define-module gauche.macroutil
-  (export unwrap-syntax xmac xmac1))
+  (export xmac xmac1))
 (select-module gauche.macroutil)
-
-;; strip off syntactic information from identifiers in the macro output.
-(define (unwrap-syntax form)
-  (cond
-   ((identifier? form) (identifier->symbol form))
-   ((pair? form) (cons (unwrap-syntax (car form))
-                       (unwrap-syntax (cdr form))))
-   ((vector? form)
-    (list->vector (map unwrap-syntax (vector->list form))))
-   (else form)))
 
 (define-syntax xmac
   (syntax-rules ()
