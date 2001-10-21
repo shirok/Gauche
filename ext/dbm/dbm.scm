7f867a62c62bc7830196c6ebfa7b88622f8417ec
Shiro Kawai <shiro@acm.org>
2001-10-21 19:33:19 +0000

    small fixes
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2159 c205566b-fb4e-0410-ab5c-8f0660156277

--- ext/dbm/dbm.scm
+++ ext/dbm/dbm.scm
@@ -12,81 +12,54 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: dbm.scm,v 1.1 2001-10-20 21:18:36 shirok Exp $
+;;;  $Id: dbm.scm,v 1.2 2001-10-21 19:33:19 shirok Exp $
 ;;;
 
 (define-module dbm
   (export <dbm>
-          dbm-open
-          dbm-close
-          dbm-closed?
-          dbm-get
-          dbm-put!
-          dbm-delete!
-          dbm-exists?
-          dbm-for-each
-          dbm-map
-          dbm-error))
+          dbm-open    dbm-close     dbm-closed?    dbm-get
+          dbm-put!    dbm-delete!   dbm-exists?
+          dbm-for-each  dbm-map     dbm-error))
 (select-module dbm)
 
-(define-class <DBM> ()
+(define-class <dbm> ()
   ((path       :init-keyword :path)
    (rw-mode    :init-keyword :rw-mode    :initform :write)
    (file-mode  :init-keyword :file-mode  :initform #o664)
    (serializer :init-keyword :serializer :initform #f)
-   ;; internal slots
-   ->string
-   string->
+   (deserializer :init-keyword :deserializer :initform #f)
    ))
 
-(define-method initialize ((self <dbm>) initargs)
-  (next-method)
-  (let ((ser (slot-ref self 'serializer)))
-    (slot-set! self '->string
-               (cond ((eq? ser #f) (lambda (o) o))
-                     ((eq? ser #t)
-                      (lambda (o)
-                        (with-output-to-string (lambda () (write* o)))))
-                     (else
-                      (lambda (o)
-                        (write-to-string-with-serializer ser o :preserve-equality #t)))))
-    (slot-set! self 'string->
-               (cond ((eq? ser #f) (lambda (o) o))
-                     ((eq? ser #t)
-                      (lambda (o)
-                        (with-input-from-string o read)))
-                     (else
-                      (lambda (o)
-                        (read-from-string-with-serializer ser o :preserve-equality #t)))))
-    ))
+(define-method dbm-open ((class <class>) . initargs)
+  (dbm-open (apply make class initargs)))
 
 ;;
 ;; Method prototypes.  Actual method should be defined in subclasses.
 ;;
 
-(define-method DBM-PUT! ((dbm <dbm>) key value)
+(define-method dbm-put! ((dbm <dbm>) key value)
   (when (dbm-closed? dbm) (errorf "dbm-put!: dbm already closed: ~s" dbm)))
 
-(define-method DBM-GET ((dbm <dbm>) key . args)
+(define-method dbm-get ((dbm <dbm>) key . args)
   (when (dbm-closed? dbm) (errorf "dbm-get: dbm already closed: ~s" dbm)))
 
-(define-method DBM-EXISTS? ((dbm <dbm>) key)
+(define-method dbm-exists? ((dbm <dbm>) key)
   (when (dbm-closed? dbm) (errorf "dbm-exists?: dbm already closed: ~s" dbm)))
 
-(define-method DBM-DELETE! ((dbm <dbm>) key)
+(define-method dbm-delete! ((dbm <dbm>) key)
   (when (dbm-closed? dbm) (errorf "dbm-delete!: dbm already closed: ~s" dbm)))
 
-(define-method DBM-FOR-EACH ((dbm <dbm>) proc)
+(define-method dbm-for-each ((dbm <dbm>) proc)
   (when (dbm-closed? dbm) (errorf "dbm-for-each: dbm already closed: ~s" dbm))
   (unless (procedure? proc) (errorf "dbm-for-each: bad procedure: ~s" proc)))
 
-(define-method DBM-MAP ((dbm <dbm>) proc)
+(define-method dbm-map ((dbm <dbm>) proc)
   (when (dbm-closed? dbm) (errorf "dbm-map: dbm already closed: ~s" dbm))
   (unless (procedure? proc) (errorf "dbm-map: bad procedure: ~s" proc)))
 
-(define-method DBM-CLOSE ((dbm <dbm>)) #f)
+(define-method dbm-close ((dbm <dbm>)) #f)
 
-(define-method DBM-CLOSED? ((dbm <dbm>)) #f)
+(define-method dbm-closed? ((dbm <dbm>)) #f)
 
 
 (provide "dbm")
