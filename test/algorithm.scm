12dc89e8ac56f7b6b10347ce8018bde32da1c1f4
Shiro Kawai <shiro@acm.org>
2001-10-23 19:50:47 +0000

    added
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2171 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ test/algorithm.scm
@@ -0,0 +1,62 @@
+;;
+;; test argorithm modules
+;;
+
+(use gauche.test)
+(test-start "algorithm.*")
+
+(use srfi-1)
+
+;;-----------------------------------------------
+(test-section "algorithm.isomorph")
+(use algorithm.isomorph)
+
+(define (make-data type)
+  (let* ((z (vector #f #f #f))
+         (x (circular-list "a" 'b 4 9845938427094857239485 #\z 8+5i z))
+         (y (circular-list "a" 'b 4 9845938427094857239485 #\z 8+5i z)))
+    (vector-set! z 0 x)
+    (vector-set! z 1 y)
+    (if type (vector-set! z 2 x) (vector-set! z 2 y))
+    z))
+
+(test "isomorphic?" #t
+      (lambda () (isomorphic? (make-data #f) (make-data #f))))
+(test "isomorphic?" #f
+      (lambda () (isomorphic? (make-data #t) (make-data #f))))
+
+
+;;-----------------------------------------------
+(test-section "algorithm.toposort")
+(use algorithm.toposort)
+
+
+;; using famous socks example (Corman)
+(test "topological-sort"
+      '(socks undershorts watch shirt tie pants belt jacket shoes)
+      (lambda ()
+        (topological-sort '((shirt tie belt)
+                            (tie jacket)
+                            (belt jacket)
+                            (watch)
+                            (pants shoes belt)
+                            (undershorts pants shoes)
+                            (socks shoes))
+                          eq?)))
+
+(test "topological-sort"
+      '("socks" "undershorts" "watch" "shirt" "tie" "pants" "belt" "jacket" "shoes")
+      (lambda ()
+        (topological-sort '(("shirt" "tie" "belt")
+                            ("tie" "jacket")
+                            ("belt" "jacket")
+                            ("watch")
+                            ("pants" "shoes" "belt")
+                            ("undershorts" "pants" "shoes")
+                            ("socks" "shoes"))
+                          equal?)))
+
+(test-end)
+
+
+
