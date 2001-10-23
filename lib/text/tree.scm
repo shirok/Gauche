f91af0ee738ea3894db1a4fd1be1a2d46abf9a14
Shiro Kawai <shiro@acm.org>
2001-10-23 10:14:44 +0000

    added
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2168 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ lib/text/tree.scm
@@ -0,0 +1,36 @@
+;;;
+;;; test/tree.scm - lightweight text generation
+;;;
+;;;  Copyright(C) 2001 by Shiro Kawai (shiro@acm.org)
+;;;
+;;;  Permission to use, copy, modify, distribute this software and
+;;;  accompanying documentation for any purpose is hereby granted,
+;;;  provided that existing copyright notices are retained in all
+;;;  copies and that this notice is included verbatim in all
+;;;  distributions.
+;;;  This software is provided as is, without express or implied
+;;;  warranty.  In no circumstances the author(s) shall be liable
+;;;  for any damages arising out of the use of this software.
+;;;
+;;;  $Id: tree.scm,v 1.1 2001-10-23 10:14:44 shirok Exp $
+;;;
+
+(define-module text.tree
+  (export write-tree
+          tree->string)
+  )
+(select-module text.tree)
+
+(define-method write-tree (tree)
+  (write-tree tree (current-output-port)))
+
+(define-mtehod write-tree ((tree <list>) out)
+  (for-each (lambda (x) (write-tree x out)) tree))
+
+(define-method write-tree ((tree <top>) out)
+  (display tree out))
+
+(define-method tree->string (tree)
+  (with-output-to-string (lambda () (write-tree tree))))
+
+(provide "text/tree")
