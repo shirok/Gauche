8a3476a24551b470ce1ad6be484910b1591a91d6
Shiro Kawai <shiro@acm.org>
2001-11-02 10:46:56 +0000

    added test for queue
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2236 c205566b-fb4e-0410-ab5c-8f0660156277

--- test/util.scm
+++ test/util.scm
@@ -25,6 +25,53 @@
 (test "isomorphic?" #f
       (lambda () (isomorphic? (make-data #t) (make-data #f))))
 
+;;-----------------------------------------------
+(test-section "util.queue")
+(use util.queue)
+
+(test "queue?" #f (lambda () (queue? (cons 'a 'b))))
+(test "queue?" #f (lambda () (queue? 3)))
+(test "queue?" #f (lambda () (queue? '())))
+(define q (make-queue))
+
+(test "queue?" #t (lambda () (queue? q)))
+(test "enqueue!" #t (lambda () (enqueue! q 'a) (queue? q)))
+(test "enqueue!" #t (lambda () (enqueue! q 'b) (queue? q)))
+(test "enqueue!" #t (lambda () (enqueue! q 'c) (queue? q)))
+
+(test "queue-front" 'a (lambda () (queue-front q)))
+(test "queue-rear" 'c (lambda () (queue-rear q)))
+
+(test "enqueue!" '(a f)
+      (lambda ()
+        (enqueue! q 'd 'e 'f)
+        (list (queue-front q) (queue-rear q))))
+
+(test "dequeue!" 'a (lambda () (dequeue! q)))
+(test "dequeue!" 'b (lambda () (dequeue! q)))
+(test "queue-empty?" #f (lambda () (queue-empty? q)))
+(test "dequeue!" 'c (lambda () (dequeue! q)))
+(test "dequeue!" 'd (lambda () (dequeue! q)))
+(test "dequeue!" 'e (lambda () (dequeue! q)))
+(test "dequeue!" 'f (lambda () (dequeue! q)))
+(test "queue-empty?" #t (lambda () (queue-empty? q)))
+
+(test "queue-push!" '(c a)
+      (lambda ()
+        (queue-push! q 'a) (queue-push! q 'b) (queue-push! q 'c)
+        (list (queue-front q) (queue-rear q))))
+(test "queue-push!" '(f a)
+      (lambda ()
+        (queue-push! q 'd 'e 'f)
+        (list (queue-front q) (queue-rear q))))
+(test "queue-pop!" 'f (lambda () (queue-pop! q)))
+(test "queue-pop!" 'e (lambda () (queue-pop! q)))
+(test "queue-empty?" #f (lambda () (queue-empty? q)))
+(test "queue-pop!" 'd (lambda () (queue-pop! q)))
+(test "queue-pop!" 'c (lambda () (queue-pop! q)))
+(test "queue-pop!" 'b (lambda () (queue-pop! q)))
+(test "queue-pop!" 'a (lambda () (queue-pop! q)))
+(test "queue-empty?" #t (lambda () (queue-empty? q)))
 
 ;;-----------------------------------------------
 (test-section "util.toposort")
