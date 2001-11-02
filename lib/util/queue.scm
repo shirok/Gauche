860b6d28843ff7cda1f10557149f3478fef20b4b
Shiro Kawai <shiro@acm.org>
2001-11-02 10:31:03 +0000

    allow queue-push! and enqueue! to have optional args
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2234 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/util/queue.scm
+++ lib/util/queue.scm
@@ -5,14 +5,14 @@
 ;;;  Public Domain..  I guess lots of Scheme programmers have already
 ;;;  written similar code.
 ;;;
-;;;  $Id: queue.scm,v 1.1 2001-11-02 10:23:55 shirok Exp $
+;;;  $Id: queue.scm,v 1.2 2001-11-02 10:31:03 shirok Exp $
 ;;;
 
 ;; This queue implementation is tuned for speed.  A queue is simply
-;; a pair that hols the head and tail of queue, rather defining a
+;; a pair that holds the head and tail of queue, rather defining a
 ;; special structure.   It bypasses consistency checks as well.
-;;;So it is unsafe, but faster.
-;; The API is compatible with SLIB's queue module.
+;; So it is unsafe, but faster.
+;; The API is upper compatible with SLIB's queue module.
 
 (define-module util.queue
   )
@@ -20,6 +20,7 @@
 
 (define (make-queue) (cons #f #f))
 
+;; this one we does (expensive) check.
 (define (queue? obj)
   (and (pair? obj)
        (or (and (not (car obj)) (not (cdr obj)))
@@ -32,15 +33,21 @@
 
 (define (queue-empty? obj) (%empty? obj))
 
-(define (queue-push! q obj)
+(define (queue-push! q obj . more-objs)
   (if (%empty? q)
       (let ((cell (list obj)))
         (set-car! q cell)
         (set-cdr! q cell))
       (set-car! q (cons obj (car q))))
+  (unless (null? more-objs)
+    (let loop ((head (car q))
+               (objs more-objs))
+      (if (null? objs)
+          (set-car! q head)
+          (loop (cons (car objs) head) (cdr objs)))))
   q)
 
-(define (enqueue! q obj)
+(define (enqueue! q obj . more-objs)
   (if (%empty? q)
       (let ((cell (list obj)))
         (set-car! q cell)
@@ -48,6 +55,13 @@
       (begin
         (set-cdr! (cdr q) (list obj))
         (set-cdr! q (cddr q))))
+  (unless (null? more-objs)
+    (let loop ((tail (cdr q))
+               (objs more-objs))
+      (if (null? objs)
+          (set-cdr! q tail)
+          (begin (set-cdr! tail (list (car objs)))
+                 (loop (cdr tail) (cdr more-objs))))))
   q)
 
 (define (queue-front q)
