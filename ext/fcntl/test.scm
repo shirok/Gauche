936204ae8e0e93024f7e26bbbfe2f8440b8fcc95
Shiro Kawai <shiro@acm.org>
2001-11-06 09:36:17 +0000

    added real tests
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2248 c205566b-fb4e-0410-ab5c-8f0660156277

--- ext/fcntl/test.scm
+++ ext/fcntl/test.scm
@@ -7,4 +7,35 @@
 (load "fcntl")
 (import gauche.fcntl)
 
+;; It is difficult to compose a full test that works on every situation.
+;; Here I provide tests for some common features.
 
+(sys-unlink "test.o")
+
+(test "F_GETFL" |O_WRONLY|
+      (lambda ()
+        (call-with-output-file "test.o"
+          (lambda (p) (logand (sys-fcntl p |F_GETFL|) |O_ACCMODE|)))))
+
+(test "F_GETFL" |O_RDONLY|
+      (lambda ()
+        (call-with-input-file "test.o"
+          (lambda (p) (logand (sys-fcntl p |F_GETFL|) |O_ACCMODE|)))))
+
+(test "F_GETFL" #t
+      (lambda ()
+        (call-with-input-file "test.o"
+          (lambda (p)
+            (zero? (logand (sys-fcntl p |F_GETFL|) |O_APPEND|))))))
+
+(test "F_GETFL" #f
+      (lambda ()
+        (call-with-output-file "test.o"
+          (lambda (p) (zero? (logand (sys-fcntl p |F_GETFL|) |O_APPEND|)))
+          :if-exists :append)))
+
+(sys-unlink "test.o")
+
+;; TODO: test lock
+
+(test-end)
