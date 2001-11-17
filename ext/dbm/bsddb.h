11fe12f9c3594b400f7dd7e93a74eca4d94a8fd1
Shiro Kawai <shiro@acm.org>
2001-11-17 23:04:36 +0000

    snapshot
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2323 c205566b-fb4e-0410-ab5c-8f0660156277

--- ext/dbm/bsddb.h
+++ ext/dbm/bsddb.h
@@ -12,7 +12,7 @@
  *  warranty.  In no circumstances the author(s) shall be liable
  *  for any damages arising out of the use of this software.
  *
- *  $Id: bsddb.h,v 1.1 2001-11-17 11:16:28 shirok Exp $
+ *  $Id: bsddb.h,v 1.2 2001-11-17 23:04:36 shirok Exp $
  */
 
 #ifndef GAUCHE_BSDDB_H
@@ -24,20 +24,14 @@
 
 typedef struct ScmBsdDbRec {
     SCM_HEADER;
-    const char *file;
-    int flags;
-    int mode;
-    DBTYPE type;
+    ScmObj name;
     DB *db;
 } ScmBsdDb;
 
 extern ScmClass Scm_BsdDbClass;
 #define SCM_CLASS_BSD_DB     (&Scm_BsdDbClass)
 #define SCM_BSD_DB(obj)      ((ScmBsdDb*)obj)
-#define SCM_BSD_DB_T(obj)    Scm_TypeP(obj, SCM_CLASS_BSD_DB)
-
+#define SCM_BSD_DB_P(obj)    SCM_XTYPEP(obj, SCM_CLASS_BSD_DB)
 #define SCM_BSD_DB_TYPE(obj) SCM_BSD_DB(obj)->type
-
-ScmObj Scm_BsdDbType(ScmBsdDb *db);
 
 #endif /*GAUCHE_BSDDB_H*/
