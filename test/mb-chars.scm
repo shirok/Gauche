54a0aa4b54499320afc8775b58971b583195bbce
Shiro Kawai <shiro@acm.org>
2001-12-23 03:40:23 +0000

    added UTF-8 test
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2462 c205566b-fb4e-0410-ab5c-8f0660156277

--- test/mb-chars.scm
+++ test/mb-chars.scm
@@ -5,5 +5,6 @@
 (case (gauche-character-encoding)
   ((euc-jp) (load "../test/euc-jp"))
   ((sjis)   (load "../test/sjis"))
+  ((utf-8)  (load "../test/utf-8"))
   (else (format #t "No test provided for the character encoding '~s"
                 (gauche-character-encoding))))
