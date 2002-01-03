d76810e5a8af88550d837fe74f06fb05233b310f
Shiro Kawai <shiro@acm.org>
2002-01-03 08:07:44 +0000

    organized tests
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2467 c205566b-fb4e-0410-ab5c-8f0660156277

--- test/mb-chars.scm
+++ test/mb-chars.scm
@@ -2,9 +2,11 @@
 ;; test for multibyte characters
 ;;
 
+(add-load-path "../test")
+
 (case (gauche-character-encoding)
-  ((euc-jp) (load "../test/euc-jp"))
-  ((sjis)   (load "../test/sjis"))
-  ((utf-8)  (load "../test/utf-8"))
-  (else (format #t "No test provided for the character encoding '~s"
+  ((euc-jp) (load "euc-jp"))
+  ((sjis)   (load "sjis"))
+  ((utf-8)  (load "utf-8"))
+  (else (format #t "No test provided for the character encoding ~s"
                 (gauche-character-encoding))))
