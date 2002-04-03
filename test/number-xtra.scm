b1a36dece0a826cf8c7412158fdbc4977dd1de5d
Shiro Kawai <shiro@acm.org>
2002-04-03 22:30:36 +0000

    added
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2715 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ test/number-xtra.scm
@@ -0,0 +1,40 @@
+;;
+;; Extra test for number reading/writing
+;;
+
+;; Generate test file.
+(define (generate-fixnum-file file num)
+  (with-output-to-file file
+    (lambda ()
+      (dotimes (n num)
+        (display (modulo (sys-random) #x20000000))
+        (newline)))))
+
+(define (generate-bignum-file file num)
+  (with-output-to-file file
+    (lambda ()
+      (dotimes (n num)
+        (let ((k (+ (* RAND_MAX RAND_MAX (sys-random))
+                    (* RAND_MAX (sys-random))
+                    (sys-random))))
+          (display k) (newline))))))
+
+(define (generate-flonum-file file num)
+  (with-output-to-file file
+    (lambda ()
+      (dotimes (n num)
+        (let* ((m (/ (sys-random) RAND_MAX))
+               (e (inexact->exact (floor (* (/ (sys-random) RAND_MAX) 1023))))
+               (s (if (odd? (sys-random)) -1 1))
+               (f (ldexp m (* s e))))
+          (display f) (newline))))))
+
+;; Benchmarking readers
+(define (test-reader file repeat)
+  (let ((input (call-with-input-file file port->string-list)))
+    (receive (sec0 usec0) (sys-gettimeofday)
+      (dotimes (n repeat)
+        (for-each string->number input))
+      (receive (sec1 usec1) (sys-gettimeofday)
+        (- (+ (* sec1 1000000) usec1)
+           (+ (* sec0 1000000) usec0))))))
