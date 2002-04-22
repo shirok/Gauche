0831a06e3d55ebf261755e711248cde42ec029e3
Shiro Kawai <shiro@acm.org>
2002-04-22 03:35:14 +0000

    added test for www.*
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2786 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ test/www.scm
@@ -0,0 +1,80 @@
+;;
+;; test www.* modules
+;;
+
+(use gauche.test)
+(use gauche.parameter)
+(test-start "www.* modules")
+
+;;------------------------------------------------
+(test-section "www.cgi")
+(use www.cgi)
+
+(define params #f)
+(define qs1 "a=foo+bar&boo=baz=doo&z%3Dz=%21%26&a=+%20&#=#&z=z=8&r&r=2")
+(define qr1 '(("boo" "baz=doo") ("z=z" "!&") ("a" "foo bar" "  ") ("#" "#") ("z" "z=8") ("r" #t "2")))
+
+(define qs2 "zz=aa&aa=zz")
+(define qr2 '(("zz" "aa") ("aa" "zz")))
+
+(test "cgi-parse-parameters" qr1
+      (lambda ()
+        (set! params (cgi-parse-parameters :query-string qs1))
+        params))
+
+(test "cgi-get-parameter" "foo bar"
+      (lambda () (cgi-get-parameter "a" params)))
+(test "cgi-get-parameter" '("foo bar" "  ")
+      (lambda () (cgi-get-parameter "a" params :list #t)))
+(test "cgi-get-parameter" #t
+      (lambda () (cgi-get-parameter "r" params)))
+(test "cgi-get-parameter" '(#t "2")
+      (lambda () (cgi-get-parameter "r" params :list #t)))
+(test "cgi-get-parameter" '("baz=doo")
+      (lambda () (cgi-get-parameter "boo" params :list #t)))
+(test "cgi-get-parameter" 'none
+      (lambda () (cgi-get-parameter "booz" params :default 'none)))
+(test "cgi-get-parameter" #f
+      (lambda () (cgi-get-parameter "booz" params)))
+(test "cgi-get-parameter" '()
+      (lambda () (cgi-get-parameter "booz" params :list #t)))
+(test "cgi-get-parameter" '(0 2)
+      (lambda () (cgi-get-parameter "r" params :convert x->integer :list #t)))
+
+(test "cgi-get-query (GET)" qr1
+      (lambda ()
+        (parameterize ((cgi-metavariables `(("REQUEST_METHOD" "GET")
+                                            ("QUERY_STRING" ,qs1))))
+          (with-input-from-string qs2
+            cgi-parse-parameters))))
+(test "cgi-get-query (HEAD)" qr1
+      (lambda ()
+        (parameterize ((cgi-metavariables `(("REQUEST_METHOD" "HEAD")
+                                            ("QUERY_STRING" ,qs1))))
+          (with-input-from-string qs2
+            cgi-parse-parameters))))
+(test "cgi-get-query (POST)" qr2
+      (lambda ()
+        (parameterize ((cgi-metavariables `(("REQUEST_METHOD" "POST")
+                                            ("QUERY_STRING" ,qs1))))
+          (with-input-from-string qs2
+            cgi-parse-parameters))))
+(test "cgi-get-query (POST)" qr2
+      (lambda ()
+        (parameterize ((cgi-metavariables `(("REQUEST_METHOD" "POST")
+                                            ("CONTENT_LENGTH" ,(string-length qs2)))))
+          (with-input-from-string qs2
+            cgi-parse-parameters))))
+(test "cgi-get-query (POST)" '(("zz" "aa"))
+      (lambda ()
+        (parameterize ((cgi-metavariables `(("REQUEST_METHOD" "POST")
+                                            ("CONTENT_LENGTH" 5))))
+          (with-input-from-string qs2
+            cgi-parse-parameters))))
+      
+
+;; TODO: more to come
+
+(test-end)
+
+
