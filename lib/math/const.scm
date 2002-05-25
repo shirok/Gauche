fca19adb993093dfafb07f45dd30ea7f4dc773fb
Shiro Kawai <shiro@acm.org>
2002-05-25 00:32:03 +0000

    added pi/180 and 180/pi
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@2959 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/math/const.scm
+++ lib/math/const.scm
@@ -4,17 +4,19 @@
 ;;; public domain
 ;;;
 
-;; $Id: const.scm,v 1.1 2002-05-24 21:39:07 shirok Exp $
+;; $Id: const.scm,v 1.2 2002-05-25 00:32:03 shirok Exp $
 
 (define-module math.const
   (export pi e 1/pi pi/2 pi/4 2pi))
 
 (select-module math.const)
 
-(define-constant pi   3.141592653589793)
-(define-constant pi/2 1.5707963267948966)
-(define-constant pi/4 0.7853981633974483)
-(define-constant 1/pi 0.3183098861837907)
-(define-constant e    2.718281828459045)
+(define-constant pi     3.141592653589793)
+(define-constant pi/2   1.5707963267948966)
+(define-constant pi/4   0.7853981633974483)
+(define-constant pi/180 0.017453292519943295)
+(define-constant 1/pi   0.3183098861837907)
+(define-constant 180/pi 57.29577951308232)
+(define-constant e      2.718281828459045)
 
 (provide "math/const")
