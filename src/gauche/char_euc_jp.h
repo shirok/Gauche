47c057aea0be8a9cafd287db36a783f6d30fab89
Shiro Kawai <shiro@acm.org>
2002-06-12 12:01:24 +0000

    adapted to JISX0212 and JISX0213-plane2
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@3035 c205566b-fb4e-0410-ab5c-8f0660156277

--- src/gauche/char_euc_jp.h
+++ src/gauche/char_euc_jp.h
@@ -12,7 +12,7 @@
  *  warranty.  In no circumstances the author(s) shall be liable
  *  for any damages arising out of the use of this software.
  *
- *  $Id: char_euc_jp.h,v 1.10 2001-05-28 11:55:20 shirok Exp $
+ *  $Id: char_euc_jp.h,v 1.11 2002-06-12 12:01:24 shirok Exp $
  */
 
 #ifndef SCM_CHAR_ENCODING_BODY
@@ -31,17 +31,18 @@
  * two-byte character, it returns 1.   It may return -1 if
  * the given byte can't be a valid first byte of multibyte characters.
  */
-#define SCM_CHAR_NFOLLOWS(ch) \
-    ((((unsigned char)(ch)) >= 0x80) ? 1 : 0)
+#define SCM_CHAR_NFOLLOWS(ch)                           \
+    ((((unsigned char)(ch)) >= 0x80) ?                  \
+     ((((unsigned char)(ch)) == 0x8f) ? 2 : 1) : 0)
 
 /* Given wide character CH, returns # of bytes used when CH is
  * encoded in multibyte string.
  */
 #define SCM_CHAR_NBYTES(ch) \
-    (((ch) >= 0x80) ? 2 : 1)
+    (((ch) >= 0x80) ? (((ch) >= 0x10000)? 3 : 2) : 1)
 
 /* Maximun # of multibyte character */
-#define SCM_CHAR_MAX_BYTES     2
+#define SCM_CHAR_MAX_BYTES     3
 
 /* From a multibyte string pointed by const char *cp, extract a character
  * and store it in ScmChar ch.  If cp doesn't point to valid multibyte
@@ -50,7 +51,13 @@
 #define SCM_CHAR_GET(cp, ch)                                    \
     do {                                                        \
         if (((ch) = (unsigned char)*(cp)) >= 0x80) {            \
-            (ch) = ((ch) << 8) + (unsigned char)*(cp+1);        \
+            if ((ch) == 0x8f) {                                 \
+                 (ch) = ((ch) << 16)                            \
+                     + ((unsigned char)(cp)[1] << 8)            \
+                     + (unsigned char)(cp)[2];                  \
+            } else {                                            \
+                (ch) = ((ch) << 8) + (unsigned char)(cp)[1];    \
+            }                                                   \
         }                                                       \
     } while (0)
 
@@ -60,9 +67,15 @@
  */
 #define SCM_CHAR_PUT(cp, ch)                    \
     do {                                        \
-        if (ch > 0xff) {                        \
-            (cp)[0] = (ch >> 8) & 0xff;         \
-            (cp)[1] = ch & 0xff;                \
+        if ((ch) > 0xff) {                      \
+            if ((ch) > 0xffff) {                \
+                (cp)[0] = ((ch) >> 16) & 0xff;  \
+                (cp)[1] = ((ch) >> 8) & 0xff;   \
+                (cp)[2] = (ch) & 0xff;          \
+            } else {                            \
+                (cp)[0] = (ch >> 8) & 0xff;     \
+                (cp)[1] = ch & 0xff;            \
+            }                                   \
         } else {                                \
             (cp)[0] = ch & 0xff;                \
         }                                       \
@@ -78,6 +91,10 @@
     do {                                                        \
         switch ((cp) - (start)) {                               \
         default:                                                \
+            (result) = (cp) - 3;                                \
+            if (SCM_CHAR_NFOLLOWS(*(result)) == 2) break;       \
+            /* FALLTHROUGH */                                   \
+        case 2:                                                 \
             (result) = (cp) - 2;                                \
             if (SCM_CHAR_NFOLLOWS(*(result)) == 1) break;       \
             /* FALLTHROUGH */                                   \
