b11d6379aaf166a9ecd5c7fb6bcd9c73170fb5b3
Shiro Kawai <shiro@acm.org>
2002-06-22 08:39:10 +0000

    further addition of arith.h macros and tests
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@3091 c205566b-fb4e-0410-ab5c-8f0660156277

--- src/gauche/arith.h
+++ src/gauche/arith.h
@@ -12,7 +12,7 @@
  *  warranty.  In no circumstances the author(s) shall be liable
  *  for any damages arising out of the use of this software.
  *
- *  $Id: arith.h,v 1.1 2002-06-18 06:06:16 shirok Exp $
+ *  $Id: arith.h,v 1.2 2002-06-22 08:39:10 shirok Exp $
  */
 
 #ifndef GAUCHE_ARITH_H
@@ -21,6 +21,10 @@
 /*
  * This file defines some basic integer arithmetic operations.
  * Most modern CPUs have efficient way to perform those operations.
+ *
+ * Not intended for global use; i.e. only Gauche internal code uses these.
+ * Only the features needed by Gauche's guts are implemented.  You see
+ * the provided macros are not symmetric.
  */
 
 /* some useful constants */
@@ -42,39 +46,121 @@
 #define HI(word)           (((word) >> HALF_BITS)&LOMASK)
 
 /*-----------------------------------------------------------------
- * UADD(r, c, x, y)
- *  u_long : r, x, c, y;
+ * UADD(r, c, x, y)      unsigned word add with carry
+ *  u_long : r, c, x, y;
  *  r <- x + y + c  mod wordsize
  *  c <- 1 if carry, 0 otherwise
  */
 
+#ifndef UADD
 /* Portable version */
 #define UADD(r, c, x, y)                                        \
   do {                                                          \
     (r) = (x) + (y) + (c);                                      \
     (c) = ((r)<(x) || ((r)==(x) && ((y)>0||(c)>0)))? 1 : 0;     \
   } while (0)
+#endif /*UADD*/
 
 /*-----------------------------------------------------------------
- * USUB(r, c, x, y)
+ * UADDOV(r, c, x, y)    unsigned word add with overflow check
+ *  u_long : r, c, x, y;
+ *  if x + y overflows, c = 1
+ *  else r <- x + y, c = 0
+ */
+
+#ifndef UADDOV
+/* Portable version */
+#define UADDOV(r, c, x, y)                      \
+  do {                                          \
+    (r) = (x) + (y);                            \
+    (c) = (r) < (x);                            \
+  } while (0)
+#endif /*UADDOV*/
+
+/*-----------------------------------------------------------------
+ * SADDOV(r, c, x, y)     signed word addition with overflow check
+ *  long : r, c, x, y;
+ *  if x + y overflows, c = 1,
+ *  else r <- x + y, c = 0
+ */
+
+#ifndef SADDOV
+/* Portable version */
+#define SADDOV(r, c, x, y)                      \
+  do {                                          \
+    (r) = (x) + (y);                            \
+    if ((x) >= 0) {                             \
+      if ((y) >= 0 && (r) < 0) (c) = 1;         \
+      else (c) = 0;                             \
+    } else {                                    \
+      if ((y) < 0 && (r) >= 0) (c) = 1;         \
+      else (c) = 0;                             \
+    }                                           \
+  } while (0)
+#endif /*SADDOV*/
+
+/*-----------------------------------------------------------------
+ * USUB(r, c, x, y)        unsigned word subtract with borrow
  *  u_long : r, x, c, y;
  *  r <- x - y - c  mod wordsize
  *  c <- 1 if borrow, 0 otherwise
  */
 
+#ifndef USUB
 /* Portable version */
 #define USUB(r, c, x, y)                                        \
   do {                                                          \
     (r) = (x) - (y) - (c);                                      \
     (c) = ((r)>(x) || ((r)==(x) && ((y)>0||(c)>0)))? 1 : 0;     \
   } while (0)
+#endif /*USUB*/
 
 /*-----------------------------------------------------------------
- * UMUL(hi, lo, x y)
+ * USUBOV(r, c, x, y)      unsigned word subtract with overflow check
+ *  u_long : r, c, x, y;
+ *  if x - y overflows, c = 1
+ *  else r <- x - y, c = 0
+ */
+
+#ifndef USUBOV
+/* Portable version */
+#define USUBOV(r, c, x, y)                      \
+  do {                                          \
+    (r) = (x) - (y);                            \
+    (c) = (r) > (x);                            \
+  } while (0)
+#endif /*USUBOV*/
+
+/*-----------------------------------------------------------------
+ * SSUBOV(r, c, x, y)     signed word subtract without borrow
+ *  long : r, c, x, y;
+ *  if x - y overflows, c = 1,
+ *  else r <- x - y, c = 0
+ *  Note that borrow is _not_ propagated.
+ */
+
+#ifndef SSUBOV
+/* Portable version */
+#define SSUBOV(r, c, x, y)                      \
+  do {                                          \
+    (r) = (x) - (y);                            \
+    if ((x) >= 0) {                             \
+      if ((y) < 0 && (r) <= 0) (c) = 1;         \
+      else (c) = 0;                             \
+    } else {                                    \
+      if ((y) >= 0 && (r) > 0) (c) = 1;         \
+      else (c) = 0;                             \
+    }                                           \
+  } while (0)
+#endif /*SSUBOV*/
+
+/*-----------------------------------------------------------------
+ * UMUL(hi, lo, x, y)       unsigned word multiply
  *  u_long : hi, lo, x, y;
  *  [hi, lo] <- x * y
  */
 
+#ifndef UMUL
 /* Portable version */
 #define UMUL(hi, lo, x, y)                                              \
     do {                                                                \
@@ -91,19 +177,62 @@
         lo += t4_;                                                      \
         if (lo < t4_) hi++;                                             \
     } while (0)
+#endif /*UMUL*/
 
+/*-----------------------------------------------------------------
+ * UMULOV(r, c, x, y)      unsigned word multiply with overflow check
+ *  u_long : r, x, y
+ *  int : c
+ *  if x * y overflows, c = 1
+ *  else r <- x * y, c = 0
+ */
 
-#if 0
-#undef UMUL
-/* example of processor-specific optimization */
-#define UMUL(hi, lo, x, y)                                              \
-    do {                                                                \
-        asm("movl %2, %%eax; mull %3; movl %%edx, %0; movl %%eax, %1"   \
-            : "=g" (hi), "=g" (lo)                                      \
-            : "g" (x), "g" (y)                                          \
-            : "%eax", "%edx");                                          \
+#ifndef UMULOV
+#define UMULOV(r, c, x, y)                              \
+    do {                                                \
+        if ((x)==0 || (y)==0) { (c) = (r) = 0; }        \
+        else {                                          \
+            (r) = (x) * (y);                            \
+            (c) = ((r)<(x) || (r)<(y));                 \
+        }                                               \
     } while (0)
-#endif
+#endif /*UMULOV*/
+
+/*-----------------------------------------------------------------
+ * SMULOV(r, c, x, y)      signed word multiply with overflow check
+ *  long : r, x, y
+ *  int : c
+ *  if x * y overflows, c = 1
+ *  else r <- x * y, c = 0
+ */
+
+#ifndef SMULOV
+#define SMULOV(r, c, x, y)                              \
+    do {                                                \
+        u_long t0_;                                     \
+        if ((x) >= 0) {                                 \
+            if ((y) >= 0) {                             \
+                UMULOV(t0_, c, x, y);                   \
+                if (t0_ > LONG_MAX) (c) = 1;            \
+                else (r) = t0_;                         \
+            } else {                                    \
+                UMULOV(t0_, c, x, -y);                  \
+                if (t0_ > LONG_MAX+1UL) (c) = 1;        \
+                else (r) = -t0_;                        \
+            }                                           \
+        } else {                                        \
+            if ((y) >= 0) {                             \
+                UMULOV(t0_, c, -x, y);                  \
+                if (t0_ > LONG_MAX+1UL) (c) = 1;        \
+                else (r) = -t0_;                        \
+            } else {                                    \
+                UMULOV(t0_, c, -x, -y);                 \
+                if (t0_ > LONG_MAX) (c) = 1;            \
+                else (r) = t0_;                         \
+            }                                           \
+        }                                               \
+    } while (0)
+#endif /*SMULOV*/
 
 
 #endif /* GAUCHE_ARITH_H */
--- src/test-arith.c
+++ src/test-arith.c
@@ -1,6 +1,6 @@
 /*
- * Test gauche/arith.h macros
- * $Id: test-arith.c,v 1.2 2002-06-18 06:16:30 shirok Exp $
+ * Test/arith.h macros
+ * $Id: test-arith.c,v 1.3 2002-06-22 08:39:03 shirok Exp $
  */
 
 #include <stdio.h>
@@ -8,6 +8,8 @@
 #include "gauche/arith.h"
 
 #define UMAX SCM_ULONG_MAX
+#define SMAX LONG_MAX
+#define SMIN LONG_MIN
 
 int errcount = 0;
 
@@ -25,21 +27,56 @@ void message(FILE *out, const char *m, int filler)
 
 #define TEST_SECTION(name) message(stdout, "<" name ">", '-')
 
-#define TEST_UADD(x_, y_, c_, rexp, cexp)                               \
-    do {                                                                \
-        printf("testing %u+%u c=%u expectes r=%u, c=%u =>", x_, y_, c_, \
-               rexp, cexp);                                             \
-        c = c_;                                                         \
-        x = x_;                                                         \
-        y = y_;                                                         \
-        UADD(r, c, x, y);                                               \
-        if (r == rexp && c == cexp) {                                   \
-            printf("ok\n");                                             \
-        } else {                                                        \
-            errcount++;                                                 \
-            printf("ERROR: got r=%u, c=%u\n", r, c);                    \
-        }                                                               \
+#define TEST5(x_, y_, c_, rexp, cexp, numfmt, opmsg, op)        \
+    do {                                                        \
+        printf("testing " numfmt opmsg numfmt " c=" numfmt      \
+               " expects r=" numfmt ", c=" numfmt " =>",        \
+               x_, y_, c_, rexp, cexp);                         \
+        c = c_;                                                 \
+        x = x_;                                                 \
+        y = y_;                                                 \
+        op(r, c, x, y);                                         \
+        if (r == rexp && c == cexp) {                           \
+            printf("ok\n");                                     \
+        } else {                                                \
+            errcount++;                                         \
+            printf("ERROR: got r=" numfmt                       \
+                   ", c=" numfmt "\n", r, c);                   \
+        }                                                       \
     } while (0)
+
+#define TESTOV(x_, y_, rexp, cexp, numfmt, opmsg, op)           \
+    do {                                                        \
+        x = x_;                                                 \
+        y = y_;                                                 \
+        if (cexp)                                               \
+            printf("testing " numfmt opmsg numfmt               \
+                   " expects overflow =>", x_, y_);             \
+        else                                                    \
+            printf("testing " numfmt opmsg numfmt               \
+                   " expects " numfmt " =>", x_, y_, rexp);     \
+        op(r, c, x, y);                                         \
+        if (c) {                                                \
+            if (cexp) printf("ok\n");                           \
+            else {                                              \
+                errcount++;                                     \
+                printf("ERROR: got r=" numfmt                   \
+                       ", c=" numfmt "\n", r, c);               \
+            }                                                   \
+        } else {                                                \
+            if (r == rexp) printf("ok\n");                      \
+            else {                                              \
+                errcount++;                                     \
+                printf("ERROR: got r=" numfmt "\n", r);         \
+            }                                                   \
+        }                                                       \
+    } while (0)
+
+/*
+ * UADD
+ */
+#define TEST_UADD(x_, y_, c_, rexp, cexp)               \
+  TEST5(x_, y_, c_, rexp, cexp, "%u", "+", UADD)
 
 void test_uadd(void)
 {
@@ -48,56 +85,111 @@ void test_uadd(void)
 
     /* MAX + 0 + 0 => [MAX, 0] */
     TEST_UADD(UMAX, 0, 0, UMAX, 0);
-
     /* MAX + 1 + 0 => [0, 1]   */
     TEST_UADD(UMAX, 1, 0, 0, 1);
-
     /* MAX + 2 + 0 => [1, 1]   */
     TEST_UADD(UMAX, 2, 0, 1, 1);
-
     /* MAX + MAX + 0 => [MAX-1, 1] */
     TEST_UADD(UMAX, UMAX, 0, UMAX-1, 1);
-
     /* MAX/2 + MAX/2 + 0 => [MAX-1, 0] */
     TEST_UADD(UMAX/2, UMAX/2, 0, UMAX-1, 0);
-
     /* MAX/2 + MAX + 0 => [MAX/2-1, 1] */
     TEST_UADD(UMAX/2, UMAX, 0, UMAX/2-1, 1);
-
     /* MAX-1 + 0 + 1 => [MAX, 0] */
     TEST_UADD(UMAX-1, 0, 1, UMAX, 0);
-
     /* MAX + 0 + 1 => [0, 1] */
     TEST_UADD(UMAX, 0, 1, 0, 1);
-
     /* MAX + 1 + 1 => [1, 1] */
     TEST_UADD(UMAX, 1, 1, 1, 1);
-
     /* MAX + MAX + 1 => [MAX, 1] */
     TEST_UADD(UMAX, UMAX, 1, UMAX, 1);
-
     /* MAX/2 + MAX/2 + 1 => [MAX, 0] */
     TEST_UADD(UMAX/2, UMAX/2, 1, UMAX, 0);
-
     /* MAX/2 + MAX + 1 => [MAX/2, 1] */
     TEST_UADD(UMAX/2, UMAX, 1, UMAX/2, 1);
 }
 
-#define TEST_USUB(x_, y_, c_, rexp, cexp)                               \
-    do {                                                                \
-        printf("testing %u-%u c=%u expectes r=%u, c=%u =>", x_, y_, c_, \
-               rexp, cexp);                                             \
-        c = c_;                                                         \
-        x = x_;                                                         \
-        y = y_;                                                         \
-        USUB(r, c, x, y);                                               \
-        if (r == rexp && c == cexp) {                                   \
-            printf("ok\n");                                             \
-        } else {                                                        \
-            errcount++;                                                 \
-            printf("ERROR: got r=%u, c=%u\n", r, c);                    \
-        }                                                               \
-    } while (0)
+/*
+ * UADDOV
+ */
+#define TEST_UADDOV(x_, y_, rexp, cexp)         \
+    TESTOV(x_, y_, rexp, cexp, "%u", "+", UADDOV)
+
+void test_uaddov(void)
+{
+    u_long r, c, x, y;
+    TEST_SECTION("UADDOV");
+
+    /* MAX + 0 => MAX */
+    TEST_UADDOV(UMAX, 0, UMAX, 0);
+    /* MAX + 1 => overflow */
+    TEST_UADDOV(UMAX, 1, 0, 1);
+    /* MAX + 2 => overflow */
+    TEST_UADDOV(UMAX, 2, 0, 1);
+    /* MAX + MAX => overflow */
+    TEST_UADDOV(UMAX, UMAX, 0, 1);
+    /* MAX/2 + MAX/2 => MAX-1, 0 */
+    TEST_UADDOV(UMAX/2, UMAX/2, UMAX-1, 0);
+    /* MAX/2 + MAX => overflow */
+    TEST_UADDOV(UMAX/2, UMAX, 0, 1);
+    /* MAX-1 + 1 => MAX */
+    TEST_UADDOV(UMAX-1, 1, UMAX, 0);
+    /* MAX-1 + 2 => overflow */
+    TEST_UADDOV(UMAX-1, 2, 0, 1);
+}
+
+/*
+ * SADDOV
+ */
+#define TEST_SADDOV(x_, y_, rexp, cexp)         \
+    TESTOV(x_, y_, rexp, cexp, "%ld", "+", SADDOV)
+
+void test_saddov(void)
+{
+    long r, c, x, y;
+    TEST_SECTION("SADDOV");
+
+    /* 1 + 1 => 2 */
+    TEST_SADDOV(1, 1, 2, 0);
+    /* -1 + -1 => -2 */
+    TEST_SADDOV(-1, -1, -2, 0);
+    /* SMAX + 0 => SMAX */
+    TEST_SADDOV(SMAX, 0, SMAX, 0);
+    /* SMAX + 1 => overflow */
+    TEST_SADDOV(SMAX, 1, 0, 1);
+    /* SMAX + 2 => overflow */
+    TEST_SADDOV(SMAX, 2, 0, 1);
+    /* SMAX + -1 => SMAX-1 */
+    TEST_SADDOV(SMAX, -1, SMAX-1, 0);
+    /* SMAX + -SMAX => 0 */
+    TEST_SADDOV(SMAX, -SMAX, 0, 0);
+    /* SMAX + -SMAX-1 => -1 */
+    TEST_SADDOV(SMAX, -SMAX-1, -1, 0);
+    /* SMAX/2 + SMAX/2 => SMAX-1 */
+    TEST_SADDOV(SMAX/2, SMAX/2, SMAX-1, 0);
+    /* SMAX/2 + -SMAX/2 => 0 */
+    TEST_SADDOV(SMAX/2, -(SMAX/2), 0, 0);
+    /* -SMAX/2 + -SMAX/2 => -(SMAX-1) */
+    TEST_SADDOV(-(SMAX/2), -(SMAX/2), -(SMAX-1), 0);
+    /* 0 + -SMAX => -SMAX */
+    TEST_SADDOV(0, -SMAX, -SMAX, 0);
+    /* -1 + -SMAX => SMIN */
+    TEST_SADDOV(-1, -SMAX, SMIN, 0);
+    /* -2 + -SMAX => overflow */
+    TEST_SADDOV(-2, -SMAX, 0, 1);
+    /* -SMAX + -2 => overflow */
+    TEST_SADDOV(-SMAX, -2, 0, 1);
+    /* -SMAX + -SMAX => overflow */
+    TEST_SADDOV(-SMAX, -SMAX, 0, 1);
+    /* -SMAX + SMIN => overflow */
+    TEST_SADDOV(-SMAX, SMIN, 0, 1);
+}
+
+/*
+ * USUB
+ */
+#define TEST_USUB(x_, y_, c_, rexp, cexp)               \
+    TEST5(x_, y_, c_, rexp, cexp, "%u", "-", USUB)
 
 void test_usub(void)
 {
@@ -106,44 +198,112 @@ void test_usub(void)
 
     /* MAX - 0 - 0 => [MAX, 0] */
     TEST_USUB(UMAX, 0, 0, UMAX, 0);
-
     /* MAX - 1 - 0 => [MAX-1, 0] */
     TEST_USUB(UMAX, 1, 0, UMAX-1, 0);
-
     /* 0 - MAX - 0 => [1, 1] */
     TEST_USUB(0, UMAX, 0, 1, 1);
-    
     /* 1 - MAX - 0 => [2, 1] */
     TEST_USUB(1, UMAX, 0, 2, 1);
-
     /* MAX - MAX/2 - 0 => [MAX/2+1, 0] */
     TEST_USUB(UMAX, UMAX/2, 0, UMAX/2+1, 0);
-
     /* MAX/2 - MAX - 0 => [MAX/2+1, 1] */
     TEST_USUB(UMAX/2, UMAX, 0, UMAX/2+1, 1);
-
     /* MAX - 0 - 1 => [MAX-1, 0] */
     TEST_USUB(UMAX, 0, 1, UMAX-1, 0);
-
     /* MAX - 1 - 1 => [MAX-2, 0] */
     TEST_USUB(UMAX, 1, 1, UMAX-2, 0);
-
     /* 0 - MAX - 1 => [0, 1] */
     TEST_USUB(0, UMAX, 1, 0, 1);
-    
     /* 1 - MAX - 1 => [1, 1] */
     TEST_USUB(1, UMAX, 1, 1, 1);
-
     /* MAX - MAX/2 - 1 => [MAX/2, 0] */
     TEST_USUB(UMAX, UMAX/2, 1, UMAX/2, 0);
-
     /* MAX/2 - MAX - 1 => [MAX/2, 1] */
     TEST_USUB(UMAX/2, UMAX, 1, UMAX/2, 1);
 }
 
+/*
+ * USUBOV
+ */
+#define TEST_USUBOV(x_, y_, rexp, cexp)         \
+    TESTOV(x_, y_, rexp, cexp, "%u", "-", USUBOV)
+
+void test_usubov(void)
+{
+    u_long r, c, x, y;
+    TEST_SECTION("USUBOV");
+
+    /* MAX - 0  => MAX */
+    TEST_USUBOV(UMAX, 0, UMAX, 0);
+    /* MAX - 1  => MAX-1 */
+    TEST_USUBOV(UMAX, 1, UMAX-1, 0);
+    /* MAX - MAX => 0 */
+    TEST_USUBOV(UMAX, UMAX, 0, 0);
+    /* MAX-1 - MAX => overflow */
+    TEST_USUBOV(UMAX-1, UMAX, 0, 1);
+    /* MAX - MAX-1 => 1 */
+    TEST_USUBOV(UMAX, UMAX-1, 1, 0);
+    /* 0 - 1 => overflow */
+    TEST_USUBOV(0, 1, 0, 1);
+    /* 0 - MAX => overflow */
+    TEST_USUBOV(0, UMAX, 0, 1);
+}
+
+/*
+ * SSUBOV
+ */
+#define TEST_SSUBOV(x_, y_, rexp, cexp)         \
+    TESTOV(x_, y_, rexp, cexp, "%ld", "-", SSUBOV)
+
+void test_ssubov(void)
+{
+    long r, c, x, y;
+    TEST_SECTION("SSUBOV");
+
+    /* 1 - 1 => 0 */
+    TEST_SSUBOV(1, 1, 0, 0);
+    /* -1 - -1 => 0 */
+    TEST_SSUBOV(-1, -1, 0, 0);
+    /* SMAX - 0 => SMAX */
+    TEST_SSUBOV(SMAX, 0, SMAX, 0);
+    /* SMAX - 1 => SMAX-1 */
+    TEST_SSUBOV(SMAX, 1, SMAX-1, 0);
+    /* SMAX - -1 => overflow */
+    TEST_SSUBOV(SMAX, -1, 0, 1);
+    /* SMAX - -2 => overflow */
+    TEST_SSUBOV(SMAX, -2, 0, 1);
+    /* SMAX - -SMAX => overflow */
+    TEST_SSUBOV(SMAX, -SMAX, 0, 1);
+    /* SMAX - SMAX => 0 */
+    TEST_SSUBOV(SMAX, SMAX, 0, 0);
+    /* SMAX/2 - -SMAX/2 => SMAX-1 */
+    TEST_SSUBOV(SMAX/2, -(SMAX/2), SMAX-1, 0);
+    /* SMAX/2 - SMAX/2 => 0 */
+    TEST_SSUBOV(SMAX/2, SMAX/2, 0, 0);
+    /* -SMAX/2 - SMAX/2 => 0 */
+    TEST_SSUBOV(-(SMAX/2), SMAX/2, -(SMAX-1), 0);
+    /* 0 - -SMAX => SMAX */
+    TEST_SSUBOV(0, -SMAX, SMAX, 0);
+    /* 1 - -SMAX => overflow */
+    TEST_SSUBOV(1, -SMAX, 0, 1);
+    /* 0 - -SMAX => SMAX */
+    TEST_SSUBOV(0, -SMAX, SMAX, 0);
+    /* SMIN - 1 => overflow */
+    TEST_SSUBOV(SMIN, 1, 0, 1);
+    /* SMIN - -1 => SMIN+1 */
+    TEST_SSUBOV(SMIN, -1, SMIN+1, 0);
+    /* -SMAX - -SMAX => 0 */
+    TEST_SSUBOV(-SMAX, -SMAX, 0, 0);
+    /* -SMAX - SMIN => 1 */
+    TEST_SSUBOV(-SMAX, SMIN, 1, 0);
+}
+
+/*
+ * UMUL
+ */
 #define TEST_UMUL(x_, y_, hiexp, loexp)                                 \
     do {                                                                \
-        printf("testing %u*%u expectes hi=%u, lo=%u =>", x_, y_,        \
+        printf("testing %u*%u expects hi=%u, lo=%u =>", x_, y_,        \
                hiexp, loexp);                                           \
         x = x_;                                                         \
         y = y_;                                                         \
@@ -163,17 +323,118 @@ void test_umul(void)
 
     /* MAX * MAX => [MAX-1, 1] */
     TEST_UMUL(UMAX, UMAX, UMAX-1, 1);
-
     /* MAX-1 * MAX-1 => [MAX-3, 4] */
     TEST_UMUL(UMAX-1, UMAX-1, UMAX-3, 4);
-    
     /* MAX/2 * 2 => [0, MAX-1] */
     TEST_UMUL(UMAX/2, 2, 0, UMAX-1);
-
     /* MAX/2+1 * 2 => [1, 0] */
     TEST_UMUL(UMAX/2+1, 2, 1, 0);
 }
 
+/*
+ * UMULOV
+ */
+#define TEST_UMULOV(x_, y_, rexp, cexp)          \
+    TESTOV(x_, y_, rexp, cexp, "%lu", "*", UMULOV)
+
+void test_umulov(void)
+{
+    u_long r, c, x, y;
+    TEST_SECTION("UMULOV");
+
+    /* 0 * 1 => 0 */
+    TEST_UMULOV(0, 1, 0, 0);
+    /* 1 * 0 => 0 */
+    TEST_UMULOV(1, 0, 0, 0);
+    /* 1 * 1 => 1 */
+    TEST_UMULOV(1, 1, 1, 0);
+    /* 1 * UMAX => UMAX */
+    TEST_UMULOV(1, UMAX, UMAX, 0);
+    /* 2 * UMAX => overflow */
+    TEST_UMULOV(2, UMAX, 0, 1);
+    /* UMAX/2 * 2 => UMAX-1 */
+    TEST_UMULOV(UMAX/2, 2, UMAX-1, 0);
+    /* UMAX/2 * 3 => overflow */
+    TEST_UMULOV(UMAX/2, 3, 0, 1);
+    /* UMAX * UMAX => overflow */
+    TEST_UMULOV(UMAX, UMAX, 0, 1);
+    /* UMAX-1 * UMAX-1 => overflow */
+    TEST_UMULOV(UMAX-1, UMAX-1, 0, 1);
+}
+
+/*
+ * SMULOV
+ */
+#define TEST_SMULOV(x_, y_, rexp, cexp)          \
+    TESTOV(x_, y_, rexp, cexp, "%ld", "*", SMULOV)
+
+void test_smulov(void)
+{
+    long r, c, x, y;
+    TEST_SECTION("SMULOV");
+
+    /* 0 * 1 => 0 */
+    TEST_SMULOV(0, 1, 0, 0);
+    /* 1 * 0 => 0 */
+    TEST_SMULOV(1, 0, 0, 0);
+    /* 1 * 1 => 1 */
+    TEST_SMULOV(1, 1, 1, 0);
+    /* 1 * -1 => -1 */
+    TEST_SMULOV(1, -1, -1, 0);
+    /* -1 * 1 => -1 */
+    TEST_SMULOV(-1, 1, -1, 0);
+    /* -1 * -1 => 1 */
+    TEST_SMULOV(-1, -1, 1, 0);
+    /* 1 * SMAX => SMAX */
+    TEST_SMULOV(1, SMAX, SMAX, 0);
+    /* 1 * -SMAX => -SMAX */
+    TEST_SMULOV(1, -SMAX, -SMAX, 0);
+    /* -1 * SMAX => -SMAX */
+    TEST_SMULOV(-1, SMAX, -SMAX, 0);
+    /* -1 * -SMAX => SMAX */
+    TEST_SMULOV(-1, -SMAX, SMAX, 0);
+    /* 1 * SMIN => SMIN */
+    TEST_SMULOV(1, SMIN, SMIN, 0);
+    /* -1 * SMIN => overflow */
+    TEST_SMULOV(-1, SMIN, 0, 1);
+    /* 2 * SMAX => overflow */
+    TEST_SMULOV(2, SMAX, 0, 1);
+    /* 2 * -SMAX => overflow */
+    TEST_SMULOV(2, -SMAX, 0, 1);
+    /* -2 * SMAX => overflow */
+    TEST_SMULOV(-2, SMAX, 0, 1);
+    /* -2 * -SMAX => overflow */
+    TEST_SMULOV(-2, -SMAX, 0, 1);
+    /* SMAX/2 * 2 => SMAX-1 */
+    TEST_SMULOV(SMAX/2, 2, SMAX-1, 0);
+    /* SMAX/2 * -2 => -(SMAX-1) */
+    TEST_SMULOV(SMAX/2, -2, -(SMAX-1), 0);
+    /* -SMAX/2 * 2 => -(SMAX-1) */
+    TEST_SMULOV(-(SMAX/2), 2, -(SMAX-1), 0);
+    /* -SMAX/2 * -2 => SMAX-1 */
+    TEST_SMULOV(-(SMAX/2), -2, SMAX-1, 0);
+    /* SMAX/2+1 * 2 => overflow */
+    TEST_SMULOV(SMAX/2+1, 2, 0, 1);
+    /* SMAX/2+1 * -2 => SMIN */
+    TEST_SMULOV(SMAX/2+1, -2, SMIN, 0);
+    /* -(SMAX/2+1) * 2 => SMIN */
+    TEST_SMULOV(-(SMAX/2+1), 2, SMIN, 0);
+    /* -(SMAX/2+1) * -2 => overflow */
+    TEST_SMULOV(-(SMAX/2+1), -2, 0, 1);
+    /* SMAX * SMAX => overflow */
+    TEST_SMULOV(SMAX, SMAX, 0, 1);
+    /* SMAX * -SMAX => overflow */
+    TEST_SMULOV(SMAX, -SMAX, 0, 1);
+    /* -SMAX * SMAX => overflow */
+    TEST_SMULOV(-SMAX, SMAX, 0, 1);
+    /* -SMAX * -SMAX => overflow */
+    TEST_SMULOV(-SMAX, -SMAX, 0, 1);
+}
+
+
+/*
+ * main
+ */
 int main(int argc, char **argv)
 {
     const char *testmsg = "Testing integer arithmetic macros ... ";
@@ -182,8 +443,14 @@ int main(int argc, char **argv)
     message(stdout, testmsg, '=');
     
     test_uadd();
+    test_uaddov();
+    test_saddov();
     test_usub();
+    test_usubov();
+    test_ssubov();
     test_umul();
+    test_umulov();
+    test_smulov();
 
     if (errcount) {
         fprintf(stderr, "failed.\n");
