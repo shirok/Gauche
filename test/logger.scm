58baaac1ae67f81dda19166918edbc13b488c2ce
Shiro Kawai <shiro@acm.org>
2002-09-21 20:29:43 +0000

    added tests for logger
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@3418 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ test/logger.scm
@@ -0,0 +1,75 @@
+;;
+;; testing gauche.logger
+;;
+
+;; $Id: logger.scm,v 1.1 2002-09-21 20:29:43 shirok Exp $
+
+(use gauche.test)
+(use gauche.logger)
+
+(test-start "logger")
+
+;;-------------------------------------------------------------------------
+(test-section "log-open")
+
+(sys-system "rm -f test.o")
+
+;; these shouldn't go to the log
+(log-format "testing...")
+(log-format "testing ~a..." 2)
+(log-format "testing ~a..." 3)
+
+(log-open "test.o")
+
+(log-format "real testing...")
+(log-format "real testing ~a..." 2)
+(log-format "output string\ncontaining newline\ncharacters")
+
+(log-open #f)
+
+(log-format "fake testing...")
+
+(log-open "test.o")
+
+(log-format "real testing again...")
+
+(test "log-open"
+      '("real testing..."
+        "real testing 2..."
+        "output string"
+        "containing newline"
+        "characters"
+        "real testing again...")
+      (lambda ()
+        (map (lambda (line)
+               (cond ((#/^... \d\d ..:..:.. .+\[\d+\]: (.*)$/ line)
+                      => (lambda (m) (m 1)))
+                     (else #f)))
+             (call-with-input-file "test.o" port->string-list))))
+
+(sys-system "rm -f test.o")
+
+;;-------------------------------------------------------------------------
+(test-section "customized formatter")
+
+(sys-system "rm -f test.o")
+
+(log-open "test.o" :prefix "zeepa:")
+(log-format "booba bunba bomba")
+
+(test "customized formatter"
+      '("zeepa:booba bunba bomba")
+      (lambda ()
+        (call-with-input-file "test.o" port->string-list)))
+
+(sys-system "rm -f test.o")
+
+(log-open "test.o" :prefix (lambda (drain) "poopa:"))
+(log-format "booba bunba bomba")
+
+(test "customized formatter"
+      '("poopa:booba bunba bomba")
+      (lambda ()
+        (call-with-input-file "test.o" port->string-list)))
+
+(test-end)
