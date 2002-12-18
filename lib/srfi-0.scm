ad19dfa7d296aac04423b5bb101a0340247fdfda
Shiro Kawai <shiro@acm.org>
2002-12-18 01:27:11 +0000

    added srfi-31 id
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@3631 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/srfi-0.scm
+++ lib/srfi-0.scm
@@ -1,7 +1,7 @@
 ;;;
 ;;; SRFI-0   feature based conditional expansion construct
 ;;;
-;;; $Id: srfi-0.scm,v 1.14 2002-07-17 10:39:23 shirok Exp $
+;;; $Id: srfi-0.scm,v 1.15 2002-12-18 01:27:11 shirok Exp $
 ;;;
 
 (define-module srfi-0
@@ -12,7 +12,8 @@
 ;;;
 ;;;   srfi-0, srfi-1, srfi-2, srfi-4, srfi-6, srfi-8,
 ;;;   srfi-9, srfi-11, srfi-13, srfi-14, srfi-17, srfi-18, srfi-19,
-;;;   srfi-22, srfi-23, srfi-25, srfi-27, srfi-28,  gauche
+;;;   srfi-22, srfi-23, srfi-25, srfi-27, srfi-28, srfi-30,
+;;;   srfi-31,  gauche
 ;;;
 
 (define-syntax cond-expand
@@ -22,7 +23,9 @@
                  srfi-11 srfi-12 srfi-13 srfi-14 srfi-15
                  srfi-16 srfi-17 srfi-18 srfi-19 srfi-20
                  srfi-21 srfi-22 srfi-23 srfi-24 srfi-25
-                 srfi-26 srfi-27 srfi-28 srfi-29 srfi-30)
+                 srfi-26 srfi-27 srfi-28 srfi-29 srfi-30
+                 srfi-31 srfi-32 srfi-33 srfi-34 srfi-35
+                 srfi-36 srfi-37 srfi-38 srfi-39 srfi-40)
     ((cond-expand) (error "Unfulfilled cond-expand"))
 
     ((cond-expand (else body ...))
@@ -67,27 +70,35 @@
      (begin body ...))
     ((cond-expand (srfi-9 body ...) more-clauses ...)
      (begin (use srfi-9) body ...))
+    ((cond-expand (srfi-10 body ...) more-clauses ...)
+     (begin body ...))
     ((cond-expand (srfi-11 body ...) more-clauses ...)
      (begin (use srfi-11) body ...))
     ((cond-expand (srfi-13 body ...) more-clauses ...)
      (begin (use srfi-13) body ...))
     ((cond-expand (srfi-14 body ...) more-clauses ...)
      (begin (use srfi-14) body ...))
+    ((cond-expand (srfi-17 body ...) more-clauses ...)
+     (begin body ...))
     ((cond-expand (srfi-18 body ...) more-clauses ...)
      (begin (use gauche.threads) body ...))
     ((cond-expand (srfi-19 body ...) more-clauses ...)
      (begin (use srfi-19) body ...))
-    ((cond-expand (srfi-17 body ...) more-clauses ...)
-     (begin body ...))
     ((cond-expand (srfi-22 body ...) more-clauses ...)
      (begin body ...))
     ((cond-expand (srfi-23 body ...) more-clauses ...)
      (begin body ...))
     ((cond-expand (srfi-25 body ...) more-clauses ...)
      (begin (use gauche.array) body ...))
+    ((cond-expand (srfi-26 body ...) more-clauses ...)
+     (begin body ...))
     ((cond-expand (srfi-27 body ...) more-clauses ...)
      (begin (use srfi-27) body ...))
     ((cond-expand (srfi-28 body ...) more-clauses ...)
+     (begin body ...))
+    ((cond-expand (srfi-30 body ...) more-clauses ...)
+     (begin body ...))
+    ((cond-expand (srfi-31 body ...) more-clauses ...)
      (begin body ...))
     ((cond-expand (feature-id body ...) more-clauses ...)
      (cond-expand more-clauses ...))))
