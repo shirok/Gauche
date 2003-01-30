be3d9e2301c4ce4f2ef647f679797e52c0047374
Shiro Kawai <shiro@acm.org>
2003-01-30 10:44:38 +0000

    new version from Alex
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@3718 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/text/gettext.scm
+++ lib/text/gettext.scm
@@ -1,7 +1,7 @@
 ;;;; gettext.scm -- gettext variant implemented in scheme
 
 ;;; Created:    <2002-04-14 11:29:21 foof>
-;;; Time-stamp: <2003-01-14 08:47:34 foof>
+;;; Time-stamp: <2003-01-17 12:27:13 foof>
 ;;; Author:     Alex Shinn <foof@synthcode.com>
 
 ;;; Commentary:
@@ -33,11 +33,15 @@
 ;;; sync/write options so you can use the set functionality to offer
 ;;; other interfaces to editing messages and easily save the results.
 
+;; $Id: gettext.scm,v 1.2 2003-01-30 10:44:38 shirok Exp $
+
 (define-module text.gettext
+  (use srfi-1)    ;; list library
   (use srfi-2)    ;; and-let*
   (use srfi-13)   ;; string library
   (use rfc.822)   ;; message headers parsing (same syntax for .po meta-data)
   (use file.util) ;; file-is-readable?
+  (use util.combinations)
   (use gauche.charconv)
   (use gauche.parameter)
   (export
@@ -68,16 +72,16 @@
 (define (gettext-file-guess-encoding gfile)
   (let ((locale (gettext-file-locale gfile)))
     (rxmatch-cond
-     ;; explicit encoding used in locale name
-     ((rxmatch #/\.(.*)$/ locale)
-        (#f encoding)
+      ;; explicit encoding used in locale name
+      ((rxmatch #/\.(.*)$/ locale)
+       (#f encoding)
        encoding)
-     ;; no encoding, but Japanese so we can auto-detect
-     ((rxmatch #/^ja.*/ locale)
-        (#f)
+      ;; no encoding, but Japanese so we can auto-detect
+      ((rxmatch #/^ja.*/ locale)
+       (#f)
        "*JP")
-     ;; otherwise use internal encoding (maybe default to utf-8?)
-     (else (gauche-character-encoding)))))
+      ;; otherwise use internal encoding (maybe default to utf-8?)
+      (else (gauche-character-encoding)))))
 
 (define (gettext-file-update-properties! f)
   (let ((filename (gettext-file-filename f))
@@ -85,7 +89,7 @@
         (properties '()))
     ;;(format #t "gettext: file: ~S guess-encoding: ~S\n" filename encoding)
     (when (file-is-readable? filename)
-      (and-let* ((property-msg (lookup-message filename "" encoding)))
+      (and-let* ((property-msg (lookup-po-message filename "" encoding)))
         (set! properties
               (call-with-input-string property-msg rfc822-header->list))
         (and-let* ((type-ls (assoc "content-type" properties))
@@ -134,180 +138,167 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; the gettext .po parser
 
-(define (lookup-message file msg encoding)
+(define (lookup-po-message file msg encoding)
+  ;; resisting jokes about indigent messages...
+
   ;; utilities
   (define (tail-str str)
     (call-with-input-string str (lambda (p) (read p) (read p))))
   (define (read-str str p)
     (if (equal? (peek-char p) #\")
-      (let reader ((s str)
-                   (line (read-line p)))
-        (cond ((eof-object? line)
-               s)
-              ((rxmatch #/^\s*".*"/ line)
-               (let ((obj (call-with-input-string line read)))
-                 (if (equal? (peek-char p) #\")
-                   (reader (string-append s obj) (read-line p))
-                   (string-append s obj))))
-              (else
-               s)))
-      str))
-  ;; read from the file if it exists
-  (if (file-is-readable? file)
-    (call-with-input-file file
-      (lambda (p)
-        ;;(format #t "reading: ~S\n" file)
-        (let loop ((line (read-line p)))
+        (let reader ((s str)
+                     (line (read-line p)))
           (cond ((eof-object? line)
-                 #f)
-                ((string-prefix? "msgid " line)
-                 ;;(display "msgid\n")
-                 (let ((msgid (read-str (tail-str line) p)))
-                   ;;(format #t "msgid: ~S\n\n" msgid)
-                   (cond ((string=? msgid msg)
-                          ;; skip msgstr
-                          ;;(display "msgstr\n")
-                          (let loop2 ((line (read-line p)))
-                            (cond ((eof-object? line)
-                                   #f)
-                                  ((string-prefix? "msgstr " line)
-                                   (read-str (tail-str line) p))
-                                  (else (loop2 (read-line p))))))
-                         (else
-                          (loop (read-line p))))))
+                 s)
+                ((rxmatch #/^\s*".*"/ line)
+                 (let ((obj (call-with-input-string line read)))
+                   (if (equal? (peek-char p) #\")
+                       (reader (string-append s obj) (read-line p))
+                       (string-append s obj))))
                 (else
-                 (loop (read-line p))))))
-      :encoding encoding)))
+                 s)))
+        str))
+
+  ;; read from the file if it exists
+  ;;(format #t "(lookup-po-message ~S ~S ~S)\n" file msg encoding)
+  (if (file-is-readable? file)
+      (with-error-handler
+          (lambda (err) (warn "error reading from file ~S: ~S" file err) #f)
+        (lambda ()
+          (call-with-input-file file
+            (lambda (p)
+              ;;(format #t "reading: ~S\n" file)
+              (let loop ((line (read-line p)))
+                (cond ((eof-object? line)
+                       #f)
+                      ((string-prefix? "msgid " line)
+                       (let ((msgid (read-str (tail-str line) p)))
+                         ;;(format #t "msgid: ~S\n\n" msgid)
+                         (cond ((string=? msgid msg)
+                                ;; skip msgstr
+                                ;;(display "msgstr\n")
+                                (let loop2 ((line (read-line p)))
+                                  (cond ((eof-object? line)
+                                         #f)
+                                        ((string-prefix? "msgstr " line)
+                                         (read-str (tail-str line) p))
+                                        (else (loop2 (read-line p))))))
+                               (else
+                                (loop (read-line p))))))
+                      (else
+                       (loop (read-line p))))))
+            :encoding encoding)))
+      ;; file not readable
+      #f))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; the internal routines for building/caching files and lookups
 
-;; only .po files for now
-(define (expand-lang-files dir cdir domain lang)
-  (let ((res (list (make-gettext-file (string-append dir "/" lang "/"
-                                                     cdir "/" domain ".po")
-                                      lang))))
+(define (split-langs lang)
+  (let ((res (list lang)))
     (for-each
      (lambda (sep)
-       (cond ((string-index lang sep) =>
-              (lambda (i)
-                (let* ((la (substring lang 0 i))
-                       (file (string-append dir "/" la "/" cdir "/"
-                                           domain ".po")))
-                  (set! res (cons (make-gettext-file file la) res)))))))
-     ;; LANG_REGION.CHARSET
+       (and-let* ((i (string-index lang sep)))
+         (push! res (substring lang 0 i))))
      '(#\. #\_))
     (reverse res)))
 
 ;; (make-gettext-interal domain locale dirs cdir cached?)
 (define (make-gettext-internal domain locale dirs cdir cached?)
-  ;;(format #t "make-gettext: LANG => ~S\n" (sys-getenv "LANG"))
-  (letrec
-      ((files '())
-       (cache #f)
-       (make-file-list
-        (lambda ()
-          (let ((res '()))
-            (for-each
-             (lambda (la)
-               (for-each
-                (lambda (dmn)
-                  (for-each
-                   (lambda (d)
-                     (set! res (append (expand-lang-files d cdir dmn la) res)))
-                   (reverse dirs)))
-                (reverse domain)))
-             (reverse locale))
-            (set! files (reverse res))
-            ;;(format #t "gettext files: ~S\n" files)
-            )))
-       (get
-        (lambda (msg)
-          ;;(format #t "get ~S\n" msg)
-          (or (and cached? (hash-table-get cache msg #f))
-              (let ((res #f))
-                (let loop ((l files))
-                  (if (pair? l)
-                    (let ((f (car l)))
-                      (unless (gettext-file-properties f)
-                        (gettext-file-update-properties! f))
-                      (let ((m (lookup-message (gettext-file-filename f)
-                                               msg (gettext-file-encoding f))))
-                        (if m (set! res m) (loop (cdr l)))))))
-                (if (and cached? res) (hash-table-put! cache msg res))
-                res))))
-       (set
-        (lambda (msg val)
-          ;; will trigger an error if caching was not set
-          (hash-table-put! cache msg val)))
-       (clear-cache
-        (lambda () (set! cache (make-hash-table 'string=?)))))
-    ;; initialize file-list from prefs and build dir list
-    (make-file-list)
-    (clear-cache)
-    ;; the dispatcher
-    (lambda (dispatch . args)
-      (case dispatch
-        ((getter) get)
-        ((setter) set)
-        ((get) (apply get args))
-        ((set!) (apply set args))
-        ((locale) locale)
-        ((domain) domain)
-        ((dirs) dirs)
-        ((set-locale!)
-         (set! locale (listify (car args))) (make-file-list) (clear-cache))
-        ((set-domain!)
-         (set! domain (listify (car args))) (make-file-list) (clear-cache))
-        ((set-dirs!)
-         (set! dirs (listify (car args))) (make-file-list) (clear-cache))
-        ((use-cache)
-         (set! cached? (car args)))
-        ((clear)
-         (clear-cache))
-        ))))
+
+  (define files '())  ;; internal list of message files
+  (define cache #f)   ;; internal message cache
+  (define (make-file-list)
+    (set! files
+          (filter
+           (lambda (f) (file-is-readable? (gettext-file-filename f)))
+           (map
+            (lambda (x)
+              (make-gettext-file
+               (string-append (caddr x) "/" (car x) "/" cdir "/" (cadr x) ".po")
+               (car x)))
+            (cartesian-product (list (append-map split-langs locale)
+                                     domain dirs)))))
+    ;;(format #t "gettext-files: ~S\n" (map gettext-file-filename files))
+    )
+  (define (get msg)
+    ;;(format #t "get ~S\n" msg)
+    (or (and cached? (hash-table-get cache msg #f))
+        (let ((res #f))
+          (let loop ((l files))
+            (if (pair? l)
+                (let ((f (car l)))
+                  (unless (gettext-file-properties f)
+                    (gettext-file-update-properties! f))
+                  (let ((m (lookup-po-message (gettext-file-filename f)
+                                              msg (gettext-file-encoding f))))
+                    (if m (set! res m) (loop (cdr l)))))))
+          (if (and cached? res) (hash-table-put! cache msg res))
+          res)))
+  (define (set msg val)
+    ;; will trigger an error if caching was not set
+    (hash-table-put! cache msg val))
+  (define (clear-cache)
+    (set! cache (make-hash-table 'string=?)))
+
+  ;; initialize file-list from prefs and build dir list
+  (make-file-list)
+  (clear-cache)
+  ;; the dispatcher
+  (lambda (dispatch . args)
+    (case dispatch
+      ((getter) get)
+      ((setter) set)
+      ((get) (apply get args))
+      ((set!) (apply set args))
+      ((locale) locale)
+      ((domain) domain)
+      ((dirs) dirs)
+      ((set-locale!)
+       (set! locale (listify (car args))) (make-file-list) (clear-cache))
+      ((set-domain!)
+       (set! domain (listify (car args))) (make-file-list) (clear-cache))
+      ((set-dirs!)
+       (set! dirs (listify (car args))) (make-file-list) (clear-cache))
+      ((use-cache)
+       (set! cached? (car args)))
+      ((clear)
+       (clear-cache))
+      )))
 
 (define gettext-lookup-cache (make-hash-table 'equal?))
 
 ;; cache the lookups and provide a more friendly interface.  should this
 ;; take keyword arguments?
+;; (make-gettext domain locale dirs gettext-cached? lookup-cached?)
 (define (make-gettext . args)
-  (let* ((locale (list (or (sys-getenv "LANG") (sys-getenv "LC_ALL") "C")))
-         (dirs (cond ((sys-getenv "GETTEXT_PATH")
-                      => (cut string-split <> ":"))
+  (let-optionals* args
+      ((domain '("default"))
+       (locale (list (or (sys-getenv "LANG") (sys-getenv "LC_ALL") "C")))
+       (dirs   (cond ((sys-getenv "GETTEXT_PATH") => (cut string-split <> ":"))
                      (else '())))
-         (cdir (or (sys-getenv "LC_CATEGORY") "LC_MESSAGES"))
-         (domain '("default"))
-         (gettext-cached? #t)
-         (lookup-cached? #t))
-    ;; optional domain & locale & dirs & cached?
-    (when (pair? args)
-      (if (car args) (set! domain (listify (car args))))
-      (set! args (cdr args))
-      (when (pair? args)
-        (if (car args) (set! locale (listify (car args))))
-        (set! args (cdr args))
-        (when (pair? args)
-          (if (car args) (set! dirs (listify (car args))))
-          (set! args (cdr args))
-          (when (pair? args)
-            (set! gettext-cached? (car args))
-            (set! args (cdr args))
-            (if (pair? args) (set! lookup-cached? (car args)))))))
-    ;; prepend default dirs based on domain
-    (set! dirs (append (hash-table-get domain-message-paths domain
-                                       (message-path))
-                       dirs))
-    ;; optionally lookup from cache
-    (if lookup-cached?
-      (let* ((key (list domain locale dirs cdir gettext-cached?))
-             (lookup (hash-table-get gettext-lookup-cache key #f)))
-        (unless lookup
-          (set! lookup (make-gettext-internal domain locale dirs
-                                              cdir gettext-cached?))
-          (hash-table-put! gettext-lookup-cache key lookup))
-        lookup)
-      (make-gettext-internal domain locale dirs cdir gettext-cached?))))
+       (gettext-cached? #t)
+       (lookup-cached? #t))
+    ;; make sure given arg is listified
+    (let ((domain (listify domain))
+          (locale (listify locale))
+          (dirs   (listify dirs))
+          (cdir   (or (sys-getenv "LC_CATEGORY") "LC_MESSAGES")))
+      ;; prepend default dirs based on domain
+      (set! dirs (append (hash-table-get domain-message-paths domain
+                                         (message-path))
+                         dirs))
+      ;; optionally lookup from cache
+      (if lookup-cached?
+          (let* ((key (list domain locale dirs cdir gettext-cached?))
+                 (lookup (hash-table-get gettext-lookup-cache key #f)))
+            (unless lookup
+              (set! lookup (make-gettext-internal domain locale dirs
+                                                  cdir gettext-cached?))
+              (hash-table-put! gettext-lookup-cache key lookup))
+            lookup)
+          (make-gettext-internal domain locale dirs cdir gettext-cached?))
+      )))
 
 (provide "text/gettext")
 
