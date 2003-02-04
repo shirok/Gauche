2ec507784ed44344438d98d4f273ef48f2664149
Shiro Kawai <shiro@acm.org>
2003-02-04 12:35:06 +0000

    added util.list
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@3727 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ lib/util/list.scm
@@ -0,0 +1,72 @@
+;;;
+;;; util/list.scm - more list library
+;;;
+;;;  Copyright(C) 2003 by Shiro Kawai (shiro@acm.org)
+;;;
+;;;  Permission to use, copy, modify, distribute this software and
+;;;  accompanying documentation for any purpose is hereby granted,
+;;;  provided that existing copyright notices are retained in all
+;;;  copies and that this notice is included verbatim in all
+;;;  distributions.
+;;;  This software is provided as is, without express or implied
+;;;  warranty.  In no circumstances the author(s) shall be liable
+;;;  for any damages arising out of the use of this software.
+;;;
+;;;  $Id: list.scm,v 1.1 2003-02-04 12:34:20 shirok Exp $
+;;;
+
+;; This module adds useful list utility procedures that are not in SRFI-1.
+
+(define-module util.list
+  (use srfi-1)
+  (export take* drop* split-at* slices)
+  )
+(select-module util.list)
+
+;;-----------------------------------------------------------------
+;; permissive take and drop - if the length of given list is shorter
+;; than index, returns shorter list or fills the rest.
+
+(define (split-at* lis k . args)
+  (when (or (not (integer? k)) (negative? k))
+    (error "index must be non-negative integer" k))
+  (let-optionals* args ((fill? #f)
+                        (filler #f))
+    (let loop ((i 0)
+               (lis lis)
+               (r '()))
+      (cond ((= i k) (values (reverse! r) lis))
+            ((null? lis)
+             (values (if fill?
+                         (append! (reverse! r) (make-list (- k i) filler))
+                         (reverse! r))
+                     lis))
+            (else (loop (+ i 1) (cdr lis) (cons (car lis) r)))))))
+
+(define (take* lis k . args)
+  (receive (h t) (apply split-at* lis k args) h))
+
+(define (drop* lis k)
+  (when (or (not (integer? k)) (negative? k))
+    (error "index must be non-negative integer" k))
+  (let loop ((i 0)
+             (lis lis))
+    (cond ((= i k) lis)
+          ((null? lis) '())
+          (else (loop (+ i 1) (cdr lis))))))
+
+;;-----------------------------------------------------------------
+;; slices - split a list to a bunch of sublists of length k
+;;
+
+(define (slices lis k . args)
+  (unless (and (integer? k) (positive? k))
+    (error "index must be positive integer" k))
+  (let loop ((lis lis)
+             (r '()))
+    (if (null? lis)
+        (reverse! r)
+        (receive (h t) (apply split-at* lis k args)
+          (loop t (cons h r))))))
+
+(provide "util/list")
