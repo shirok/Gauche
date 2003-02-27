efddf3f6fd3645194c7ff24b7203f8b1c035e8f1
Shiro Kawai <shiro@acm.org>
2003-02-27 11:58:52 +0000

    forgot to add intersperse in export list---fixed.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@3792 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/util/list.scm
+++ lib/util/list.scm
@@ -12,15 +12,15 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: list.scm,v 1.4 2003-02-27 08:19:34 shirok Exp $
+;;;  $Id: list.scm,v 1.5 2003-02-27 11:58:52 shirok Exp $
 ;;;
 
 ;; This module adds useful list utility procedures that are not in SRFI-1.
 
 (define-module util.list
   (use srfi-1)
-  (export take* drop* take-right* drop-right* split-at* slices
-          cond-list)
+  (export take* drop* take-right* drop-right* split-at*
+          slices intersperse cond-list)
   )
 (select-module util.list)
 
