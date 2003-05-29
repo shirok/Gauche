aac02c7b3c87671dec1d800a707adc4c16bf31fb
Shiro Kawai <shiro@acm.org>
2003-05-29 20:06:21 +0000

    exports LOG_ constants conditionally
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@3909 c205566b-fb4e-0410-ab5c-8f0660156277

--- ext/syslog/syslog.scm
+++ ext/syslog/syslog.scm
@@ -1,7 +1,7 @@
 ;;;
 ;;; syslog - syslog interface
 ;;;
-;;;  Copyright(C) 2002 by Shiro Kawai (shiro@acm.org)
+;;;  Copyright(C) 2002-2003 by Shiro Kawai (shiro@acm.org)
 ;;;
 ;;;  Permission to use, copy, modify, distribute this software and
 ;;;  accompanying documentation for any purpose is hereby granted,
@@ -12,51 +12,51 @@
 ;;;  warranty.  In no circumstances the author(s) shall be liable
 ;;;  for any damages arising out of the use of this software.
 ;;;
-;;;  $Id: syslog.scm,v 1.2 2002-09-22 00:40:30 shirok Exp $
+;;;  $Id: syslog.scm,v 1.3 2003-05-29 20:06:21 shirok Exp $
 ;;;
 
 (define-module gauche.syslog
-  (export sys-openlog sys-syslog sys-closelog sys-setlogmask
-          LOG_CONS
-          LOG_NDELAY
-          LOG_NOWAIT
-          LOG_ODELAY
-          LOG_PERROR
-          LOG_PID
-          LOG_AUTH
-          LOG_AUTHPRIV
-          LOG_CRON
-          LOG_DAEMON
-          LOG_FTP
-          LOG_KERN
-          LOG_LOCAL0
-          LOG_LOCAL1
-          LOG_LOCAL2
-          LOG_LOCAL3
-          LOG_LOCAL4
-          LOG_LOCAL5
-          LOG_LOCAL6
-          LOG_LOCAL7
-          LOG_LPR
-          LOG_MAIL
-          LOG_NEWS
-          LOG_SYSLOG
-          LOG_USER
-          LOG_UUCP
-          LOG_EMERG
-          LOG_ALERT
-          LOG_CRIT
-          LOG_ERR
-          LOG_WARNING
-          LOG_NOTICE
-          LOG_INFO
-          LOG_DEBUG
-          )
-  )
+  (export sys-openlog sys-syslog sys-closelog sys-setlogmask))
 
 (select-module gauche.syslog)
 
 (dynamic-load "syslog")
+
+(export-if-defined |LOG_CONS|
+                   |LOG_NDELAY|
+                   |LOG_NOWAIT|
+                   |LOG_ODELAY|
+                   |LOG_PERROR|
+                   |LOG_PID|
+                   |LOG_AUTH|
+                   |LOG_AUTHPRIV|
+                   |LOG_CRON|
+                   |LOG_DAEMON|
+                   |LOG_FTP|
+                   |LOG_KERN|
+                   |LOG_LOCAL0|
+                   |LOG_LOCAL1|
+                   |LOG_LOCAL2|
+                   |LOG_LOCAL3|
+                   |LOG_LOCAL4|
+                   |LOG_LOCAL5|
+                   |LOG_LOCAL6|
+                   |LOG_LOCAL7|
+                   |LOG_LPR|
+                   |LOG_MAIL|
+                   |LOG_NEWS|
+                   |LOG_SYSLOG|
+                   |LOG_USER|
+                   |LOG_UUCP|
+                   |LOG_EMERG|
+                   |LOG_ALERT|
+                   |LOG_CRIT|
+                   |LOG_ERR|
+                   |LOG_WARNING|
+                   |LOG_NOTICE|
+                   |LOG_INFO|
+                   |LOG_DEBUG|
+                   )
 
 (provide "gauche/syslog")
 
