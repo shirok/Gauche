fc6c58e69a2d51101fec9633103f631386685e6f
Shiro Kawai <shiro@acm.org>
2003-09-26 10:25:37 +0000

    added test to check sxpath module
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@4113 c205566b-fb4e-0410-ab5c-8f0660156277

--- ext/sxml/test.scm
+++ ext/sxml/test.scm
@@ -1,3 +1,19 @@
+;;
+;; test sxml tools
+;;
+
+;; ssax test is derived from the original SSAX source.
 (load "./ssax-test.scm")
+
 ;(load "./tree-trans-test.scm")
 ;(load "./to-html-test.scm")
+
+;; we don't have new sxpath test suite yet.  for now, just check
+;; module consistency.
+
+(use gauche.test)
+
+(test-start "sxpath")
+(use sxml.sxpath)
+(test-module 'sxml.sxpath)
+(test-end)
