44e110dac7bba9b67f9cc2a830ed7415a30776ea
Shiro Kawai <shiro@acm.org>
2003-09-28 12:46:04 +0000

    applied patch from Kimura Fuyuki to enable waiting for multiple sockets
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@4124 c205566b-fb4e-0410-ab5c-8f0660156277

--- examples/echo-server.scm
+++ examples/echo-server.scm
@@ -1,21 +1,12 @@
 ;; Example of gauche.selector
-;;  $Id: echo-server.scm,v 1.1 2003-01-08 02:05:30 shirok Exp $
+;;  $Id: echo-server.scm,v 1.2 2003-09-28 12:46:04 shirok Exp $
 
 (use gauche.net)
 (use gauche.selector)
 
 (define (echo-server port)
   (let ((selector (make <selector>))
-        (server   (make-server-socket 'inet port :reuse-addr? #t)))
-
-    (define (accept-handler sock flag)
-      (let* ((client (socket-accept server))
-             (output (socket-output-port client)))
-        (selector-add! selector
-                       (socket-input-port client :buffered? #f)
-                       (lambda (input flag)
-                         (echo client input output))
-                       '(r))))
+        (servers  (make-server-sockets #f port :reuse-addr? #t)))
 
     (define (echo client input output)
       (let ((str (read-block 4096 input)))
@@ -25,14 +16,26 @@
             (begin (display str output)
                    (flush output)))))
 
-    (selector-add! selector
-                   (socket-fd server)
-                   accept-handler
-                   '(r))
+    (for-each
+     (lambda (server)
+
+       (define (accept-handler sock flag)
+	 (let* ((client (socket-accept server))
+		(output (socket-output-port client)))
+	   (selector-add! selector
+			  (socket-input-port client :buffered? #f)
+			  (lambda (input flag)
+			    (echo client input output))
+			  '(r))))
+
+       (selector-add! selector
+		      (socket-fd server)
+		      accept-handler
+		      '(r)))
+     servers)
     (do () (#f) (selector-select selector))))
 
 (define (main args)
   (print "echo server starting on port 3131")
   (echo-server 3131)
   0)
-
