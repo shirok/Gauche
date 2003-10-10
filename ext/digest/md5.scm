0d517c1f38f71b248bf6d33ee152c346e64f085a
Kimura Fuyuki <fuyuki@hadaly.org>
2003-10-10 21:52:19 +0000

    update my mail address and copyright notice
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@4161 c205566b-fb4e-0410-ab5c-8f0660156277

--- ext/digest/md5.scm
+++ ext/digest/md5.scm
@@ -1,18 +1,36 @@
 ;;;
 ;;; md5 - MD5 message-digest
 ;;;
-;;;  Copyright(C) 2002 by Kimura Fuyuki (fuyuki@hadaly.org)
+;;;   Copyright (c) 2002-2003 Kimura Fuyuki, All rights reserved.
 ;;;
-;;;  Permission to use, copy, modify, distribute this software and
-;;;  accompanying documentation for any purpose is hereby granted,
-;;;  provided that existing copyright notices are retained in all
-;;;  copies and that this notice is included verbatim in all
-;;;  distributions.
-;;;  This software is provided as is, without express or implied
-;;;  warranty.  In no circumstances the author(s) shall be liable
-;;;  for any damages arising out of the use of this software.
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
 ;;;
-;;;  $Id: md5.scm,v 1.5 2002-12-12 06:32:35 shirok Exp $
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;
+;;;   3. Neither the name of the authors nor the names of its contributors
+;;;      may be used to endorse or promote products derived from this
+;;;      software without specific prior written permission.
+;;;
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;
+;;;  $Id: md5.scm,v 1.6 2003-10-10 21:52:18 fuyuki Exp $
 ;;;
 
 ;;; RFC 1321 The MD5 Message-Digest Algorithm
