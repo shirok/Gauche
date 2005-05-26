b70a6fc793ba66bce5e9b31eb9b8cdcb6cc65631
Shiro Kawai <shiro@acm.org>
2005-05-26 11:33:30 +0000

    added
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@5039 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ doc/mingw-dist.sh
@@ -0,0 +1,42 @@
+# Create Mingw binary distribution.
+# See mingw-memo.txt for the details.
+
+# You need Cygwin, and Cygwin-version of Gauche installed in order to
+# make Mingw binary package from the release tarball.
+
+# Set those two variables for your env.
+mingwdir=${MINGWDIR:-/cygdrive/c/mingw}
+cyggosh=${CYGGOSH:-/usr/local/bin/gosh}
+
+# make sure we're going to use Mingw gcc
+PATH=$mingwdir/bin:$PATH
+case `gcc --version` in
+  *mingw*) ;;
+  *) echo "Set PATH to have MinGW bin directory first"
+     exit 1;;
+esac
+
+# build
+if [ -f Makefile ]; then make distclean; fi
+if [ -f examples/spigot/Makefile ]; then 
+  (cd examples/spigot; make maintainer-clean);
+fi
+distdir=`pwd`/../Gauche-mingw-dist
+rm -rf $distdir
+./configure --build=i686-pc-mingw32 --prefix=$distdir/Gauche
+make
+
+# use Cygwin's gosh to install stuff
+VERSION=`cat VERSION`
+make GOSH=$cyggosh install
+(cd src; make GOSH=$cyggosh install-mingw)
+rm -rf $distdir/Gauche/bin/gauche-install
+rm -rf $distdir/Gauche/bin/gauche-package
+rm -rf $distdir/Gauche/bin/gauche-config
+rm -rf $distdir/Gauche/bin/gauche-cesconv
+rm -rf $distdir/Gauche/lib/libgauche.dll*
+rm -rf $distdir/Gauche/lib/gauche/$VERSION/*/gosh.exe
+rm -rf $distdir/Gauche/lib/gauche/$VERSION/*/libgauche.dll
+cp COPYING $distdir/Gauche
+cp $mingwdir/bin/mingwm10.dll $distdir/Gauche/bin
+(cd $distdir; zip -r Gauche-mingw-$VERSION.zip Gauche)
