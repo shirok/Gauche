1e2a99b50c3b6f15332481b40db6e37d996f5457
Shiro Kawai <shiro@acm.org>
2005-08-10 10:10:15 +0000

    added sys-htons etc.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@5183 c205566b-fb4e-0410-ab5c-8f0660156277

--- ext/net/net.scm
+++ ext/net/net.scm
@@ -1,7 +1,7 @@
 ;;;
 ;;; net - network interface
 ;;;  
-;;;   Copyright (c) 2000-2003 Shiro Kawai, All rights reserved.
+;;;   Copyright (c) 2000-2005 Shiro Kawai, All rights reserved.
 ;;;   
 ;;;   Redistribution and use in source and binary forms, with or without
 ;;;   modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
 ;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ;;;  
-;;;  $Id: net.scm,v 1.29 2005-07-11 03:33:13 shirok Exp $
+;;;  $Id: net.scm,v 1.30 2005-08-10 10:10:15 shirok Exp $
 ;;;
 
 (define-module gauche.net
@@ -51,6 +51,7 @@
           <sys-hostent> sys-gethostbyname sys-gethostbyaddr
           <sys-protoent> sys-getprotobyname sys-getprotobynumber
           <sys-servent> sys-getservbyname sys-getservbyport
+          sys-htonl sys-htons sys-ntohl sys-ntohs
           )
   )
   
