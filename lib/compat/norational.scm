cf1e07a78470cb331d29dfd44bd6fcb637b2251f
Shiro Kawai <shiro@acm.org>
2006-10-07 07:38:10 +0000

    more comment
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@5533 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/compat/norational.scm
+++ lib/compat/norational.scm
@@ -1,6 +1,8 @@
 ;;;
-;;; compat.norational - 
+;;; compat.norational - override '/' for compatibility
 ;;;  
+;;; $Id: norational.scm,v 1.2 2006-10-07 07:38:10 shirok Exp $
+;;;
 
 (define-module compat.norational
   (export /))
