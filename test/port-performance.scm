04cb6b354f5b8a396926f14639d111609c5a57d6
Shiro Kawai <shiro@acm.org>
2006-11-11 20:57:34 +0000

    added
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@5595 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ test/port-performance.scm
@@ -0,0 +1,13 @@
+;;
+;; a short test program to measure port-I/O primitive performance
+;;
+
+(use gauche.time)
+
+(time (with-input-from-file "/usr/share/dict/words"
+        (lambda ()
+          (with-output-to-file "/dev/null"
+            (lambda ()
+              (port-for-each write-byte read-byte))))))
+
+      
