9dbbaf5dc40e8b82aa7e2e51907f99f07ae9ed6a
Shiro Kawai <shiro@acm.org>
2007-02-27 09:33:38 +0000

    some copyright fix
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@5796 c205566b-fb4e-0410-ab5c-8f0660156277

--- src/core.c
+++ src/core.c
@@ -1,7 +1,7 @@
 /*
  * core.c - core kernel interface
  *
- *   Copyright (c) 2000-2007 Shiro Kawai (shiro@acm.org)
+ *   Copyright (c) 2000-2007 Shiro Kawai  <shiro@acm.org>
  * 
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *  $Id: core.c,v 1.75 2007-02-13 06:40:36 shirok Exp $
+ *  $Id: core.c,v 1.76 2007-02-27 09:33:38 shirok Exp $
  */
 
 #include <stdlib.h>
