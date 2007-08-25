2b9a6cd71c0c850b6f005e22b5c7326ac5e66131
Shiro Kawai <shiro@acm.org>
2007-08-25 02:29:34 +0000

    adapted gauche-config to MSVC build
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@5982 c205566b-fb4e-0410-ab5c-8f0660156277

--- src/getdir_win.c
+++ src/getdir_win.c
@@ -2,7 +2,7 @@
  * getdir_win.c - get the library directory at runtime (fow windows)
  *  included from paths.c
  *
- * $Id: getdir_win.c,v 1.3 2007-08-24 23:55:42 shirok Exp $
+ * $Id: getdir_win.c,v 1.4 2007-08-25 02:29:31 shirok Exp $
  */
 
 #include <windows.h>
@@ -19,11 +19,9 @@ static int get_install_dir(char *buf, int buflen,
 #if defined(UNICODE)
     WCHAR path[MAX_PATH];
     const WCHAR *libname = L"libgauche.dll";
-#define WCS2MBS(x) Scm_WCS2MBS(x)
 #else
     char path[MAX_PATH];
     const char *libname = "libgauche.dll";
-#define WCS2MBS(x) (x)
 #endif
 
     if ((mod = GetModuleHandle(libname)) == NULL) {
@@ -34,13 +32,13 @@ static int get_install_dir(char *buf, int buflen,
     }
     /* remove \libgauche.dll */
     if (!PathRemoveFileSpec(path)) {
-        errfn("PathRemoveFileSpec failed on %s", WCS2MBS(path));
+        errfn("PathRemoveFileSpec failed on %s", SCM_WCS2MBS(path));
     }
     /* remobe \bin */
     if (!PathRemoveFileSpec(path)) {
-        errfn("PathRemoveFileSpec failed on %s", WCS2MBS(path));
+        errfn("PathRemoveFileSpec failed on %s", SCM_WCS2MBS(path));
     }
-    mbpath = WCS2MBS(path);
+    mbpath = SCM_WCS2MBS(path);
     len = (int)strlen(mbpath);
     if (len >= buflen-1) {
         errfn("Pathname too long: %s", mbpath);
