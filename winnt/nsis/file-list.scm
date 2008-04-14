d1d551f60ea918ff50980f93ab14c818df2c189f
ENDO Yasuyuki <yasuyuki@javaopen.org>
2008-04-14 09:57:40 +0000

    include Gauche-mingw file-list into setup.nsi from setup.nsi.in
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@6180 c205566b-fb4e-0410-ab5c-8f0660156277

--- winnt/nsis/file-list.scm
+++ winnt/nsis/file-list.scm
@@ -4,15 +4,20 @@
 ;;  See COPYING for terms and conditions of using this software 
 
 (use file.util)
+(use srfi-1)
 (use srfi-13)
 (use gauche.parameter)
 (use gauche.parseopt)
+(use text.tree)
 
+(define *dist-path* "../../../Gauche-mingw-dist/Gauche")
 (define *instdir* "$INSTDIR")
 (define *ignore-path* "CVS")
 (define *target-prefix* "    SetOutPath ")
 (define *install-prefix* "    File ")
 (define *delete-prefix* "    Delete /REBOOTOK ")
+(define *cr-lf* "\r\n")
+(define *path-separater* "\\")
 (define *root-path* (make-parameter #f))
 (define *file-list* (make-parameter #f))
 
@@ -24,41 +29,64 @@
   (directory-fold
    path cons '()
    :lister (lambda (path seed)
-         (receive (d f)
-         (directory-list2 path :add-path? #t :children? #t)
-           (values d
-               (cons (cons path f) seed))))))
+	     (receive (d f)
+		      (directory-list2 path :add-path? #t :children? #t)
+		      (values d
+			      (cons (cons path f) seed))))))
 
 (define (initialize path)
   (begin0
    (*root-path* path)
-   (*file-list* (reverse (file-list (*root-path*))))))
+   (*file-list*
+    (reverse
+     (filter
+      (lambda (ls) (and (pair? (cdr ls))
+			(not (string-contains (car ls) *ignore-path*))))
+      (file-list (*root-path*)))))))
 
-(define (print-inst ls)
-  (unless (or (null? (cdr ls))
-	      (string-contains (car ls) *ignore-path*))
-	  (print *target-prefix*
-		 (replace-inst (*root-path*) (car ls)))
-	  (for-each (cut print *install-prefix* <>)
-		    (cdr ls))))
+(define (inst-str ls)
+  (cons
+   #`",|*target-prefix*|,(change-path (replace-inst (*root-path*) (car ls))),|*cr-lf*|"
+   (map change-path
+	(map
+	 (lambda (s) #`",|*install-prefix*|,|s|,|*cr-lf*|")
+	(cdr ls)))))
 
-(define (print-del ls)
-  (unless (or (null? (cdr ls))
-	      (string-contains (car ls) *ignore-path*))
-	  (for-each
-	   (lambda (s)
-	     (print *delete-prefix*
-		    (replace-inst (*root-path*) s)))
-	   (cdr ls))))
+(define (del-str ls)
+  (map
+   (lambda (s) #`",|*delete-prefix*|,(change-path (replace-inst (*root-path*) s)),|*cr-lf*|")
+   (cdr ls)))
+
+(define (install-files)
+  (map inst-str (*file-list*)))
+
+(define (delete-files)
+  (map del-str (*file-list*)))
+
+(define (include line)
+  (or
+   (and-let* ((match (#/^##\(include (.*)\)\r\n$/ line))
+	      (exp (call-with-input-string (match 1) (cut read <>))))
+	     (eval exp (current-module)))
+   line))
+
+(define (include-all lis)
+  (map include lis))
+
+(define (change-path str)
+  (string-join (string-split str #\/) *path-separater*))
+
+(define (read-line-crlf file)
+  (let1 line (read-line file)
+	(if (eof-object? line) line
+	    #`",|line|,|*cr-lf*|")))
 
 (define (main args)
-  (let-args (cdr args)
-   [(ins "i|ins" #f)
-    (del "d|del" #f)
-    (path "p|path=s")]
-   (when path
-	 (initialize path)
-	 (when ins
-	       (for-each print-inst (*file-list*)))
-	 (when del
-	       (for-each print-del (*file-list*))))))
+  (let* ((in (cadr args))
+	 (out (path-sans-extension in))
+	 (lines (file->list read-line-crlf in)))
+    (begin
+      (initialize *dist-path*)
+      (call-with-output-file out
+	(cut write-tree (include-all lines) <>))
+      0)))
