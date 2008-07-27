0459b5c22e4a0ddce7ae8e142f78c69a19cd3252
Shiro Kawai <shiro@acm.org>
2008-07-27 23:36:09 +0000

    updated for newer version
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@6308 c205566b-fb4e-0410-ab5c-8f0660156277

--- ext/peg/benchmark.scm
+++ ext/peg/benchmark.scm
@@ -1,18 +1,20 @@
 (use parser.peg)
 (use gauche.charconv)
+(use srfi-13)
 (use srfi-42)
 
 (define data
-  (call-with-input-file "data/13tokyo.csv"
-    port->string
-    :encoding 'utf8))
+  (let1 s (call-with-input-file "data/13tokyo.csv"
+            port->string
+            :encoding 'utf8)
+    (string-concatenate (make-list 10 s))))
 
 (define csv-parser
-  (let* ((spaces_ ($many_ ($one-of #[ \t])))
+  (let* ((spaces_ ($skip-many ($one-of #[ \t])))
          (spaces ($many ($one-of #[ \t])))
          (comma  ($seq spaces_ ($char #\,) spaces_))
          (dquote ($char #\"))
-         (double-dquote ($do (($string "\"\"")) ($return #\")))
+         (double-dquote ($do [($string "\"\"")] ($return #\")))
          (quoted-body ($many ($or ($one-of #[^\"]) double-dquote)))
          (quoted ($between dquote quoted-body dquote))
          (unquoted ($alternate ($one-of #[^ \t\n,]) spaces))
@@ -20,8 +22,9 @@
          (record ($sep-by ($->rope field) comma 1)))
     ($sep-by record newline)))
 
-(time (parse-string csv-parser data))
+(time (peg-parse-string csv-parser data))
 
+#|
 (add-load-path ".")
 (use peg-orig)
 
@@ -38,6 +41,7 @@
     ($sep-by record newline)))
 
 (time (parse-string csv-parser data))
+|#
 
 ;;;============================================================
 (use text.csv)
