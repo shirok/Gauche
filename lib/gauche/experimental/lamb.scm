ebebdf907111c33ff383a4c5572d72154bb610d8
Shiro Kawai <shiro@acm.org>
2008-08-29 22:18:29 +0000

    experimentally added ^. and ^*
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@6366 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/gauche/experimental/lamb.scm
+++ lib/gauche/experimental/lamb.scm
@@ -3,11 +3,20 @@
 ;;;
 
 (define-module gauche.experimental.lamb
-  (export ^))
+  (use util.match)
+  (export ^ ^. ^*))
 (select-module gauche.experimental.lamb)
 
 (define-syntax ^
   (syntax-rules ()
     [(^ formals . body) (lambda formals . body)]))
+
+(define-syntax ^.
+  (syntax-rules ()
+    [(^. . clauses) (match-lambda . clauses)]))
+
+(define-syntax ^*
+  (syntax-rules ()
+    [(^* . clauses) (match-lambda* . clauses)]))
 
 (provide "gauche/experimental/lamb")
