f2a6bade543fa2856ab8828703e4bf45437a238a
Shiro Kawai <shiro@acm.org>
2008-10-30 10:27:23 +0000

    added rfc.zlib
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@6425 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ ext/zlib/gauche-zlib.h
@@ -0,0 +1,128 @@
+/*
+ * Gauche-zlib - zlib module
+ * 
+ *    Copyright (c) 2006 Rui Ueyama, All rights reserved.
+ * 
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions
+ *    are met:
+ * 
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ * 
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ * 
+ *    3. Neither the name of the authors nor the names of its contributors
+ *       may be used to endorse or promote products derived from this
+ *       software without specific prior written permission.
+ * 
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ *   $Id: zlib.h,v 1.1.1.1 2006-01-17 07:52:02 rui Exp $
+ */
+
+/* Prologue */
+#ifndef GAUCHE_ZLIB_H
+#define GAUCHE_ZLIB_H
+
+#include <gauche.h>
+#include <gauche/extend.h>
+#include <zlib.h>
+
+SCM_DECL_BEGIN
+
+typedef struct ScmZlibInfoRec {
+    z_streamp strm;
+    ScmPort *remote;            /* source or drain port */
+    int ownerp;
+    int flush;
+    int stream_endp;
+    int bufsiz;
+    char *buf;
+    char *ptr;
+    unsigned char *dict;
+    int dictlen;
+    int level;
+    int strategy;
+    ScmObj dict_adler;
+} ScmZlibInfo;
+
+#define SCM_PORT_ZLIB_INFO(p) ((ScmZlibInfo*)(p)->src.buf.data)
+#define SCM_PORT_ZSTREAM(p)   (SCM_PORT_ZLIB_INFO(p)->strm)
+
+SCM_CLASS_DECL(Scm_DeflatePortClass);
+#define SCM_CLASS_DEFLATE_PORT  (&Scm_DeflatePortClass)
+#define SCM_DEFLATE_PORT_P(obj) SCM_ISA(obj, SCM_CLASS_DEFLATE_PORT)
+SCM_CLASS_DECL(Scm_InflatePortClass);
+#define SCM_CLASS_INFLATE_PORT  (&Scm_InflatePortClass)
+#define SCM_INFLATE_PORT_P(obj) SCM_ISA(obj, SCM_CLASS_INFLATE_PORT)
+
+SCM_EXTERN ScmObj Scm_MakeDeflatePort(ScmPort *source, int level,
+                                  int window_bits, int memlevel,
+                                  int strategy, ScmObj dict,
+                                  int bufsiz, int ownerp);
+SCM_EXTERN ScmObj Scm_MakeInflatePort(ScmPort *sink, int bufsiz,
+                                      int window_bits, ScmObj dict,
+                                      int ownerp);
+
+/*================================================================
+ * Conditions
+ */
+
+typedef ScmError ScmZlibError;
+
+SCM_CLASS_DECL(Scm_ZlibErrorClass);
+#define SCM_CLASS_ZLIB_ERROR  (&Scm_ZlibErrorClass)
+#define SCM_ZLIB_ERROR(obj)   ((ScmZlibError*)(obj))
+#define SCM_ZLIB_ERRORP(obj)  SCM_ISA(obj, SCM_CLASS_ZLIB_ERROR)
+
+typedef ScmZlibError ScmZlibNeedDictError;
+SCM_CLASS_DECL(Scm_ZlibNeedDictErrorClass);
+#define SCM_CLASS_ZLIB_NEED_DICT_ERROR  (&Scm_ZlibNeedDictErrorClass)
+#define SCM_ZLIB_NEED_DICT_ERROR(obj)   ((ScmZlibNeedDictError*)(obj))
+#define SCM_ZLIB_NEED_DICT_ERRORP(obj)  SCM_ISA(obj, SCM_CLASS_ZLIB_NEED_DICT_ERROR)
+
+typedef ScmZlibError ScmZlibStreamError;
+SCM_CLASS_DECL(Scm_ZlibStreamErrorClass);
+#define SCM_CLASS_ZLIB_STREAM_ERROR  (&Scm_ZlibStreamErrorClass)
+#define SCM_ZLIB_STREAM_ERROR(obj)   ((ScmZlibStreamError*)(obj))
+#define SCM_ZLIB_STREAM_ERRORP(obj)  SCM_ISA(obj, SCM_CLASS_ZLIB_STREAM_ERROR)
+
+typedef ScmZlibError ScmZlibDataError;
+SCM_CLASS_DECL(Scm_ZlibDataErrorClass);
+#define SCM_CLASS_ZLIB_DATA_ERROR       (&Scm_ZlibDataErrorClass)
+#define SCM_ZLIB_DATA_ERROR(obj)        ((ScmZlibDataError*)(obj))
+#define SCM_ZLIB_DATA_ERRORP(obj)       SCM_ISA(obj, SCM_CLASS_ZLIB_DATA_ERROR)
+
+typedef ScmZlibError ScmZlibMemoryError;
+SCM_CLASS_DECL(Scm_ZlibMemoryErrorClass);
+#define SCM_CLASS_ZLIB_MEMORY_ERROR     (&Scm_ZlibMemoryErrorClass)
+#define SCM_ZLIB_MEMORY_ERROR(obj)      ((ScmZlibMemoryError*)(obj))
+#define SCM_ZLIB_MEMORY_ERRORP(obj)     SCM_ISA(obj, SCM_CLASS_ZLIB_MEMORY_ERROR)
+
+typedef ScmZlibError ScmZlibVersionError;
+SCM_CLASS_DECL(Scm_ZlibVersionErrorClass);
+#define SCM_CLASS_ZLIB_VERSION_ERROR    (&Scm_ZlibVersionErrorClass)
+#define SCM_ZLIB_VERSION_ERROR(obj)     ((ScmZlibVersionError*)(obj))
+#define SCM_ZLIB_VERSION_ERRORP(obj)    SCM_ISA(obj, SCM_CLASS_ZLIB_VERSION_ERROR)
+
+SCM_EXTERN ScmObj Scm_MakeZlibError(ScmObj message, int error_code);
+SCM_EXTERN void Scm_ZlibError(int error_code, const char *msg, ...);
+SCM_EXTERN ScmObj Scm_InflateSync(ScmPort *port);
+
+/* Epilogue */
+SCM_DECL_END
+
+#endif  /* GAUCHE_ZLIB_H */
---
+++ ext/zlib/rfc/zlib.scm
@@ -0,0 +1,107 @@
+;;;
+;;; Gauche-zlib - zlib module
+;;;
+;;;   Copyright (c) 2006 Rui Ueyama, All rights reserved.
+;;;
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;
+;;;   3. Neither the name of the authors nor the names of its contributors
+;;;      may be used to endorse or promote products derived from this
+;;;      software without specific prior written permission.
+;;;
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;
+;;;  $Id: zlib.scm,v 1.2 2006-12-02 16:00:40 rui Exp $
+;;;
+
+(define-module rfc.zlib
+  (use gauche.uvector)
+  (export zlib-version adler32 crc32
+          open-deflate-port open-inflate-port
+          deflate-string inflate-string
+          <zlib-error> <zlib-need-dict-error>
+          <zlib-stream-error> <zlib-data-error>
+          <zlib-memory-error> <zlib-version-error>
+          <deflate-port> <inflate-port>
+          deflate-port-full-flush
+          zstream-total-in zstream-total-out
+          zstream-params-set!
+          zstream-adler32
+          zstream-data-type
+          zstream-dictionary-adler32
+          gzip-encode-string gzip-decode-string
+          inflate-sync
+          |Z_NO_COMPRESSION| |Z_BEST_SPEED|
+          |Z_BEST_COMPRESSION| |Z_DEFAULT_COMPRESSION|
+          |Z_FILTERED| |Z_HUFFMAN_ONLY|
+          |Z_RLE| |Z_FIXED| |Z_DEFAULT_STRATEGY|
+          |Z_BINARY| |Z_TEXT| |Z_ASCII| |Z_UNKNOWN|
+          ))
+(select-module rfc.zlib)
+
+(dynamic-load "zlib")
+
+;; body
+(define (open-deflate-port source . args)
+  (let-keywords* args ((compression-level |Z_DEFAULT_COMPRESSION|)
+                       (window-bits 15)
+                       (memory-level 8)
+                       (strategy |Z_DEFAULT_STRATEGY|)
+                       (dictionary #f)
+                       (buffer-size 0)
+                       (owner? #f))
+    (%open-deflate-port source compression-level
+                        window-bits memory-level
+                        strategy dictionary
+                        buffer-size owner?)))
+
+;; utility procedures
+(define (deflate-string str . args)
+  (call-with-output-string
+    (lambda (p)
+      (let1 p2 (apply open-deflate-port p args)
+        (display str p2)
+        (close-output-port p2)))))
+
+(define (inflate-string str . args)
+  (port->string
+   (apply open-inflate-port
+          (open-input-string str)
+          args)))
+
+(define (gzip-encode-string str . args)
+  (call-with-output-string
+    (lambda (p)
+      (let1 p2 (apply open-deflate-port p
+                      :window-bits (+ 15 16)
+                      args)
+        (display str p2)
+        (close-output-port p2)))))
+
+(define (gzip-decode-string str . args)
+  (port->string
+   (apply open-inflate-port
+          (open-input-string str)
+          :window-bits (+ 15 16)
+          args)))
+
+(provide "rfc/zlib")
