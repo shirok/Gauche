02d47c4d48df79f7abaf59c7da31aa45973904d7
Shiro Kawai <shiro@acm.org>
2009-03-22 05:54:31 +0000

    added benchmark for sparse table
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@6587 c205566b-fb4e-0410-ab5c-8f0660156277

--- ext/sparse/bench.scm
+++ ext/sparse/bench.scm
@@ -38,12 +38,22 @@
   (dolist [n *problem-set*]
     (spvector-ref spv n)))
 
+(define (st-set st)
+  (dolist [n *problem-set*]
+    (sptable-set! st n n)))
+
+(define (st-ref st)
+  (dolist [n *problem-set*]
+    (sptable-ref st n n)))
+  
 (define (bench-speed)
   (let ([null-timer  (make <user-time-counter>)]
         [htset-timer (make <user-time-counter>)]
         [htref-timer (make <user-time-counter>)]
         [svset-timer (make <user-time-counter>)]
-        [svref-timer (make <user-time-counter>)])
+        [svref-timer (make <user-time-counter>)]
+        [stset-timer (make <user-time-counter>)]
+        [stref-timer (make <user-time-counter>)])
 
     (define (calc-time timer)
       (* (/. (- (time-counter-value timer) (time-counter-value null-timer))
@@ -59,12 +69,17 @@
         (hash-table-clear! ht))
       (let1 sv (make-spvector)
         (with-time-counter svset-timer (sv-set sv))
-        (with-time-counter svref-timer (sv-ref sv))))
+        (with-time-counter svref-timer (sv-ref sv)))
+      (let1 st (make-sptable 'eqv?)
+        (with-time-counter stset-timer (st-set st))
+        (with-time-counter stref-timer (st-ref st))))
 
     (print "Hash table insertion: "    (calc-time htset-timer))
     (print "Hash table lookup: "       (calc-time htref-timer))
     (print "Sparse vector insertion: " (calc-time svset-timer))
     (print "Sparse vector lookup: "    (calc-time svref-timer))
+    (print "Sparse table insertion: "  (calc-time stset-timer))
+    (print "Sparse table lookup: "     (calc-time stref-timer))
     ))
 
 (define (active-memory-size)
@@ -84,7 +99,9 @@
                       (bench-mem (lambda () (ht-set (make-hash-table 'eqv?)))))]
     [("svmem") (print "Sparse vector mem: "
                       (bench-mem (lambda () (sv-set (make-spvector)))))]
-    [_ (print "Usage: bench speed|htmem|svmem")])
+    [("stmem") (print "Sparse table mem: "
+                      (bench-mem (lambda () (st-set (make-sptable 'eqv?)))))]
+    [_ (print "Usage: bench speed|htmem|svmem|stmem")])
   0)
   
     
