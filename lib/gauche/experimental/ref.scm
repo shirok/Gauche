d275297df85b69037d61a59695518a2a4e026d6c
Shiro Kawai <shiro@acm.org>
2009-10-18 03:08:06 +0000

    experimentally attached compile-time expander to ~
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@6760 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/gauche/experimental/ref.scm
+++ lib/gauche/experimental/ref.scm
@@ -3,13 +3,13 @@
 ;;;  
 
 (define-module gauche.experimental.ref
+  (use util.match)
   (export ~))
 (select-module gauche.experimental.ref)
 
 ;; A small experiment to see how I feel this...
 ;;  (~ a b c d) => (ref (ref (ref a b) c) d)
-;; Ideally this should be a compiler-macro (we can't make it a macro,
-;; for we want to say (set! (~ x'y) val).
+
 (define ~
   (getter-with-setter
    (case-lambda
@@ -19,5 +19,17 @@
      [(obj selector val) ((setter ref) obj selector val)]
      [(obj selector selector2 . rest)
       (apply (setter ref) (ref obj selector) selector2 rest)])))
+
+;; EXPERIMENTAL EXPERIMENTAL
+((with-module gauche.internal attach-inline-transformer)
+ ~
+ (lambda (x r c)
+   (define (nested form)
+     (match form
+       [(obj sel) `(ref ,obj ,sel)]
+       [(obj sel . sels) (nested `((ref ,obj ,sel) ,@sels))]))
+   (match x
+     [(_ obj sel . sels) (nested (cdr x))]
+     [_ x])))
 
 (provide "gauche/experimental/ref")
