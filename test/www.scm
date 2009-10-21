19bad3dea0ec7b64ec9745e3f75b38e821d3d097
Shiro Kawai <shiro@acm.org>
2009-10-21 03:03:32 +0000

    mingw adaptation
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@6784 c205566b-fb4e-0410-ab5c-8f0660156277

--- test/www.scm
+++ test/www.scm
@@ -212,86 +212,91 @@ VGhpcyBpcyBhIHRlc3Qgc2VudGVuY2Uu
          (set! (cgi-test-environment-ref 'REMOTE_HOST) "foo.com")
          (cgi-test-environment-ref "REMOTE_HOST")))
 
-(sys-system "rm -rf test.o")
-(sys-mkdir "test.o" #o755)
+(cond-expand
+ [gauche.os.windows
+  ;; windows can't support #! magic anyways.
+  ]
+ [else
+  (sys-system "rm -rf test.o")
+  (sys-mkdir "test.o" #o755)
 
-(with-output-to-file "test.o/cgitest.cgi"
-  (lambda ()
-    (print "#!/bin/sh")
-    (print "echo Content-type: text/plain")
-    (print "echo")
-    (print "echo \"SERVER_NAME = $SERVER_NAME\"")
-    (print "echo \"REMOTE_HOST = $REMOTE_HOST\"")
-    (print "echo \"REQUEST_METHOD = $REQUEST_METHOD\"")
-    (print "echo \"CONTENT_TYPE = $CONTENT_TYPE\"")
-    (print "echo \"QUERY_STRING = $QUERY_STRING\"")))
+  (with-output-to-file "test.o/cgitest.cgi"
+    (lambda ()
+      (print "#!/bin/sh")
+      (print "echo Content-type: text/plain")
+      (print "echo")
+      (print "echo \"SERVER_NAME = $SERVER_NAME\"")
+      (print "echo \"REMOTE_HOST = $REMOTE_HOST\"")
+      (print "echo \"REQUEST_METHOD = $REQUEST_METHOD\"")
+      (print "echo \"CONTENT_TYPE = $CONTENT_TYPE\"")
+      (print "echo \"QUERY_STRING = $QUERY_STRING\"")))
 
-(sys-chmod "test.o/cgitest.cgi" #o755)
+  (sys-chmod "test.o/cgitest.cgi" #o755)
 
-(test* "call-with-cgi-script" '(("content-type" "text/plain"))
-       (call-with-cgi-script "test.o/cgitest.cgi"
-                             (lambda (p)
-                               (rfc822-header->list p)))
-       )
+  (test* "call-with-cgi-script" '(("content-type" "text/plain"))
+         (call-with-cgi-script "test.o/cgitest.cgi"
+           (lambda (p)
+             (rfc822-header->list p)))
+         )
 
-(test* "run-cgi-script->string-list"
-       '((("content-type" "text/plain"))
-         ("SERVER_NAME = localhost"
-          "REMOTE_HOST = foo.com"
-          "REQUEST_METHOD = GET"
-          "CONTENT_TYPE = "
-          "QUERY_STRING = "))
-       (receive r (run-cgi-script->string-list "test.o/cgitest.cgi")
-         r)
-       )
+  (test* "run-cgi-script->string-list"
+         '((("content-type" "text/plain"))
+           ("SERVER_NAME = localhost"
+            "REMOTE_HOST = foo.com"
+            "REQUEST_METHOD = GET"
+            "CONTENT_TYPE = "
+            "QUERY_STRING = "))
+         (receive r (run-cgi-script->string-list "test.o/cgitest.cgi")
+           r)
+         )
 
-(test* "run-cgi-script->string-list (using parameters/GET)"
-       '("SERVER_NAME = localhost"
-         "REMOTE_HOST = foo.com"
-         "REQUEST_METHOD = GET"
-         "CONTENT_TYPE = "
-         "QUERY_STRING = a=b&%26%26%24%26=%21%40%21%40")
-       (receive (_ body)
+  (test* "run-cgi-script->string-list (using parameters/GET)"
+         '("SERVER_NAME = localhost"
+           "REMOTE_HOST = foo.com"
+           "REQUEST_METHOD = GET"
+           "CONTENT_TYPE = "
+           "QUERY_STRING = a=b&%26%26%24%26=%21%40%21%40")
+         (receive (_ body)
            (run-cgi-script->string-list "test.o/cgitest.cgi"
-                                        :parameters '((a . b) (&&$& . !@!@)))
-         body))
+            :parameters '((a . b) (&&$& . !@!@)))
+           body))
 
-(test* "run-cgi-script->string-list (using parameters/HEAD)"
-       '("SERVER_NAME = localhost"
-         "REMOTE_HOST = foo.com"
-         "REQUEST_METHOD = HEAD"
-         "CONTENT_TYPE = "
-         "QUERY_STRING = a=b&%26%26%24%26=%21%40%21%40")
-       (receive (_ body)
+  (test* "run-cgi-script->string-list (using parameters/HEAD)"
+         '("SERVER_NAME = localhost"
+           "REMOTE_HOST = foo.com"
+           "REQUEST_METHOD = HEAD"
+           "CONTENT_TYPE = "
+           "QUERY_STRING = a=b&%26%26%24%26=%21%40%21%40")
+         (receive (_ body)
            (run-cgi-script->string-list "test.o/cgitest.cgi"
-                                        :environment '((REQUEST_METHOD . HEAD))
-                                        :parameters '((a . b) (&&$& . !@!@)))
-         body))
+            :environment '((REQUEST_METHOD . HEAD))
+            :parameters '((a . b) (&&$& . !@!@)))
+           body))
 
-(with-output-to-file "test.o/cgitest.cgi"
-  (lambda ()
-    (print "#!/bin/sh")
-    (print "echo Content-type: text/plain")
-    (print "echo")
-    (print "echo \"REQUEST_METHOD = $REQUEST_METHOD\"")
-    (print "echo \"CONTENT_TYPE = $CONTENT_TYPE\"")
-    (print "echo \"CONTENT_LENGTH = $CONTENT_LENGTH\"")
-    (print "echo \"QUERY_STRING = $QUERY_STRING\"")
-    (print "cat")))
+  (with-output-to-file "test.o/cgitest.cgi"
+    (lambda ()
+      (print "#!/bin/sh")
+      (print "echo Content-type: text/plain")
+      (print "echo")
+      (print "echo \"REQUEST_METHOD = $REQUEST_METHOD\"")
+      (print "echo \"CONTENT_TYPE = $CONTENT_TYPE\"")
+      (print "echo \"CONTENT_LENGTH = $CONTENT_LENGTH\"")
+      (print "echo \"QUERY_STRING = $QUERY_STRING\"")
+      (print "cat")))
        
-(test* "run-cgi-script->string-list (using parameters)"
-       '("REQUEST_METHOD = POST"
-         "CONTENT_TYPE = application/x-www-form-urlencoded"
-         "CONTENT_LENGTH = 29"
-         "QUERY_STRING = "
-         "a=b&%26%26%24%26=%21%40%21%40")
-       (receive (_ body)
+  (test* "run-cgi-script->string-list (using parameters)"
+         '("REQUEST_METHOD = POST"
+           "CONTENT_TYPE = application/x-www-form-urlencoded"
+           "CONTENT_LENGTH = 29"
+           "QUERY_STRING = "
+           "a=b&%26%26%24%26=%21%40%21%40")
+         (receive (_ body)
            (run-cgi-script->string-list "test.o/cgitest.cgi"
-                                        :environment '((REQUEST_METHOD . POST))
-                                        :parameters '((a . b) (&&$& . !@!@)))
-         body))
+            :environment '((REQUEST_METHOD . POST))
+            :parameters '((a . b) (&&$& . !@!@)))
+           body))
 
-(sys-system "rm -rf test.o")
+  (sys-system "rm -rf test.o")])
 
 
 (test-end)
