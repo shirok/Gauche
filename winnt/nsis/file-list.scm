e073f6d8e84e58470c1983dd7065511b5cb40db0
Shiro Kawai <shiro@acm.org>
2009-11-16 12:21:51 +0000

    updated nsis script to work with mingw gosh.  cygwin no longer required.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@6839 c205566b-fb4e-0410-ab5c-8f0660156277

--- winnt/nsis/file-list.scm
+++ winnt/nsis/file-list.scm
@@ -1,11 +1,13 @@
 ;; -*- coding: utf-8 -*-
 ;;  Copyright (c) 2008 ENDO Yasuyuki, All rights reserved. 
 ;;  Copyright (c) 2008 Kahua Project, All rights reserved. 
-;;  See COPYING for terms and conditions of using this software 
+;;  See COPYING for terms and conditions of using this software
 
 (use file.util)
 (use srfi-1)
 (use srfi-13)
+(use util.list)
+(use util.match)
 (use gauche.parameter)
 (use gauche.parseopt)
 (use text.tree)
@@ -16,28 +18,36 @@
 (define *install-prefix* "    File ")
 (define *delete-prefix* "    Delete /REBOOTOK ")
 (define *cr-lf* "\r\n")
-(define *path-separater* "\\")
+(define *path-separator* "\\")
 (define *root-path* (make-parameter #f))
 (define *file-list* (make-parameter #f))
 (define *current-version* (make-parameter #f))
 
 (define (replace-inst prefix path)
-  (regexp-replace (string->regexp prefix)
-          path *instdir*))
+  (let* ([pre1 (regexp-replace-all #/\// prefix "\\\\")]
+         [pre2 (regexp-replace-all #/\\/ pre1 "/")])
+    (cond [(or (string-prefix? pre1 path)
+               (string-prefix? pre2 path))
+           (string-replace path *instdir* 0 (string-length pre1))]
+          [else (error "Path prefix ~s does not match: ~a" prefix path)])))
+
+;; NSIS requires version number to be X.X.X.X
+(define (canonical-version version)
+  (string-join (take* (string-split (regexp-replace #/[-_].+$/ version "") #\.)
+                      4 #t "0")
+               "."))
 
 (define (file-list path)
   (directory-fold
    path cons '()
    :lister (lambda (path seed)
-	     (receive (d f)
-		      (directory-list2 path :add-path? #t :children? #t)
-		      (values d
-			      (cons (cons path f) seed))))))
+	     (receive (d f) (directory-list2 path :add-path? #t :children? #t)
+               (values d (acons path f seed))))))
 
 (define (initialize path version)
   (begin0
    (*root-path* path)
-   (*current-version* version)
+   (*current-version* (canonical-version version))
    (*file-list*
     (reverse
      (filter
@@ -49,51 +59,41 @@
   (cons
    #`",|*target-prefix*|,(change-path (replace-inst (*root-path*) (car ls))),|*cr-lf*|"
    (map change-path
-	(map
-	 (lambda (s) #`",|*install-prefix*|,|s|,|*cr-lf*|")
-	(cdr ls)))))
+	(map (lambda (s) #`",|*install-prefix*|,|s|,|*cr-lf*|") (cdr ls)))))
 
 (define (del-str ls)
-  (map
-   (lambda (s) #`",|*delete-prefix*|,(change-path (replace-inst (*root-path*) s)),|*cr-lf*|")
-   (cdr ls)))
+  (map (lambda (s) #`",|*delete-prefix*|,(change-path (replace-inst (*root-path*) s)),|*cr-lf*|")
+       (cdr ls)))
 
-(define (install-files)
-  (map inst-str (*file-list*)))
+(define (install-files) (map inst-str (*file-list*)))
 
-(define (delete-files)
-  (map del-str (*file-list*)))
+(define (delete-files)  (map del-str (*file-list*)))
 
 (define (include line)
-  (or
-   (and-let* ((match (#/^(.*)##\(include (.*)\)(.*\r\n)$/ line))
-	      (exp (call-with-input-string (match 2) (cut read <>))))
-	     (list (match 1) (eval exp (current-module)) (match 3)))
-   line))
+  (or (and-let* ((match (#/^(.*)##\(include (.*)\)(.*\r\n)$/ line))
+                 (exp (call-with-input-string (match 2) (cut read <>))))
+        (list (match 1) (eval exp (current-module)) (match 3)))
+      line))
 
-(define (include-all lis)
-  (map include lis))
+(define (include-all lis) (map include lis))
 
-(define (change-path str)
-  (string-join (string-split str #\/) *path-separater*))
+(define (change-path str) (string-join (string-split str #\/) *path-separator*))
 
 (define (read-line-crlf file)
   (let1 line (read-line file)
-	(if (eof-object? line) line
-	    #`",|line|,|*cr-lf*|")))
+    (if (eof-object? line) line #`",|line|,|*cr-lf*|")))
 
 (define (main args)
-  (if (> (length args) 3)
-      (let* ((path (cadr args))
-	     (in (caddr args))
-	     (version (cadddr args))
-	     (out (path-sans-extension in))
-	     (lines (file->list read-line-crlf in)))
-	(if (file-exists? path)
-	    (begin
-	      (initialize path version)
-	      (call-with-output-file out
-		(cut write-tree (include-all lines) <>)))
-	    (print "Path not exists: " path))
-	0)
-      (print "Usage: gosh " (car args) " <dist-path> <template-file> <gauche-version>")))
+  (match (cdr args)
+    [(path in version)
+     (let ([out (path-sans-extension in)]
+           [lines (file->list read-line-crlf in)])
+       (if (file-exists? path)
+         (begin
+           (initialize path version)
+           (call-with-output-file out
+             (cut write-tree (include-all lines) <>)))
+         (print "Path not exists: " path)))]
+    [else
+     (print "Usage: gosh " (car args) " <dist-path> <template-file> <gauche-version>")])
+  0)
