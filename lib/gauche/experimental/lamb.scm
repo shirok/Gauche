b47e2e98eb38b21217a4c606df27d29373acb319
Shiro Kawai <shiro@acm.org>
2009-11-16 00:00:18 +0000

    gauche.experimental.lamb: added ^a, ^b, etc... to see if they are useful.
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@6834 c205566b-fb4e-0410-ab5c-8f0660156277

--- lib/gauche/experimental/lamb.scm
+++ lib/gauche/experimental/lamb.scm
@@ -4,7 +4,8 @@
 
 (define-module gauche.experimental.lamb
   (use util.match)
-  (export ^ ^. ^* ^_))
+  (export ^ ^. ^* ^_ ^a ^b ^c ^d ^e ^f ^g ^h ^i ^j ^k ^l ^m
+          ^n ^o ^p ^q ^r ^s ^t ^u ^v ^w ^x ^y ^z))
 (select-module gauche.experimental.lamb)
 
 (define-syntax ^
@@ -19,7 +20,15 @@
   (syntax-rules ()
     [(^* . clauses) (match-lambda* . clauses)]))
 
-(define-macro (^_ . body)
-  `(lambda (_) ,@body))
+;; TODO: need to make 'lambda's hygineic!
+(define-macro (^-generator var)
+  (let1 name (string->symbol #`"^,var")
+    `(define-macro (,name . body)
+       `(lambda (,',var) ,@body))))
 
+(define-macro (define-^x . vars)
+  `(begin ,@(map (lambda (x) `(^-generator ,x)) vars)))
+
+(define-^x _ a b c d e f g h i j k l m n o p q r s t u v w x y z)
+    
 (provide "gauche/experimental/lamb")
