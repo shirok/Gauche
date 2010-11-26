67ba98d135bb0397ddb0a45e0d34ac62363cacd6
Shiro Kawai <shiro@acm.org>
2010-11-26 05:22:00 +0000

    start adding windows scripting examples
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche/trunk@7258 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ examples/windows/messagebox.scm
@@ -0,0 +1,21 @@
+(use os.windows)
+
+(define (main args)
+  (guard (e [else (sys-message-box #f (format "ERROR: ~a" (~ e'message))
+                                   (sys-basename *program-name*)
+                                   (logior MB_OK MB_ICONERROR))])
+    (messagebox-sample (sys-basename (car args))))
+  0)
+
+(define (messagebox-sample program-name)
+  (sys-message-box #f "Let's start!" program-name
+                   (logior MB_OK MB_ICONINFORMATION))
+  (let loop ((count 0))
+    (let1 r (sys-message-box
+             #f (format "You've counted to ~a.\nCount more?" count)
+             program-name (logior MB_YESNO MB_ICONQUESTION))
+      (if (= r IDYES)
+        (loop (+ count 1))
+        (sys-message-box #f #`"You counted up to ,|count|."
+                         program-name MB_OK))))
+  0)
