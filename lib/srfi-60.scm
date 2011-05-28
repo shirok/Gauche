58b040cf427b34ef7676a9124a793276a18b360b
Shiro Kawai <shiro@acm.org>
2011-05-28 08:22:59 -1000

    Support full srfi-60
    Although majority of functions in srfi-60 are supported as built-in
    procedures, the module defines compatibility aliases, and several
    new procedures, so it is useful having the srfi-60 module to cover
    the full range of the spec.

---
+++ lib/srfi-60.scm
@@ -0,0 +1,123 @@
+;;;
+;;;  srfi-60 - integers as bits
+;;;
+;;;  Since most procedures are already supported as builtins, this
+;;;  module only provides a thin wrapper to cover the complete srfi-60 spec.
+
+(define-module srfi-60
+  (use gauche.sequence)
+  (use srfi-42)
+  (export logand bitwise-and
+          logior bitwise-ior
+          logxor bitwise-xor
+          lognot bitwise-not
+          bitwise-if bitwise-merge
+          logtest any-bits-set?
+          logcount bit-count
+          integer-length
+          log2-binary-factors first-set-bit
+          logbit? bit-set?
+          copy-bit
+          bit-field
+          copy-bit-field
+          ash arithmetic-shift
+          rotate-bit-field
+          reverse-bit-field
+          integer->list
+          list->integer
+          booleans->integer))
+(select-module srfi-60)
+
+;;;
+;;; Bitwise operators
+;;;
+
+(define logand (with-module gauche logand))
+(define bitwise-and logand)
+(define logior (with-module gauche logior))
+(define bitwise-ior logior)
+(define logxor (with-module gauche logxor))
+(define bitwise-xor logxor)
+(define lognot (with-module gauche lognot))
+(define bitwise-not lognot)
+
+(define (bitwise-if mask n0 n1)
+  (logior (logand mask n0) (logand (lognot mask) n1)))
+(define bitwise-merge bitwise-if)
+
+(define logtest (with-module gauche logtest))
+(define any-bits-set? logtest)
+
+;;;
+;;; Integer properties
+;;;
+
+(define logcount (with-module gauche logcount))
+(define bit-count logcount)
+(define integer-length (with-module gauche integer-length))
+
+(define (log2-binary-factors n)
+  (- (integer-length (logand n (- n))) 1))
+(define first-set-bit log2-binary-factors)
+
+;;;
+;;; Bit within word
+;;;
+
+(define logbit? (with-module gauche logbit?))
+(define bit-set? logbit?)
+(define copy-bit (with-module gauche copy-bit))
+
+;;;
+;;; Field of bits
+;;;
+
+(define bit-field (with-module gauche bit-field))
+(define copy-bit-field (with-module gauche copy-bit-field))
+(define ash (with-module gauche ash))
+(define arithmetic-shift ash)
+
+(define (rotate-bit-field n count start end)
+  (if (or (>= start end) (= count 0))
+    n                                   ; trivial path
+    (let* ([mask (logxor (- (expt 2 end) 1) (- (expt 2 start) 1))]
+           [target (logand mask n)]
+           [xcount (mod count (- end start))])
+      (logior (logand (lognot mask) n)
+              (logand mask
+                      (logior (ash target xcount)
+                              (ash target (- (- (- end start) xcount)))))))))
+
+(define (reverse-bit-field n start end)
+  (if (>= start end)
+    n                                   ; trivial path
+    (let1 mask (logxor (- (expt 2 end) 1) (- (expt 2 start) 1))
+      (let loop ([m (logand n (lognot mask))]
+                 [i start]
+                 [j (- end 1)])
+        (if (= i end)
+          m
+          (loop (copy-bit j m (logbit? i n)) (+ i 1) (- j 1)))))))
+
+;;;
+;;; Bits as booleans
+;;;
+
+(define (integer->list n :optional (len (integer-length n)))
+  (list-ec (: i (- len 1) -1 -1) (logbit? i n)))
+
+(define (list->integer lis)
+  (cond [(null? lis) 0]
+        [(<= (length lis) (integer-length (greatest-fixnum)))
+         ;; fixnum range - it's faster to calculate intermediate results
+         (fold (^(bit v) (+ v v (if bit 1 0))) 0 lis)]
+        [else
+         ;; bignum range - it's faster to create list of integers and merge
+         (apply logior
+                (fold-with-index (^(i bit ns) (if bit (cons (expt 2 i) ns) ns))
+                                 '() (reverse lis)))]))
+
+(define (booleans->integer . lis) (list->integer lis))
+
+      
+    
--- test/srfi.scm
+++ test/srfi.scm
@@ -1277,4 +1277,56 @@
            (read-lines "tmp1.o")))
   )
 
+;;-----------------------------------------------------------------------
+(test-section "srfi-60")
+
+(use srfi-60)
+(test-module 'srfi-60)
+
+;; Most procedures are builtin and tested in test/numbers.scm.
+
+(test* "bitwise-if" #b01100110
+       (bitwise-if #b10101100 #b00110101 #b11001010))
+
+(for-each (^(n r) (test* (format "log2-binary-factors(~a)" n) r
+                         (log2-binary-factors n)))
+          '(0  1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16
+              -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 -13 -14 -15 -16)
+          '(-1 0  1  0  2  0  1  0  3  0   1   0   2   0   1   0   4
+               0  1  0  2  0  1  0  3  0   1   0   2   0   1   0   4))
+
+(define (test-rotate-bit-field n c s e r)
+  (test* (format "rotate-bit-field(~s,~a,~a,~a)" (number->string n 2) c s e) r
+         (number->string (rotate-bit-field n c s e) 2)))
+(test-rotate-bit-field #b0100 3 4 0 "100") ; trivial path
+(test-rotate-bit-field #b0100 3 0 4 "10")
+(test-rotate-bit-field #b0100 -1 0 4 "10")
+(test-rotate-bit-field #b0100 10 0 4 "1")
+(test-rotate-bit-field #b110100100010000 -1 5 9 "110100010010000")
+(test-rotate-bit-field #b110100100010000 1 5 9 "110100000110000")
+
+(define (test-reverse-bit-field n s e r)
+  (test* (format "reverse-bit-field(~s,~a,~a)" (number->string n 2) s e) r
+         (number->string (reverse-bit-field n s e) 2)))
+(test-reverse-bit-field #xa7 8 0 "10100111")
+(test-reverse-bit-field #xa7 0 8 "11100101")
+(test-reverse-bit-field #xa7 1 5 "10111001")
+
+(test* "integer->list" '(#t #f #f #t) (integer->list 9))
+(test* "integer->list" '(#f #f #t #f #f #t) (integer->list 9 6))
+(test* "list->integer" 9 (list->integer '(#t #f #f #t)))
+(test* "list->integer" 9 (list->integer '(#f #f #t #f #f #t)))
+;; tests bignum path
+(test* "list->integer" (+ (expt 2 63) (expt 2 62) (expt 2 31) (expt 2 30) 1)
+       (list->integer '(#t #t #f #f #f #f #f #f
+                        #f #f #f #f #f #f #f #f
+                        #f #f #f #f #f #f #f #f
+                        #f #f #f #f #f #f #f #f
+                        #t #t #f #f #f #f #f #f
+                        #f #f #f #f #f #f #f #f
+                        #f #f #f #f #f #f #f #f
+                        #f #f #f #f #f #f #f #t)))
+
+(test* "booleans->integer" 9 (booleans->integer #f #f #t #f #f #t))
+ 
 (test-end)
