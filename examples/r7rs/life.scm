be20dab563fbd0f50010bf58cc68e5d6bf375205
Shiro Kawai <shiro@acm.org>
2013-05-04 02:03:30 -1000

    R7RS "life" example

---
+++ examples/r7rs/life.scm
@@ -0,0 +1,13 @@
+(import (scheme base)
+        (only (example life) life)
+        (rename (prefix (example grid) grid-)
+                (grid-make make-grid)))
+
+(define grid (make-grid 24 24))
+(grid-set! grid 1 1 #t)
+(grid-set! grid 2 2 #t)
+(grid-set! grid 3 0 #t)
+(grid-set! grid 3 1 #t)
+(grid-set! grid 3 2 #t)
+
+(life grid 80)
