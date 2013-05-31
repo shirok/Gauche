32735f74a0ffbc7639a386ddd99f74c10d2a2e04
Shiro Kawai <shiro@acm.org>
2013-05-31 09:23:04 -1000

    Rewrote make-csv-reader for speed

--- lib/text/csv.scm
+++ lib/text/csv.scm
@@ -56,54 +56,65 @@
 
 (define-method initialize ((self <csv>) initargs)
   (next-method)
-  (let ((p (slot-ref self 'port)))
+  (let1 p (slot-ref self 'port)
     (unless (is-a? (slot-ref self 'port) <port>)
       (error "port must be given to instantiate <csv>"))
     (slot-set! self 'ioproc
                (if (input-port? p)
-                   (make-csv-reader (slot-ref self 'separator))
-                   (make-csv-writer (slot-ref self 'separator))))))
+                 (make-csv-reader (slot-ref self 'separator))
+                 (make-csv-writer (slot-ref self 'separator))))))
 
-(define (make-csv-reader separator . args)
-  (let* ((quote-char (get-optional args #\"))
-         (quote-string (string quote-char)))
-    (lambda (port)
-      (define (start line fields)
-        (if (eof-object? line)
-          (reverse! fields)
-          (let ((next (string-trim line #[ ])))
-            (if (string-prefix? quote-string next)
-              (quoted (string-drop next 1) fields '())
-              (noquote next fields)))))
-      (define (noquote line fields)
-        (cond ((string-index line separator)
-               => (lambda (i)
-                    (start (string-drop line (+ i 1))
-                           (cons (string-trim-right (string-take line i))
-                                 fields))))
-              (else (reverse! (cons (string-trim-right line #[ ]) fields)))))
-      (define (quoted line fields partial)
-        (cond ((eof-object? line) (error "unterminated quoted field"))
-              ((string-null? line)
-               (quoted (read-line port) fields (cons "\n" partial)))
-              (else
-               (receive (this next) (string-scan line quote-char 'both)
-                 (if this
-                   (if (string-prefix? quote-string next)
-                     (quoted (string-drop next 1) fields
-                             (list* quote-string this partial))
-                     (let ((next-next (string-scan next separator 'after))
-                           (f (string-concatenate-reverse (cons this partial))))
-                       (if next-next
-                         (start next-next (cons f fields))
-                         (reverse!(cons f fields)))))
-                   (quoted (read-line port) fields
-                           (list*  "\n" line partial)))))))
-      (let ((line (read-line port)))
-        (if (eof-object? line)
-          line
-          (start line '())))
-      )))
+;; API
+(define (make-csv-reader separator :optional (quote-char #\"))
+  (^[:optional (port #f)]
+    (if port
+      (with-input-from-port port (^[] (csv-reader separator quote-char)))
+      (csv-reader separator quote-char))))
+
+(define (csv-reader sep quo)
+  (define (skip-ws ch)
+    (cond [(eqv? ch sep) ch]
+          [(eof-object? ch) ch]
+          [(char-whitespace? ch) (skip-ws (read-char))]
+          [else ch]))
+
+  (define (eor? ch) (or (eof-object? ch) (eqv? ch #\newline)))
+
+  (define (start fields)
+    (let1 ch (skip-ws (read-char))
+      (cond [(eor? ch) (reverse (cons "" fields))]
+            [(eqv? ch sep) (start (cons "" fields))]
+            [(eqv? ch quo) (quoted fields)]
+            [else (unquoted (list ch) fields)])))
+
+  (define (unquoted chs fields)
+    (let loop ([ch (read-char)] [last chs] [chs chs])
+      (cond [(eor? ch) (reverse (cons (finish last) fields))]
+            [(eqv? ch sep) (start (cons (finish last) fields))]
+            [(char-whitespace? ch) (loop (read-char) last (cons ch chs))]
+            [else (let1 chs (cons ch chs)
+                    (loop (read-char) chs chs))])))
+
+  (define (finish rchrs) (list->string (reverse rchrs)))
+
+  (define (quoted fields)
+    (let loop ([ch (read-char)] [chs '()])
+      (cond [(eof-object? ch) (error "unterminated quoted field")]
+            [(eqv? ch quo)
+             (if (eqv? (peek-char) quo)
+               (begin (read-char) (loop (read-char) (cons quo chs)))
+               (quoted-tail (cons (finish chs) fields)))]
+            [else (loop (read-char) (cons ch chs))])))
+
+  (define (quoted-tail fields)
+    (let loop ([ch (read-char)])
+      (cond [(eor? ch) (reverse fields)]
+            [(eq? ch sep) (start fields)]
+            [else (loop (read-char))])))
+
+  (if (eof-object? (peek-char))
+    (eof-object)
+    (start '())))
 
 (define (make-csv-writer separator :optional (newline "\n") (quote-char #\"))
   (let* ((quote-string (string quote-char))
