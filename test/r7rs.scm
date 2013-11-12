e1e4ba916bda9227de1b8b4365066256efa3b82e
Shiro Kawai <shiro@acm.org>
2013-11-12 02:28:53 -1000

    Added r7rs test suite
    r7rs-tests.scm is taken from Chibi, which is read from r7rs.scm.
    we haven't added it to TESTS yet, for some tests fails because
    of the following reasons:
    
    - Missing exact complex number support.  Most of failures caused
      by this (including number writing tests).
    - Behavior of default 'write' (should recognize circular but not
      shared).
    - (features) doesn't have r7rs.  We add it once we complete r7rs
      features.
    
    To run r7rs test, go src and run ./gosh -ftest ../test/r7rs

---
+++ test/r7rs.scm
@@ -0,0 +1,49 @@
+;; wrapper of r7rs  -*- coding: utf-8 -*-
+
+;; fake (chibi test) used in r7rs-tests
+(define-module chibi.test
+  (use gauche.test)
+  (export test-begin test-end test-values (rename x:test test))
+
+  (define *nest-count* 0)
+
+  (define (test-begin msg)
+    (if (zero? *nest-count*)
+      (test-start msg)
+      (test-section msg))
+    (inc! *nest-count*))
+
+  (define (test-end)
+    (dec! *nest-count*)
+    (when (zero? *nest-count*)
+      (with-module gauche.test (test-end))))
+
+  (define-syntax x:test
+    (syntax-rules ()
+      [(_ expected expr)
+       (test (write-to-string 'expr) expected (lambda () expr)
+             (lambda (a b)
+               (if (and (inexact? a) (inexact? b)
+                        (finite? a) (finite? b)
+                        (not (zero? a)))
+                 (< (abs (- a b)) (* 10e-14 (abs (+ a b))))
+                 (test-check a b))))]))
+
+  (define-syntax test-values
+    (syntax-rules ()
+      [(_ expected expr)
+       (receive r expected
+         (test (write-to-string 'expr) r
+               (lambda () (values->list expr))))]))
+  )
+(provide "chibi/test")
+
+(define-module adaptor
+  (export include)
+  (define-syntax include (with-module gauche include)))
+(provide "adaptor")
+
+(extend r7rs)
+(import (adaptor))
+(include "r7rs-tests.scm")
+
