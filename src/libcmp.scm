shirok/Gauche

commit 6359b238b3bc766629f2f439124f8db186fb2644

Adjust make-comparator behavior on default procedures
Let make-comparator accept #t or #f for fallback behaviors, as
specified in srfi-114.  We modified ScmComparator struture to
support fallback compareFn/hashFn behavior (that is, if #f is
given to compare-proc at construction time,
comparator-comparison-procedure? should return #f, but
comparator-comparison-procedure should return a fallback proc.)
This technically breaks ABI compatibility but nobody should be
using comparator in 0.9.4 so it should be ok.

src/libcmp.scm
@@ -37,16 +37,49 @@
 ;;; Comparator (a la srfi-114)
 ;;;
 
-(select-module gauche)
-(define-cproc make-comparator (type-test equality-test comparison-proc hash
-                               :optional (name #f))
-  Scm_MakeComparator)
+(select-module gauche.internal)
+(define-cproc %make-comparator (type-test equality-test comparison-proc hash
+                                name no-compare::<boolean> no-hash::<boolean>)
+  (let* ([flags::ulong (logior (?: no-compare SCM_COMPARATOR_NO_ORDER 0)
+                               (?: no-hash SCM_COMPARATOR_NO_HASH 0))])
+    (result
+     (Scm_MakeComparator type-test equality-test comparison-proc hash
+                         name flags))))
 
+(define-in-module gauche (make-comparator type-test equality-test
+                                          comparison-proc hash
+                                          :optional (name #f))
+  (rlet1 self  ; referred by error proc
+      ;; We use <bottom> for applicability check except type-test, since
+      ;; those procs are only required to handle objects that passes type-test.
+      (let ([type (cond [(eq? type-test #t) (^_ #t)]
+                        [(applicable? type-test <top>) type-test]
+                        [else (error "make-comparator needs a one-argument procedure or #t as type-test, but got:" type-test)])]
+            [eq   (cond
+                   [(eq? equality-test #t)
+                    (if (applicable? comparison-proc <bottom> <bottom>)
+                      (^[a b] (= (comparison-proc a b) 0))
+                      (error "make-comparator needs a procedure as comparison-proc if equality-test is #t, but got:" comparison-proc))]
+                   [(applicable? equality-test <bottom> <bottom>) equality-test]
+                   [else (error "make-comparator needs a procedure or #t as equality-test, but got:" equality-test)])]
+            [cmp  (cond [(eq? comparison-proc #f)
+                         (^[a b] (errorf "can't compare objects by ~s: ~s vs ~s" self a b))]
+                        [(applicable? comparison-proc <bottom> <bottom>)
+                         comparison-proc]
+                        [else (error "make-comparator needs a procedure or #f as comparison-proc, but got:" comparison-proc)])]
+            [hsh  (cond [(eq? hash #f)
+                         (^[a] (errorf "~s doesn't have a hash function"))]
+                        [(applicable? hash <bottom>) hash]
+                        [else (error "make-comparator needs a procedure or #f as hash, but got:" hash)])])
+        (%make-comparator type eq cmp hash name
+                          (not comparison-proc) (not hash)))))
+
+(select-module gauche)
 (define-cproc comparator? (obj) ::<boolean> SCM_COMPARATORP)
 (define-cproc comparator-comparison-procedure? (c::<comparator>) ::<boolean>
-  (result (not (SCM_FALSEP (-> c compareFn)))))
+  (result (not (logior (-> c flags) SCM_COMPARATOR_NO_ORDER))))
 (define-cproc comparator-hash-function? (c::<comparator>) ::<boolean>
-  (result (not (SCM_FALSEP (-> c hashFn)))))
+  (result (not (logior (-> c flags) SCM_COMPARATOR_NO_HASH))))
 
 (define-cproc comparator-type-test-procedure (c::<comparator>) :constant
   (result (-> c typeFn)))

test/compare.scm
@@ -87,6 +87,25 @@
          (compare-matrix (map precedence *data*))
          (compare-matrix *data*)))
 
+;; comparator with no comparison proc / no hash proc
+
+(test* "comparator requires at least either equality or comparison"
+       (test-error)
+       (make-comparator #t #t #f #f))
+(test* "comparator equality fallback behavior" #t
+       ((comparator-equality-predicate (make-comparator #t #t (^[a b] 0) #f))
+        1 2))
+(test* "no comparison proc" #f
+       (comparator-comparison-procedure? (make-comparator #t eq? #f #f)))
+(test* "comparator fallback behavior" (test-error)
+       ((comparator-comparison-procedure (make-comparator #t eq? #f #f))
+        'a 'b))
+(test* "no hash function" #f
+       (comparator-hash-function? (make-comparator #t eq? #f #f)))
+(test* "hash fallback behavior" (test-error)
+       ((comparator-hash-procedure (make-comparator #t eq? #f #f)) 'a))
+
+
 (test-end)
 
 

src/compare.c
@@ -75,7 +75,7 @@ static ScmClassStaticSlotSpec comparator_slots[] = {
 
 ScmObj Scm_MakeComparator(ScmObj type, ScmObj eq,
                           ScmObj compare, ScmObj hash,
-                          ScmObj name)
+                          ScmObj name, u_long flags)
 {
     ScmComparator *c = SCM_NEW(ScmComparator);
     SCM_SET_CLASS(c, &Scm_ComparatorClass);
@@ -84,6 +84,7 @@ ScmObj Scm_MakeComparator(ScmObj type, ScmObj eq,
     c->eqFn = eq;
     c->compareFn = compare;
     c->hashFn = hash;
+    c->flags = flags;
     return SCM_OBJ(c);
 }
 

src/gauche/compare.h
@@ -41,10 +41,21 @@ SCM_DECL_BEGIN
 struct ScmComparatorRec {
     SCM_HEADER;
     ScmObj name;                /* debugging aid */
-    ScmObj typeFn;              /* proc or #t */
-    ScmObj eqFn;                /* proc or #t */
-    ScmObj compareFn;           /* proc or #f */
-    ScmObj hashFn;              /* proc or #f */
+    ScmObj typeFn;              /* proc */
+    ScmObj eqFn;                /* proc */
+    ScmObj compareFn;           /* proc */
+    ScmObj hashFn;              /* proc */
+    u_long flags;
+};
+
+/* comparators can be constructed with #f for comareFn and/or hashFn.
+   srfi-114 requires accessor functions to those slots returns dummy
+   procedures that raises an error.  We still need to be able to tell
+   these slots have valid procedures.  Instead of storing #f and tweak
+   accessors to return a dummy procedure, we keep the info in flags. */
+enum ScmComparatorFlags {
+    SCM_COMPARATOR_NO_ORDER = (1L<<0), /* 'compare' proc unavailable */
+    SCM_COMPARATOR_NO_HASH  = (1L<<1)  /* 'hash' proc unavailable */
 };
 
 SCM_CLASS_DECL(Scm_ComparatorClass);
@@ -54,8 +65,7 @@ SCM_CLASS_DECL(Scm_ComparatorClass);
 
 SCM_EXTERN ScmObj  Scm_MakeComparator(ScmObj type, ScmObj eq,
                                       ScmObj compare, ScmObj hash,
-                                      ScmObj name);
-
+                                      ScmObj name, u_long flags);
 
 /* Other genreic utilities */
 SCM_EXTERN int    Scm_Compare(ScmObj x, ScmObj y);

commit c31360b353d023bb14cd3bd589019805fdb66440

Fix a couple of bugs introduced in 6359b23

src/libcmp.scm
@@ -71,15 +71,15 @@
                          (^[a] (errorf "~s doesn't have a hash function"))]
                         [(applicable? hash <bottom>) hash]
                         [else (error "make-comparator needs a procedure or #f as hash, but got:" hash)])])
-        (%make-comparator type eq cmp hash name
+        (%make-comparator type eq cmp hsh name
                           (not comparison-proc) (not hash)))))
 
 (select-module gauche)
 (define-cproc comparator? (obj) ::<boolean> SCM_COMPARATORP)
 (define-cproc comparator-comparison-procedure? (c::<comparator>) ::<boolean>
-  (result (not (logior (-> c flags) SCM_COMPARATOR_NO_ORDER))))
+  (result (not (logand (-> c flags) SCM_COMPARATOR_NO_ORDER))))
 (define-cproc comparator-hash-function? (c::<comparator>) ::<boolean>
-  (result (not (logior (-> c flags) SCM_COMPARATOR_NO_HASH))))
+  (result (not (logand (-> c flags) SCM_COMPARATOR_NO_HASH))))
 
 (define-cproc comparator-type-test-procedure (c::<comparator>) :constant
   (result (-> c typeFn)))

test/compare.scm
@@ -95,11 +95,15 @@
 (test* "comparator equality fallback behavior" #t
        ((comparator-equality-predicate (make-comparator #t #t (^[a b] 0) #f))
         1 2))
+(test* "has comparison proc" #t
+       (comparator-comparison-procedure? (make-comparator #t eq? compare #f)))
 (test* "no comparison proc" #f
        (comparator-comparison-procedure? (make-comparator #t eq? #f #f)))
 (test* "comparator fallback behavior" (test-error)
        ((comparator-comparison-procedure (make-comparator #t eq? #f #f))
         'a 'b))
+(test* "has hash function" #t
+       (comparator-hash-function? (make-comparator #t eq? #f hash)))
 (test* "no hash function" #f
        (comparator-hash-function? (make-comparator #t eq? #f #f)))
 (test* "hash fallback behavior" (test-error)