bcc28da2b061626fa560c82378f4d1d278b7059f
Shiro Kawai <shiro@acm.org>
2015-04-09 15:47:23 -1000

    Precompile gauche.record

--- libsrc/gauche/record.scm
+++ libsrc/gauche/record.scm
@@ -73,23 +73,8 @@
 
 ;; some convenience utility
 
-;; concatenate symbols or identifiers.  If any one of args is an identifier,
-;; the result is also an identifier with the same scope of the first
-;; identifier in the args.
-(define (sym+ . args)
-  (define (namecat args)
-    ($ string->symbol $ apply string-append
-       $ map (^s (x->string
-                  (if (identifier? s)
-                    ((with-module gauche.internal identifier-name) s)
-                    s)))
-       args))
-  (if-let1 first-id (find identifier? args)
-    ($ (with-module gauche.internal make-identifier)
-       (namecat args)
-       ((with-module gauche.internal identifier-module) first-id)
-       ((with-module gauche.internal identifier-env) first-id))
-    (namecat args)))
+(define-macro (sym+ . args)
+  `((with-module gauche.internal identifier-append) ,@args))
 
 (define-macro (for-each-subst subs . forms)
   (define (walk x sub)
