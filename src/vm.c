488907178d85e3c1b422195e6a9f1513ca94a4f4
Shiro Kawai <shiro@acm.org>
2015-12-06 15:18:57 -1000

    Fixed a race condition bug in vm->stopRequest

--- src/vm.c
+++ src/vm.c
@@ -2646,9 +2646,11 @@ static void process_queued_requests(ScmVM *vm)
        See Scm_ThreadStop() in ext/threads/threads.c */
     if (vm->stopRequest) {
         SCM_INTERNAL_MUTEX_SAFE_LOCK_BEGIN(vm->vmlock);
-        /* Double check, since stopRequest can be canceled between the above
-           two lines. */
         switch (vm->stopRequest) {
+        case 0:
+            /* stopRequest is canceled between the last check and
+               LOCK_BEGIN. We do nothing. */
+            break;
         case SCM_VM_REQUEST_SUSPEND:
             vm->stopRequest = 0;
             vm->state = SCM_VM_STOPPED;
