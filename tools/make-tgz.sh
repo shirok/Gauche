c538db244d24d565573f8e9f04e41c9745ee9d64
yokota <qykth-git@users.noreply.github.com>
2015-12-23 03:32:07 +0900

    Add Git based distribution file generator
    It makes similar file as "./DIST tgz" command.

---
+++ tools/make-tgz.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# Build distribution archive
+#
+
+set -e
+
+topdir=$(pwd)
+
+VERSION=$(cat VERSION)
+name=Gauche-${VERSION}
+
+t=$(mktemp -d)
+g_dir=${t}/${name}
+
+#
+# Build distribution source tree
+#
+git archive --prefix=${name}/ HEAD | tar -x --directory=${t}
+
+#
+# Build generated files
+#
+cd ${g_dir}
+./DIST gen
+./configure
+make GOSH=gosh pre-package
+make distclean
+
+#
+# Make final archive file
+#
+cd ${t}
+tar -c --exclude-from=${g_dir}/DIST_EXCLUDE -f ${name}.tar ${name}
+gzip -9 ${name}.tar
+cp ${name}.tar.gz ${topdir}/../${name}.tgz
+
+#
+# Cleanup
+#
+cd ${topdir}
+rm -rf ${t}
