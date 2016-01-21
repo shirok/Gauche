798550c87c47079a419b2461418e865217eb519b
Shiro Kawai <shiro@acm.org>
2016-01-21 02:25:13 -1000

    compat.chibi-test - Handle include specially

--- lib/compat/chibi-test.scm
+++ lib/compat/chibi-test.scm
@@ -13,7 +13,7 @@
 (define gauche:test-error test-error)
 
 (define-syntax chibi-test
-  (syntax-rules (test-group test test-assert test-error parameterize)
+  (syntax-rules (test-group test test-assert test-error include parameterize)
     [(_ (test-group name . forms) . rest)
      (begin (chibi-test . forms)
             (chibi-test . rest))]
@@ -32,6 +32,23 @@
     [(_ (parameterize bindings . forms) . rest)
      (begin (gauche:parameterize bindings (chibi-test . forms))
             (chibi-test . rest))]
+    [(_ (include file) . rest)
+     ;; NB: We need to expand include by ourselves, for we must splice
+     ;; test-* forms in the included file.
+     (chibi-test:include file rest)]
     [(_ form . rest)
      (let () form (chibi-test . rest))]
     [(_) (begin)]))
+
+(define-syntax chibi-test:include
+  (er-macro-transformer
+   (^[f r c]
+     (let ([file (cadr f)]
+           [rest (caddr f)])
+       (let1 iport ((with-module gauche.internal pass1/open-include-file)
+                    file (current-load-path))
+         (unwind-protect
+             `(,(r 'chibi-test)
+               ,@(port->sexp-list iport)
+               ,@rest)
+           (close-port iport)))))))
