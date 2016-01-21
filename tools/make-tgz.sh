c5fb1fb968f2e24732d459cc6b14d752eeb4da68
yokota <qykth-git@users.noreply.github.com>
2016-01-21 22:42:56 +0900

    Enclose path names with quote

--- tools/make-tgz.sh
+++ tools/make-tgz.sh
@@ -13,20 +13,20 @@ set -e
 topdir=$(pwd)
 
 VERSION=$(cat VERSION)
-name=Gauche-${VERSION}
+name="Gauche-${VERSION}"
 
 t=$(mktemp -d)
-g_dir=${t}/${name}
+g_dir="${t}/${name}"
 
 #
 # Build distribution source tree
 #
-git archive --prefix=${name}/ HEAD | tar -x --directory=${t}
+git archive --prefix="${name}/" HEAD | tar -x --directory="${t}"
 
 #
 # Build generated files
 #
-cd ${g_dir}
+cd "${g_dir}"
 ./DIST gen
 ./configure
 make GOSH=gosh pre-package
@@ -35,13 +35,13 @@ make distclean
 #
 # Make final archive file
 #
-cd ${t}
-tar -c --exclude-from=${g_dir}/DIST_EXCLUDE --owner=root --group=root --sort=name -f ${name}.tar ${name}
-gzip -9 ${name}.tar
-cp ${name}.tar.gz ${topdir}/../${name}.tgz
+cd "${t}"
+tar -c --exclude-from="${g_dir}/DIST_EXCLUDE" --owner=root --group=root --sort=name -f "${name}.tar" "${name}"
+gzip -9 "${name}.tar"
+cp "${name}.tar.gz" "${topdir}/../${name}.tgz"
 
 #
 # Cleanup
 #
-cd ${topdir}
-rm -rf ${t}
+cd "${topdir}"
+rm -rf "${t}"
