28d75053392a603d7cc4074eadbc2624b04740a1
Shiro Kawai <shiro@acm.org>
2016-03-26 18:14:56 -1000

    Fix bugs srfi-113
    vector-binary-search - have to rule out empty vec case.  we also
    extended to accept optional start/end arg, as we did for srfi-43.
    
    %vector-check-start+end - needed more precise check when open-end?==#t case
    
    vector-reverse-copy! - handle overlapping case
    
    We also moved the source from ext/srfi to libsrc, in order to
    prepare srfi-43 to depend srfi-113.

--- libsrc/srfi-133.scm
+++ libsrc/srfi-133.scm
@@ -90,9 +90,14 @@
     (unless (or (and open-end? (<= e len))
                 (<= 0 e len))
       (errorf "end index (~s) out of range of a vector: ~s" e v))
-    (unless (or open-end? (<= s e))
+    (unless (or (and open-end? (< e 0)) (<= s e))
       (errorf "start index (~s) is greater than end index (~s)" s e))
     (values s (if (and open-end? (< e 0)) len e))))
+
+;; vector mutability check
+;; currently Gauche doesn't track vector mutability (which is a bad thing)
+;; eventually we'll check immutable vectors here.
+(define-inline (%ensure-mutable v) (values))
 
 (define %vector-unfold!
   (case-lambda
@@ -146,6 +151,7 @@
 
 (define (vector-unfold! f rvec s e . seeds)
   (check-arg vector? rvec)
+  (%ensure-mutable rvec)
   (let1 max (- (vector-length rvec) 1)
     (unless (<= 0 s max) (error "start index out of range:" s))
     (unless (<= 0 e max) (error "end index out of range:" e))
@@ -155,6 +161,7 @@
 
 (define (vector-unfold-right! f rvec s e . seeds)
   (check-arg vector? rvec)
+  (%ensure-mutable rvec)
   (let1 max (- (vector-length rvec) 1)
     (unless (<= 0 s max) (error "start index out of range:" s))
     (unless (<= 0 e max) (error "end index out of range:" e))
@@ -176,13 +183,22 @@
 
 (define (vector-reverse-copy! target tstart src :optional (sstart 0) (send -1))
   (check-arg vector? target)
+  (%ensure-mutable target)
   (let1 tlen (vector-length target)
     (unless (<= 0 tstart tlen)
       (errorf "target start index (~s) out of range of a target vector" tstart))
     (receive (s e) (%vector-check-start+end src sstart send #t)
       (unless (<= (- e s) (- tlen tstart))
         (errorf "source range (~s,~s) is greater than the target vector" s e))
-      (%vector-reverse-copy! target tstart src s e))))
+      (if (and (eq? target src)
+               (or (and (<= s tstart) (< tstart e))
+                   (and (< tstart s) (< s (+ tstart (- e s))))))
+        ;; overlapping self copy.  we need extra step not to overwrite
+        ;; before copy.
+        (begin
+          (vector-copy! target tstart src s e)
+          (%vector-reverse! target tstart (+ tstart (- e s))))
+        (%vector-reverse-copy! target tstart src s e)))))
 
 (define (vector-concatenate vecs) (apply vector-append vecs))
 
@@ -239,8 +255,9 @@
     ([proc seed v . vs]
      (let* ([vs (cons v vs)]
             [len (fold (^[v len]
-                         (check-arg vector? v) (min (vector-length v) len))
-                       +inf.0 vs)])
+                         (check-arg vector? v)
+                         (if len (min (vector-length v) len) (vector-length v)))
+                       #f vs)])
        (let loop ([i 0] [seed seed])
          (if (= i len)
            seed
@@ -256,8 +273,9 @@
     ([proc seed v . vs]
      (let* ([vs (cons v vs)]
             [len (fold (^[v len]
-                         (check-arg vector? v) (min (vector-length v) len))
-                       +inf.0 vs)])
+                         (check-arg vector? v)
+                         (if len (min (vector-length v) len) (vector-length v)))
+                       #f vs)])
        (let loop ([i (- len 1)] [seed seed])
          (if (= i -1)
            seed
@@ -326,16 +344,17 @@
 (define (vector-skip-right pred v . vs)
   (apply vector-index-right (complement pred) v vs))
 
-(define (vector-binary-search vec value cmp)
+(define (vector-binary-search vec value cmp :optional (start 0) (end -1))
   (check-arg vector? vec)
-  (let1 len (vector-length vec)
-    (let rec ([lo 0] [hi len])
-      (let* ([mid (ash (+ lo hi) -1)]
-             [r (cmp (vector-ref vec mid) value)])
-        (cond [(zero? r) mid]
-              [(= lo mid) #f]
-              [(< r 0) (rec mid hi)]
-              [else (rec lo mid)])))))
+  (receive (s e) (%vector-check-start+end vec start end #t)
+    (and (not (= s e))
+         (let rec ([lo s] [hi e])
+           (let* ([mid (ash (+ lo hi) -1)]
+                  [r (cmp (vector-ref vec mid) value)])
+             (cond [(zero? r) mid]
+                   [(= lo mid) #f]
+                   [(< r 0) (rec mid hi)]
+                   [else (rec lo mid)]))))))
      
 (define vector-any
   (case-lambda
@@ -402,18 +421,23 @@
 
 (define (vector-swap! vec i j)
   (check-arg vector? vec)
+  (%ensure-mutable vec)
   (let1 max (- (vector-length vec) 1)
     (unless (<= 0 i max) (error "index i is out of range:" i))
     (unless (<= 0 j max) (error "index j is out of range:" j))
     (%vector-swap! vec i j)))
 
+(define (%vector-reverse! vec s e)
+  (do ([i s (+ i 1)]
+       [j (- e 1) (- j 1)])
+      [(<= j i)]
+    (%vector-swap! vec i j)))
+
 (define (vector-reverse! vec :optional (start 0) (end -1))
   (check-arg vector? vec)
+  (%ensure-mutable vec)
   (receive (s e) (%vector-check-start+end vec start end #t)
-    (do ([i s (+ i 1)]
-         [j (- e 1) (- j 1)])
-        [(<= j i)]
-      (%vector-swap! vec i j))))
+    (%vector-reverse! vec s e)))
   
 (define (reverse-vector->list vec :optional (start 0) (end -1))
   (check-arg vector? vec)
