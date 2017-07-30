2b5e33dafdea246c67fc38239c4ea6c0b27eb060
Shiro Kawai <shiro@acm.org>
2017-07-29 21:58:40 -1000

    For srfi-121, only re-export bindings defined in the srfi
    Gauche.generator is upper-compatible to srfi-121, but for the
    better compatiblity, it's safer to restrict exported symbols
    from srfi-121 just as defined in the srfi.

--- lib/srfi-121.scm
+++ lib/srfi-121.scm
@@ -2,6 +2,20 @@
 ;;; SRFI-121 Generators
 ;;;
 
-;; Procedures of SRFI-121 are provided from gauche.generator
+;; Procedures of srfi-121 are provided from gauche.generator
+;; We re-export srfi-121 specific bindings
 
-(define-module srfi-121 (extend gauche.generator))
+(define-module srfi-121
+  (use gauche.generator)
+  (export generator make-iota-generator make-range-generator 
+          make-coroutine-generator list->generator vector->generator
+          reverse-vector->generator string->generator
+          bytevector->generator
+          make-for-each-generator make-unfold-generator
+          gcons* gappend gcombine gfilter gremove 
+          gtake gdrop gtake-while gdrop-while
+          gdelete gdelete-neighbor-dups gindex gselect
+          generator->list generator->reverse-list
+          generator->vector generator->vector!  generator->string
+          generator-fold generator-for-each generator-find
+          generator-count generator-any generator-every generator-unfold))
