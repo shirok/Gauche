3af6be96f4c2cc08c438ed3a9a6fcd91ca0d874a
yokota <qykth-git@users.noreply.github.com>
2018-08-07 22:56:57 +0900

    Refactor make(1) command usage
    Use "-s" option to remove unwanted lines.
    This fix might be better result for non-English or non-GNU-make environment.

--- src/list-ext-objects.sh
+++ src/list-ext-objects.sh
@@ -7,4 +7,4 @@
 
 top_builddir=$1
 
-${MAKE:=make} -C "${top_builddir}/ext" list-objects | grep $top_builddir | grep -v 'make\[.*\]: \(Entering\|Leaving\) directory'
+${MAKE:=make} -C "${top_builddir}/ext" -s list-objects
