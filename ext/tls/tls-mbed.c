c3deec918bfa96fbe1b451c43b0b4909f5b2dcae
yokota <qykth-git@users.noreply.github.com>
2018-11-03 15:06:42 +0900

    Close handle when done the job

--- ext/tls/tls-mbed.c
+++ ext/tls/tls-mbed.c
@@ -93,7 +93,13 @@ static inline ScmObj inject_cert(ScmMbedTLS *t)
 		     CERT_STORE_MAXIMUM_ALLOWED_FLAG |
 		     CERT_SYSTEM_STORE_LOCAL_MACHINE),
 		    CERT_PHYSICAL_STORE_AUTH_ROOT_NAME);
-  CertControlStore(h, 0, CERT_STORE_CTRL_AUTO_RESYNC, NULL);
+  if (h == NULL) { return SCM_FALSE; }
+
+  if(!CertControlStore(h, 0, CERT_STORE_CTRL_AUTO_RESYNC, NULL)) {
+    CertCloseStore(h, 0);
+    return SCM_FALSE;
+  }
+
 
   PCCERT_CONTEXT ctx = NULL;
   while(1) {
@@ -106,6 +112,7 @@ static inline ScmObj inject_cert(ScmMbedTLS *t)
     mbedtls_x509_crt_parse_der(t->ca, ctx->pbCertEncoded, size);
   }
 
+  CertCloseStore(h, 0);
   return SCM_TRUE;
 #else
     (void)t;
