d111b5e52094ae006d391af44784800efc47c13c
yokota <qykth-git@users.noreply.github.com>
2018-11-09 00:29:42 +0900

    Refactor

--- ext/tls/tls-mbed.c
+++ ext/tls/tls-mbed.c
@@ -173,13 +173,15 @@ static ScmObj mbed_connect(ScmTLS* tls, int fd)
         Scm_Error("Parameter tls-ca-bundle-path must have a string value,"
                   " but got: %S", s_ca_file);
     }
-    const char *ca_file = Scm_GetStringConst(SCM_STRING(s_ca_file));
     if(Scm_StringEqual(SCM_STRING(s_ca_file), SCM_STRING(SCM_MAKE_STR("@system")))) {
         if(SCM_FALSEP(load_system_cert(t))) {
             Scm_SysError("Can't load certificates from system certificate store");
         }
-    } else if(mbedtls_x509_crt_parse_file(&t->ca, ca_file) != 0) {
-        Scm_SysError("mbedtls_x509_crt_parse_file() failed: file=%S", s_ca_file);
+    } else {
+        const char *ca_file = Scm_GetStringConst(SCM_STRING(s_ca_file));
+        if(mbedtls_x509_crt_parse_file(&t->ca, ca_file) != 0) {
+            Scm_SysError("mbedtls_x509_crt_parse_file() failed: file=%S", s_ca_file);
+        }
     }
     mbedtls_ssl_conf_ca_chain(&t->conf, &t->ca, NULL);
     mbedtls_ssl_conf_authmode(&t->conf, MBEDTLS_SSL_VERIFY_REQUIRED);
