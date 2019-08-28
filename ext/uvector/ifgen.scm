5bba8c4b84f385de48fcc7e16c4369e3ef515275
Shiro Kawai <shiro@acm.org>
2019-08-27 17:52:13 -1000

    Generate gauche.uvector.base module

--- ext/uvector/ifgen.scm
+++ ext/uvector/ifgen.scm
@@ -8,12 +8,14 @@
 
 (define (usage)
   (print "Usage: gosh ./ifgen.scm TAG")
+  (print "   or: gosh ./ifgen.scm base")
   (print "   or: gosh ./ifgen.scm exports")
   (exit 1))
 
 (define (main args)
   (match (cdr args)
     [("exports") (gen-exports)]
+    [("base") (gen-base)]
     [(tag) (gen-specific tag)]
     [_ (usage)])
  0)
@@ -28,51 +30,72 @@
       (print ";; Generated automatically.  Do not edit")
       (pprint `(define-module ,(symbol-append 'gauche.uvector. tag)
                  (use gauche.uvector)
+                 (export ,@(subst tag *srfi-160-base-api*))
                  (export ,@(subst tag *srfi-160-api*)))))))
+
+(define (gen-base)
+  (with-output-to-file "uvector/base.scm"
+    (^[]
+      (print ";; Generated automatically.  Do not edit")
+      (pprint `(define-module gauche.uvector.base
+                 (use gauche.uvector)
+                 ,@(map (^[tag] `(export ,@(subst tag *srfi-160-base-api*)))
+                        '(u8 s8 u16 s16 u32 s32 u64 s64
+                          f32 f64 c64 c128)))))))
+      
 
 (define (gen-exports)
   (with-output-to-file "exports.scm"
     (^[]
       (print ";; Generated automatically.  Do not edit")
       (pprint `(export
-                ,@(append-map (cute subst <> (append *srfi-160-api*
+                ,@(append-map (cute subst <> (append *srfi-160-base-api*
+                                                     *srfi-160-api*
                                                      *extra-api*
                                                      *extra-api-integral*
                                                      *extra-api-scalar*))
                               '(u8 s8))
-                ,@(append-map (cute subst <> (append *srfi-160-api*
+                ,@(append-map (cute subst <> (append *srfi-160-base-api*
+                                                     *srfi-160-api*
                                                      *extra-api*
                                                      *extra-api-integral*
                                                      *extra-api-scalar*
                                                      *extra-api-multibyte*))
                               '(u16 s16 u32 s32 u64 s64))
-                ,@(append-map (cute subst <> (append *srfi-160-api*
+                ,@(append-map (cute subst <> (append *srfi-160-base-api*
+                                                     *srfi-160-api*
                                                      *extra-api*
                                                      *extra-api-real*
                                                      *extra-api-scalar*
                                                      *extra-api-multibyte*))
                               '(f16 f32 f64))
-                ,@(append-map (cute subst <> (append *srfi-160-api*
+                ,@(append-map (cute subst <> (append *srfi-160-base-api*
+                                                     *srfi-160-api*
                                                      *extra-api*
                                                      *extra-api-real*))
                               '(c32 c64 c128)))))))
 
-(define *srfi-160-api*
+(define *srfi-160-base-api*
   '(make-@vector
     @vector
-    @vector-unfold
+    @vector?
+    @vector-length
+    @vector-ref
+    @vector-set!
+    @vector->list
+    list->@vector
+    @?))
+
+(define *srfi-160-api*
+  '(@vector-unfold
     @vector-unfold-right
     @vector-copy
     @vector-reverse-copy
     @vector-append
     @vector-concatenate
     @vector-append-subvectors
-    @?
-    @vector?
     @vector-empty?
     @vector=
-    @vector-ref
-    @vector-length
     @vector-take
     @vector-take-right
     @vector-drop
@@ -98,14 +121,11 @@
     @vector-partition
     @vector-filter
     @vector-remove
-    @vector-set!
     @vector-swap!
     @vector-fill!
     @vector-reverse!
     @vector-copy!
     @vector-reverse-copy!
-    @vector->list
-    list->@vector
     reverse-@vector->list
     @vector->vector
     vector->@vector
