b6ca1d9478c2b38bf9dc9c041c71c02581eacebd
yokota <14939671+qykth-git@users.noreply.github.com>
2020-03-14 21:48:49 +0900

    Cosmetic fix

--- tools/scoop/make-scoop-manifesto.scm
+++ tools/scoop/make-scoop-manifesto.scm
@@ -6,9 +6,12 @@
 ;;  $ gosh make-scoop-manifasto.scm --version="version number" --msi64="64bit MSI file" --msi32="32bit MSI file" > gauche.json
 ;;
 ;;
-;; NB: Output JSON file uses commpressed format. Use pretty printer for
+;; NB: Output JSON file uses commpressed format. Use JSON pretty printer for
 ;;     human-readable form.
-;;  $ emacs --batch --file=gauche.json --load=json --funcall=json-pretty-print-buffer --eval="(set-buffer-file-coding-system 'utf-8-dos)" --funcall=save-buffer
+;;  $ emacs --batch --file=gauche.json --load=json \
+;;          --funcall=json-pretty-print-buffer \
+;;          --eval="(set-buffer-file-coding-system 'utf-8-dos)" \
+;;          --funcall=save-buffer
 ;;
 
 (use rfc.json)
