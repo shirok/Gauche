de38364279fddd336f746fe59a29a6002ab45929
yokota <14939671+qykth-git@users.noreply.github.com>
2020-03-14 23:39:35 +0900

    Add comment

--- tools/scoop/batch-pretty-print-json.el
+++ tools/scoop/batch-pretty-print-json.el
@@ -2,8 +2,10 @@
 
 (javascript-mode)
 
+;; Scoop suggests 4-space indentation
 (setq json-encoding-default-indentation "    ")
 (json-pretty-print-buffer)
 
+;; Scoop recommends UTF-8 with MSDOS style line feed
 (set-buffer-file-coding-system 'utf-8-dos)
 (save-buffer)
