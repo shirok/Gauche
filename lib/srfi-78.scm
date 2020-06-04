080759e957e5f30703a92e657ade594f28b565f6
Shiro Kawai <shiro@acm.org>
2020-06-04 12:06:50 -1000

    Replaced srfi-78 with integration of gauche.test

--- lib/srfi-78.scm
+++ lib/srfi-78.scm
@@ -1,270 +1,131 @@
 ;;;
-;;; srfi-78, taken from the reference implementation.
+;;; srfi-78 - Lightweight testing
+;;;
+;;;   Copyright (c) 2020  Shiro Kawai  <shiro@acm.org>
+;;;
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;
+;;;   3. Neither the name of the authors nor the names of its contributors
+;;;      may be used to endorse or promote products derived from this
+;;;      software without specific prior written permission.
+;;;
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ;;;
 
-;; Adapted for Gauche
+;; We realize srfi-78 as a wrapper of gauche.test, so that the tests
+;; using srfi-78 can be seamlessly embedded in other Gauche tests.
+;; 
 
 (define-module srfi-78
+  (use gauche.test)
+  (use gauche.parameter)
   (use srfi-42)
   (export check check-ec check-report check-set-mode! check-reset!
           check-passed?))
 (select-module srfi-78)
 
-;
-; Copyright (c) 2005-2006 Sebastian Egner.
-; 
-; Permission is hereby granted, free of charge, to any person obtaining
-; a copy of this software and associated documentation files (the
-; ``Software''), to deal in the Software without restriction, including
-; without limitation the rights to use, copy, modify, merge, publish,
-; distribute, sublicense, and/or sell copies of the Software, and to
-; permit persons to whom the Software is furnished to do so, subject to
-; the following conditions:
-; 
-; The above copyright notice and this permission notice shall be
-; included in all copies or substantial portions of the Software.
-; 
-; THE SOFTWARE IS PROVIDED ``AS IS'', WITHOUT WARRANTY OF ANY KIND,
-; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-; 
-; -----------------------------------------------------------------------
-; 
-; Lightweight testing (reference implementation)
-; ==============================================
-;
-; Sebastian.Egner@philips.com
-; in R5RS + SRFI 23 (error) + SRFI 42 (comprehensions)
-;
-; history of this file:
-;   SE, 25-Oct-2004: first version based on code used in SRFIs 42 and 67
-;   SE, 19-Jan-2006: (arg ...) made optional in check-ec
-;
-; Naming convention "check:<identifier>" is used only internally.
+(define check-mode (make-parameter 'report))
 
-; -- portability --
-
-; PLT:      (require (lib "23.ss" "srfi") (lib "42.ss" "srfi"))
-; Scheme48: ,open srfi-23 srfi-42 
-
-; -- utilities --
-
-(define check:write write)
-
-; You can also use a pretty printer if you have one.
-; However, the output might not improve for most cases
-; because the pretty printers usually output a trailing
-; newline.
-
-; PLT:      (require (lib "pretty.ss")) (define check:write pretty-print)
-; Scheme48: ,open pp (define check:write p)
-
-; -- mode --
-
-(define check:mode #f)
-
+;; API
+;; NB: The report-as-you-go feature is delegated to gauche.test, so
+;; the check mode other than off doesn't make any difference.
 (define (check-set-mode! mode)
-  (set! check:mode
-        (case mode
-          ((off)           0)
-          ((summary)       1)
-          ((report-failed) 10)
-          ((report)        100)
-          (else (error "unrecognized mode" mode)))))
+  (ecase mode
+    [(off summary report-failed report) (check-mode mode)]))
 
-(check-set-mode! 'report)
+;; we track this as well as gauche.test
+(define check-pass-count (make-parameter 0))
+(define check-total-count (make-parameter 0))
+(define check-failure (make-parameter #f))
 
-; -- state --
-
-(define check:correct #f)
-(define check:failed   #f)
-
+;; API
 (define (check-reset!)
-  (set! check:correct 0)
-  (set! check:failed   '()))
+  (check-pass-count 0)
+  (check-total-count 0)
+  (check-failure #f))
 
-(define (check:add-correct!)
-  (set! check:correct (+ check:correct 1)))
-
-(define (check:add-failed! expression actual-result expected-result)
-  (set! check:failed
-        (cons (list expression actual-result expected-result)
-              check:failed)))
-
-(check-reset!)
-
-; -- reporting --
-
-(define (check:report-expression expression)
-  (newline)
-  (check:write expression)
-  (display " => "))
-
-(define (check:report-actual-result actual-result)
-  (check:write actual-result)
-  (display " ; "))
-
-(define (check:report-correct cases)
-  (display "correct")
-  (if (not (= cases 1))
-      (begin (display " (")
-             (display cases)
-             (display " cases checked)")))
-  (newline))
-
-(define (check:report-failed expected-result)
-  (display "*** failed ***")
-  (newline)
-  (display " ; expected result: ")
-  (check:write expected-result)
-  (newline))
-
-(define (check-report)
-  (if (>= check:mode 1)
-      (begin
-        (newline)
-        (display "; *** checks *** : ")
-        (display check:correct)
-        (display " correct, ")
-        (display (length check:failed))
-        (display " failed.")
-        (if (or (null? check:failed) (<= check:mode 1))
-            (newline)
-            (let* ((w (car (reverse check:failed)))
-                   (expression (car w))
-                   (actual-result (cadr w))
-                   (expected-result (caddr w)))                  
-              (display " First failed example:")
-              (newline)
-              (check:report-expression expression)
-              (check:report-actual-result actual-result)
-              (check:report-failed expected-result))))))
-
-(define (check-passed? expected-total-count)
-  (and (= (length check:failed) 0)
-       (= check:correct expected-total-count)))
-       
-; -- simple checks --
-
-(define (check:proc expression thunk equal expected-result)
-  (case check:mode
-    ((0) #f)
-    ((1)
-     (let ((actual-result (thunk)))
-       (if (equal actual-result expected-result)
-           (check:add-correct!)
-           (check:add-failed! expression actual-result expected-result))))
-    ((10)
-     (let ((actual-result (thunk)))
-       (if (equal actual-result expected-result)
-           (check:add-correct!)
-           (begin
-             (check:report-expression expression)
-             (check:report-actual-result actual-result)
-             (check:report-failed expected-result)
-             (check:add-failed! expression actual-result expected-result)))))
-    ((100)
-     (check:report-expression expression)
-     (let ((actual-result (thunk)))
-       (check:report-actual-result actual-result)
-       (if (equal actual-result expected-result)
-           (begin (check:report-correct 1)
-                  (check:add-correct!))
-           (begin (check:report-failed expected-result)
-                  (check:add-failed! expression 
-				     actual-result 
-				     expected-result)))))
-    (else (error "unrecognized check:mode" check:mode)))
-  (if #f #f))
-
+;; API
 (define-syntax check
   (syntax-rules (=>)
-    ((check expr => expected)
-     (check expr (=> equal?) expected))
-    ((check expr (=> equal) expected)
-     (if (>= check:mode 1)
-	 (check:proc 'expr (lambda () expr) equal expected)))))
+    [(_ expr (=> eqproc) expected)
+     (unless (eq? (check-mode) 'off)
+       ;; Allow files that only use srfi-78 to work
+       (unless (test-running?) (test-start (current-load-path)))
+       (let ([eq eqproc]
+             [ex expected])
+         (test* 'expr ex expr eq %check-hook)))]
+    [(_ expr => expected)
+     (check expr (=> equal?) expected)]))
 
-; -- parametric checks --
+(define (%check-hook verdict name expected result)
+  (inc! (check-total-count))
+  (case verdict
+    [(pass) 
+     (inc! (check-pass-count))]
+    [(fail)
+     ;; we only record the first failure
+     (unless (check-failure)
+       (check-failure (list name expected result)))]))
 
-(define (check:proc-ec w)
-  (let ((correct? (car w))
-        (expression (cadr w))
-        (actual-result (caddr w))
-        (expected-result (cadddr w))
-	(cases (car (cddddr w))))
-    (if correct?
-        (begin (if (>= check:mode 100)
-                   (begin (check:report-expression expression)
-                          (check:report-actual-result actual-result)
-                          (check:report-correct cases)))
-               (check:add-correct!))
-        (begin (if (>= check:mode 10)
-                   (begin (check:report-expression expression)
-                          (check:report-actual-result actual-result)
-                          (check:report-failed expected-result)))
-               (check:add-failed! expression 
-				  actual-result 
-				  expected-result)))))
+;; API
+(define (check-passed? expected-num-passed)
+  (and (not (check-failure))
+       (= expected-num-passed (check-pass-count))))
 
-(define-syntax check-ec:make
-  (syntax-rules (=>)
-    ((check-ec:make qualifiers expr (=> equal) expected (arg ...))
-     (if (>= check:mode 1)
-         (check:proc-ec
-	  (let ((cases 0))
-	    (let ((w (first-ec 
-		      #f
-		      qualifiers
-		      (:let equal-pred equal)
-		      (:let expected-result expected)
-		      (:let actual-result
-                            (let ((arg arg) ...) ; (*)
-                              expr))
-		      (begin (set! cases (+ cases 1)))
-		      (if (not (equal-pred actual-result expected-result)))
-		      (list (list 'let (list (list 'arg arg) ...) 'expr)
-			    actual-result
-			    expected-result
-			    cases))))
-	      (if w
-		  (cons #f w)
-		  (list #t 
-			'(check-ec qualifiers 
-				   expr (=> equal) 
-				   expected (arg ...))
-			(if #f #f)
-		        (if #f #f)
-			cases)))))))))
+;; API
+(define (check-report)
+  (if (check-failure)
+    (format #t "Passed ~d tests out of ~d tests.  First failure: ~s\n"
+            (check-pass-count) (check-total-count) (check-failure))
+    (format #t "All ~d tests passed."
+            (check-pass-count))))
 
-; (*) is a compile-time check that (arg ...) is a list
-; of pairwise disjoint bound variables at this point.
 
+;; API
 (define-syntax check-ec
-  (syntax-rules (nested =>)
-    ((check-ec expr => expected)
-     (check-ec:make (nested) expr (=> equal?) expected ()))
-    ((check-ec expr (=> equal) expected)
-     (check-ec:make (nested) expr (=> equal) expected ()))
-    ((check-ec expr => expected (arg ...))
-     (check-ec:make (nested) expr (=> equal?) expected (arg ...)))
-    ((check-ec expr (=> equal) expected (arg ...))
-     (check-ec:make (nested) expr (=> equal) expected (arg ...)))
-
-    ((check-ec qualifiers expr => expected)
-     (check-ec:make qualifiers expr (=> equal?) expected ()))
-    ((check-ec qualifiers expr (=> equal) expected)
-     (check-ec:make qualifiers expr (=> equal) expected ()))
-    ((check-ec qualifiers expr => expected (arg ...))
-     (check-ec:make qualifiers expr (=> equal?) expected (arg ...)))
-    ((check-ec qualifiers expr (=> equal) expected (arg ...))
-     (check-ec:make qualifiers expr (=> equal) expected (arg ...)))
-
-    ((check-ec (nested q1 ...) q etc ...)
-     (check-ec (nested q1 ... q) etc ...))
-    ((check-ec q1 q2             etc ...)
-     (check-ec (nested q1 q2)    etc ...))))
+  (syntax-rules (=>)
+    [(_ q ... expr (=> eqproc) expected (args ...))
+     (let ([pass-count 0]
+           [total-count 0]
+           [first-failure #f])
+       (do-ec q ...
+              (let ([eq eqproc]
+                    [ex expected])
+                (test* 'expr expected expr exproc
+                       (^[verdict name expected result]
+                         (inc! total-count)
+                         (if (eq? verdict 'pass)
+                           (inc! total-count)
+                           (unless first-failure
+                             (set! first-failure
+                                   (list verdict name expeted result 
+                                         (list args ...)))))))))
+       (if (= total-count pass-count)
+         (inc! (check-pass-count))
+         (unless (check-failure)
+           (check-failure first-failure))))]
+    [(_ q ... expr => expected (args ...))
+     (check-ec q ... expr (=> equal?) expected (args ...))]
+    [(_ q ... expr (=> eqproc) expected)
+     (check-ec q ... expr (=> exproc) expected ())]
+    [(_ q ... expr => expected)
+     (check-ec q ... expr (=> equal?) expected ())]))
