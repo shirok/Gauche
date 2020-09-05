b78e80570f46f3e36b2e86b180d9e25acbc4d253
Shiro Kawai <shiro@acm.org>
2020-09-04 20:43:45 -1000

    Add srfi-192

---
+++ lib/srfi-192.scm
@@ -0,0 +1,44 @@
+;;;
+;;; From srfi-102 reference implementation by Shiro Kawai
+;;; MIT License
+;;;
+
+;; At this moment, it isn't possible to faithfully implment srfi-192
+;; on top of Gauche's port protocol.  We'll revise this as the port
+;; protocol evolves.
+
+(define-module srfi-192
+  (export make-i/o-invalid-position-error
+          port-has-port-position?
+          port-position
+          port-has-set-port-position!?
+          set-port-position!))
+(select-module srfi-192)
+
+(define (port-has-port-position? port)
+  (assume (port? port))
+  (boolean (port-tell port)))
+
+(define (port-position port)
+  (assume (port? port))
+  (port-tell port))
+
+(define (port-has-set-port-position!? port)
+  (assume (port? port))
+  ;; NB: We should have the way to specifically query the port is seekable.
+  (boolean (port-tell port)))
+
+(define (set-port-position! port pos)
+  (assume (port? port))
+  (port-seek port pos))
+
+;; It'd be natural for this condition to inherit <port-error>, but
+;; the public constructor doens't take the port.  So we implement it
+;; as a mixin condition.  The recommended use is to make a compound
+;; condition with a <port-error>.
+(define-condition-type <i/o-invalid-position-error-mixin> <condition>
+  i/o-invalid-position-error?
+  (position))
+
+(define (make-i/o-invalid-position-error position)
+  (make <i/o-invalid-position-error-mixin> :position position))
