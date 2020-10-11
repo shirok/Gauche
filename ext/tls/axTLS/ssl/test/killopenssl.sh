e7b2536e3d0992f5875100ee378c3b022e4221d1
Shiro Kawai <shiro@acm.org>
2020-10-10 16:00:24 -1000

    Make killopenssh.sh more robust

--- ext/tls/axTLS/ssl/test/killopenssl.sh
+++ ext/tls/axTLS/ssl/test/killopenssl.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-awk '{print $1}' "../ssl/openssl.pid" | xargs kill -9
-rm -f ../ssl/openssl.pid
+if [ -f "../ssl/openssl.pid" ]; then
+    awk '{print $1}' "../ssl/openssl.pid" | xargs kill -9
+    rm -f ../ssl/openssl.pid
+fi
