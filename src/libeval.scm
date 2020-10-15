921ea84fda2ec790a8c51d93ea78658b7f33700e
Shiro Kawai <shiro@acm.org>
2020-10-14 23:47:31 -1000

    Include thread subsystem and TLS subsystem info in version-alist

--- src/libeval.scm
+++ src/libeval.scm
@@ -572,14 +572,22 @@
 
 ;; srfi-176
 (define-in-module gauche (version-alist)
-  `((version ,(gauche-version))
-    (command "gosh")
-    (scheme.id gauche)
-    (languages scheme r5rs r7rs)
-    (encodings ,(gauche-character-encoding))
-    (website "https://practical-scheme.net/gauche")
-    (build.platform ,(gauche-architecture))
-    (scheme.path ,@*load-path*)))
+  (let1 fs ((with-module gauche.internal cond-features))
+    `((version ,(gauche-version))
+      (command "gosh")
+      (scheme.id gauche)
+      (languages scheme r5rs r7rs)
+      (encodings ,(gauche-character-encoding))
+      (website "https://practical-scheme.net/gauche")
+      (build.platform ,(gauche-architecture))
+      (scheme.path ,@*load-path*)
+      (gauche.threads ,(cond
+                        [(assq 'gauche.sys.pthreads fs) 'pthreads]
+                        [(assq 'gauche.sys.wthreads fs) 'wthreads]
+                        [else 'none]))
+      (gauche.net.tls ,@(cond-list
+                         [(assq 'gauche.net.tls.axtls fs) 'axtls]
+                         [(assq 'gauche.net.tls.mbedtls fs) 'mbedtls])))))
 
 ;;
 ;; External view of VM.
