af8336a92939298a13cf6afe5d4e5a8201581171
Shiro Kawai <shiro@acm.org>
2021-02-21 22:55:40 -1000

    More code migration from srfi-* to scheme.*

--- lib/scheme/generator.scm
+++ lib/scheme/generator.scm
@@ -1,3 +1,115 @@
-;; R7RS Large - Tangerine Edition
-;; (Originally srfi-121 in Red Edition)
-(define-module scheme.generator (extend srfi-158))
+;;;
+;;; scheme.generator - Generators and Accumulators (R7RS Tangerine)
+;;;
+;;;   Copyright (c) 2020  Shiro Kawai  <shiro@acm.org>
+;;;
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;
+;;;   3. Neither the name of the authors nor the names of its contributors
+;;;      may be used to endorse or promote products derived from this
+;;;      software without specific prior written permission.
+;;;
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+;; Originally srfi-158
+;; (In R7RS Red edition, it was srfi-121).
+;; Generator library is built-in.   We provide accumulator implementation here.
+
+(define-module scheme.generator
+  (use gauche.unicode)
+  (use gauche.generator)
+  (export generator circular-generator make-iota-generator make-range-generator
+          make-coroutine-generator list->generator vector->generator
+          reverse-vector->generator string->generator
+          bytevector->generator
+          make-for-each-generator make-unfold-generator
+          gcons* gappend gflatten ggroup gmerge gmap gcombine gfilter gremove
+          gstate-filter ggroup generator-map->list
+          gtake gdrop gtake-while gdrop-while
+          gdelete gdelete-neighbor-dups gindex gselect
+          generator->list generator->reverse-list generator-map->list
+          generator->vector generator->vector!  generator->string
+          generator-fold generator-for-each generator-find
+          generator-count generator-any generator-every generator-unfold
+
+          ;; accumulators
+          make-accumulator count-accumulator list-accumulator
+          reverse-list-accumulator vector-accumulator
+          reverse-vector-accumulator vector-accumulator!
+          string-accumulator bytevector-accumulator bytevector-accumulator!
+          sum-accumulator product-accumulator))
+(select-module scheme.generator)
+
+(define (make-accumulator kons knil finalizer)
+  (^v
+   (if (eof-object? v)
+     (finalizer knil)
+     (begin (set! knil (kons v knil)) (undefined)))))
+
+(define (list-accumulator)
+  (make-accumulator cons '() reverse))
+
+(define (reverse-list-accumulator)
+  (make-accumulator cons '() identity))
+
+(define (vector-accumulator)
+  (make-accumulator cons '() reverse-list->vector))
+
+(define (reverse-vector-accumulator)
+  (make-accumulator cons '() list->vector))
+
+(define (vector-accumulator! vec at)
+  (make-accumulator (^[v i]
+                      (when (>= i (vector-length vec))
+                        (error "vector is full"))
+                      (vector-set! vec i v)
+                      (+ i 1))
+                    at
+                    (^_ vec)))
+
+(define (string-accumulator)
+  (make-accumulator (^[c p] (write-char c p) p)
+                    (open-output-string)
+                    get-output-string))
+
+(define (bytevector-accumulator)
+  (make-accumulator (^[b p] (write-byte b p) p)
+                    (open-output-string)
+                    (^p (string->utf8 (get-output-string p)))))
+
+(define (bytevector-accumulator! vec at)
+  (assume-type vec <u8vector>)
+  (make-accumulator (^[v i]
+                      (when (>= i (uvector-length vec))
+                        (error "bytevector is full"))
+                      (u8vector-set! vec i v)
+                      (+ i 1))
+                    at
+                    (^_ vec)))
+
+(define (sum-accumulator)
+  (make-accumulator + 0 identity))
+(define (product-accumulator)
+  (make-accumulator * 1 identity))
+(define (count-accumulator)
+  (make-accumulator (^[_ c] (+ c 1)) 0 identity))
--- lib/scheme/list-queue.scm
+++ lib/scheme/list-queue.scm
@@ -1,2 +1,142 @@
-;; R7RS Large - Red edition
-(define-module scheme.list-queue (extend srfi-117))
+;;;
+;;; scheme.list-queue - Queues based on lists (R7RS Red)
+;;;
+;;;   Copyright (c) 2015-2020  Shiro Kawai  <shiro@acm.org>
+;;;
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;
+;;;   3. Neither the name of the authors nor the names of its contributors
+;;;      may be used to endorse or promote products derived from this
+;;;      software without specific prior written permission.
+;;;
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+;; Originally srfi-117
+
+;; Implemented on top of data.queue in Gauche
+
+(define-module scheme.list-queue
+  (use data.queue)
+  (export make-list-queue list-queue list-queue-copy
+          list-queue-unfold list-queue-unfold-right
+          list-queue? list-queue-empty?
+          list-queue-front list-queue-back
+          list-queue-list list-queue-first-last
+          list-queue-add-front! list-queue-add-back!
+          list-queue-remove-front! list-queue-remove-back!
+          list-queue-remove-all!
+          list-queue-set-list!
+          list-queue-append list-queue-append!
+          list-queue-concatenate
+          list-queue-map list-queue-map! list-queue-for-each))
+(select-module scheme.list-queue)
+
+(define (make-list-queue lis :optional last)
+  (if (pair? last)
+    (rlet1 q (make-queue) ; needs to satisfy O(1) restriction
+      ((with-module data.queue %queue-set-content!) q lis last))
+    (list->queue lis)))
+
+(define (list-queue . elts) (list->queue elts))
+
+(define (list-queue-copy q) (copy-queue q))
+
+(define (list-queue-unfold stop? mapper successor seed
+                           :optional (q (make-queue)))
+  (let loop ((seed seed) (elts '()))
+    (if (stop? seed)
+      (dolist [e elts] (queue-push! q e))
+      (loop (successor seed) (cons (mapper seed) elts))))
+  q)
+
+(define (list-queue-unfold-right stop? mapper successor seed
+                                 :optional (q (make-queue)))
+  (let loop ((seed seed) (elts '()))
+    (if (stop? seed)
+      (dolist [e elts] (enqueue! q e))
+      (loop (successor seed) (cons (mapper seed) elts))))
+  q)
+
+(define (list-queue? q) (queue? q))
+(define (list-queue-empty? q) (queue-empty? q))
+(define (list-queue-front q) (queue-front q))
+(define (list-queue-back q) (queue-rear q))
+(define (list-queue-list q) (queue-internal-list q))
+
+;; This also returns internal structure.
+(define (list-queue-first-last q)
+  (when (mtqueue? q)
+    (error "Can't get internal pairs of <mtqueue>:" q))
+  (values ((with-module data.queue %qhead) q)
+          ((with-module data.queue %qtail) q)))
+
+(define (list-queue-add-front! q elt) (queue-push! q elt))
+(define (list-queue-add-back! q elt ) (enqueue! q elt))
+(define (list-queue-remove-front! q)  (dequeue! q))
+(define (list-queue-remove-back! q)
+  ;; it's ok to be O(n), so we take gratuitously inefficient path
+  (let loop ([n (queue-length q)]
+             [elts (dequeue-all! q)])
+    (if (= n 1)
+      (car elts)
+      (begin (enqueue! q (car elts))
+             (loop (- n 1) (cdr elts))))))
+
+(define (list-queue-remove-all! q) (dequeue-all! q))
+
+(define (list-queue-set-list! q lis :optional last)
+  (let1 last (last-pair lis)
+    ((with-module data.queue %queue-set-content!) q lis last)
+    q))
+
+(define (list-queue-append . qs)
+  (list-queue-concatenate qs))
+
+(define (list-queue-append! . qs)
+  (let loop ([qs qs]
+             [head '()]
+             [tail '()])
+    (if (null? qs)
+      (make-list-queue head tail)
+      (receive (hd tl) (list-queue-first-last (car qs))
+        ;; we purge the original queues, so that the mutation of internal
+        ;; cells won't confuse them.
+        (dequeue-all! (car qs))
+        (cond [(null? hd) (loop (cdr qs) head tail)]
+              [(pair? tail)
+               (begin (set-cdr! tail hd) (loop (cdr qs) head tl))]
+              [else (loop (cdr qs) hd tl)])))))
+
+(define (list-queue-concatenate qs)
+  (list->queue (concatenate (map list-queue-list qs))))
+
+(define (list-queue-map proc q)
+  (list->queue (map proc (queue->list q))))
+
+(define (list-queue-map! proc q)
+  (let1 xs (map proc (queue->list q))
+    ((with-module data.queue %queue-set-content!) q xs #f)
+    q))
+
+(define (list-queue-for-each proc q)
+  (for-each proc (queue->list q)))
