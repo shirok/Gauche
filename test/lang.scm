253404b67596a1d6708ac79e236ed92d6f7e11c2
Shiro Kawai <shiro@acm.org>
2021-02-23 14:55:25 -1000

    Add lang.* tests skeleton

---
+++ test/lang.scm
@@ -0,0 +1,17 @@
+;;;
+;;; tests for lang.*
+;;;
+;;;   For now, these are experimental modules so we only test basic
+;;;   stuff.  Eventually we need comprehensive tests for each lang.* modules.
+;;;
+
+(use gauche.test)
+(test-start "lang.*")
+
+(use lang.asm.x86_64)
+(test-module 'lang.asm.x86_64)
+
+(use lang.c.parser)
+(test-module 'lang.c.parser)
+
+(test-end)
