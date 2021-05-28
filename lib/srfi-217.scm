a368414964b8b5826e853dffcca5f86b741697ac
Shiro Kawai <shiro@acm.org>
2021-05-27 16:18:44 -1000

    Start adding srfi-217

---
+++ lib/srfi-217.scm
@@ -0,0 +1,124 @@
+;;;
+;;; srfi-217 - Integer sets
+;;;
+;;;   Copyright (c) 2021  Shiro Kawai  <shiro@acm.org>
+;;;
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;
+;;;   3. Neither the name of the authors nor the names of its contributors
+;;;      may be used to endorse or promote products derived from this
+;;;      software without specific prior written permission.
+;;;
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+(define-module srfi-217
+  (use gauche.record)
+  (use gauche.sequence)
+  (use util.match)
+  (export iset iset-unfold make-range-iset
+          iset? iset-contains? iset-empty? iset-disjoint?
+          iset-member iset-min iset-max
+          iset-adjoin iset-adjoin! iset-delete iset-delete!
+          iset-delete-all iset-delete-all! iset-search iset-search!
+          iset-delete-min iset-delete-min! iset-delete-max iset-delete-max!
+          iset-size iset-find iset-count iset-any? iset-every?
+          iset-map iset-for-each iset-fold iset-fold-right
+          iset-filter iset-filter! iset-remove iset-remove!
+          iset-partition iset-partition!
+          iset-copy iset->list list->iset list->iset!
+          iset=? iset<? iset>? iset<=? iset>=?
+          iset-union iset-intersection iset-difference iset-xor
+          iset-union! iset-intersection! iset-difference! iset-xor!
+          iset-open-interval iset-closed-interval
+          iset-open-closed-interval iset-closed-open-interval
+          isbuset= isubset< isubset<= isbuset> isbuset>=)
+  )
+(select-module srfi-217)
+
+(define-record-type <iset> %make-iset iset?
+  (tmap))
+
+(define-method write-object ((iset <iset>) port)
+  (format port "#<iset ~a entrie(s)>"
+          (tree-map-num-entries (~ iset'tmap))))
+
+;; Constructors
+(define (iset . args) (list->iset args))
+
+(define (list->iset args)
+  (let1 tmap (make-tree-map < =)
+    (let loop ([p (group-contiguous-sequence (sort args <) :squeeze #t)])
+      (match p
+        [() (%make-iset tmap)]
+        [((s e) . rest) (tree-map-put! tmap s e) (loop rest)]
+        [((v) . rest) (tree-map-put! tmap v v) (loop rest)]))))
+
+(define (iset-unfold p f g seed)
+  (list->iset (unfold p f g seed)))
+
+;; Predicates
+
+;; iset? - above
+
+(define (iset-contains? iset k)
+  (assume-type iset <iset>)
+  (receive (s e) (tree-map-floor (iset-tmap iset) k)
+    (<= k e)))
+
+(define (iset-empty? iset) (tree-map-empty? (iset-tmap iset)))
+
+(define (iset-disjoint? iset1 iset2)
+  (assume-type iset1 <iset>)
+  (assume-type iset2 <iset>)
+  (let ([g1 (x->generator iset1)]
+        [g2 (x->generator iset2)])
+    (let loop ([p1 (g1)] [p2 (g2)])
+      (cond [(eof-object? p1)]
+            [(eof-object? p2)]
+            [(<= (car p1) (car p2))
+             (if (< (cdr p1) (car p2))
+               (loop (g1) p2)
+               #f)]
+            [(< (car p2) (car p1))
+             (if (< (cdr p2) (car p1))
+               (loop p1 (g2))
+               #f)]))))
+
+;; Accessors
+
+(define (iset-member iset k fallback)
+  (if (iset-contains? iset k)
+    k
+    fallback))
+
+(define (iset-min iset)
+  (assume-type iset <iset>)
+  (match (tree-map-min (iset-tmap iset))
+    [#f #f]
+    [(s . _) s]))
+
+(define (iset-max iset)
+  (assume-type iset <iset>)
+  (match (tree-map-max (iset-tmap iset))
+    [#f #f]
+    [(_ . e) e]))
