d0a9d81021dbf2fd7f3dddc361813f2109583b10
Shiro Kawai <shiro@acm.org>
2021-06-26 01:51:41 -1000

    Add <counted-box> experimentally

--- src/libbox.scm
+++ src/libbox.scm
@@ -33,6 +33,9 @@
 
 (select-module gauche)
 
+;; box is built-in since it is used in equal? implementation.  See
+;; %interleave-equal? in libbool.scm.
+
 ;;;
 ;;;  srfi-111 & srfi-195 box
 ;;;
@@ -99,3 +102,88 @@
       (Scm_Error "index out of range for %S: %d" b i))
     (set! (aref (SCM_MVBOX_VALUES b) i) val)]
    [else (SCM_TYPE_ERROR b "<box> or <mv-box>")]))
+
+;;; EXPERIMENTAL
+
+;; Counted box is an immutable box with MT-safe counter.
+;; The counter can keep track of, for example, the number of
+;; objects that shares the particular information kept in the box.
+;; Notably, if the counter is one, you can be sure that no other
+;; objects refers to the box so that you can modify the object(s)
+;; kept in it without being worried that you affect the mutation
+;; to others.
+;;
+;; Although the counter operation is MT-safe, it is not an alternative
+;; of mutex.  If the object can be mutated from multiple threads, you have
+;; to protect it with a separate mutex.
+;;
+;; The counter is neither a reference counter in a strict sense; the
+;; references to a counted box can be kept in an environment, for example.
+;;
+;; (This is not essential in core, so some other place like ext/data may
+;; be suitable, but access to the atomicP.h requires a bit of fiddling on
+;; compiler flags.)
+;;
+;; See https://srfi-email.schemers.org/srfi-discuss/msg/16912128/
+;; for the discussion triggered this implementation.
+
+(use gauche.internal)
+
+(inline-stub
+ (.include "gauche/priv/atomicP.h")
+
+ (define-ctype ScmCountedBox
+   ::(.struct ScmCountexBoxRec
+              (hdr::ScmHeader
+               counter::ScmAtomicVar
+               numValues::ScmSmallInt
+               values::(.array ScmObj (1)))))
+
+ (define-cclass <counted-box> :base :private :no-meta
+   "ScmCountedBox*" "Scm_CountedBoxClass"
+   (c "SCM_CLASS_DEFAULT_CPL")
+   ())
+ )
+
+;; API
+(define-cproc make-counted-box (initial-count::<fixnum> :rest values)
+  (let* ([numVals::ScmSmallInt (Scm_Length values)]
+         [z::ScmCountedBox* (SCM_NEW2 (.type ScmCountedBox*)
+                                      (+ (sizeof (.type ScmCountedBox*))
+                                         (* (sizeof (.type ScmObj))
+                                            (- numVals 1))))]
+         [i::int 0])
+    (SCM_SET_CLASS z (& Scm_CountedBoxClass))
+    (AO_store (& (-> z counter)) (cast ScmAtomicWord initial-count))
+    (set! (-> z numValues) numVals)
+    (dolist [v values]
+      (set! (aref (-> z values) (post++ i)) v))
+    (return (SCM_OBJ z))))
+
+;; API
+(define-cproc counted-box-unbox (cb::<counted-box>)
+  (return (Scm_ValuesFromArray (-> cb values) (-> cb numValues))))
+
+;; API
+(define-cproc counted-box-ref (cb::<counted-box> k::<fixnum>)
+  (when (or (< k 0) (>= k (-> cb numValues)))
+    (Scm_Error "Index out of range: %d" k))
+  (return (aref (-> cb values) k)))
+
+;; API
+(define-cproc counted-box-arity (cb::<counted-box>) ::<fixnum>
+  (return (-> cb numValues)))
+
+;; API
+(define-cproc counted-box-count (cb::<counted-box>)
+  (let* ([v::ScmAtomicWord (AO_load (& (-> cb counter)))])
+    ;; NB: We might need an api to convert ScmAtomicWord to Scheme integer.
+    (return (Scm_MakeInteger (cast long v)))))
+
+;; API
+(define-cproc counted-box-inc! (cb::<counted-box> delta::<fixnum>)
+  (for ()
+    (let* ([v::ScmAtomicVar (AO_load (& (-> cb counter)))]
+           [vv::ScmAtomicWord (+ v delta)])
+      (when (AO_compare_and_swap_full (& (-> cb counter)) v vv)
+        (return (Scm_MakeInteger (cast long v)))))))
