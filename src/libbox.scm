49c33820b266346aeabaed7ace6e173f9050fb9e
Shiro Kawai <shiro@acm.org>
2021-06-30 05:38:07 -1000

    Rename counted-box to shared-box and integrate it to other boxes

--- src/libbox.scm
+++ src/libbox.scm
@@ -36,8 +36,55 @@
 ;; box is built-in since it is used in equal? implementation.  See
 ;; %interleave-equal? in libbool.scm.
 
+;; Internally, we have <box> (single-value box) and <mv-box> (multi-value
+;; box) as separate types, to make single-value box efficient.
+
+;; We also EXPERIMENTALLY have <shared-box>, which has an atomic
+;; shared counter to keep track of the number of objects that shares the
+;; box.  Notably, if the counter is one, you can be sure that no other
+;; objects refers to the box so that you can modify the object(s)
+;; kept in it without being worried that you affect the mutation
+;; to others.
+;;
+;; Although the counter operation is MT-safe, it is not an alternative
+;; of mutex.  If the object can be mutated from multiple threads, you have
+;; to protect it with a separate mutex.
+;;
+;; The counter is neither a reference counter in a strict sense; the
+;; references to a counted box can be kept in an environment, for example.
+;;
+;; See https://srfi-email.schemers.org/srfi-discuss/msg/16912128/
+;; for the discussion triggered this implementation.
+
+(inline-stub
+ (.include "gauche/priv/atomicP.h")
+
+ (define-ctype ScmSharedBox
+   ::(.struct ScmSharedBoxRec
+              (hdr::ScmHeader
+               counter::ScmAtomicVar
+               numValues::ScmSmallInt
+               values::(.array ScmObj (1)))))
+
+ (define-cclass <shared-box> :base :private :no-meta
+   "ScmSharedBox*" "Scm_SharedBoxClass"
+   (c "SCM_CLASS_DEFAULT_CPL")
+   ()
+   (printer (let* ([b::ScmSharedBox* (SCM_SHARED_BOX obj)]
+                   [cnt::ScmAtomicWord (AO_load (& (-> b counter)))])
+              (Scm_Printf port "#<shared-box[%d]<%d>"
+                          (-> b numValues) cnt)
+              (dotimes [i (-> b numValues)]
+                (Scm_Printf port " %S" (aref (-> b values) i)))
+              (Scm_Printf port ">"))))
+
+ (.define SCM_SHARED_BOX_SIZE (x) (-> (SCM_SHARED_BOX x) numValues))
+ (.define SCM_SHARED_BOX_VALUES (x) (-> (SCM_SHARED_BOX x) values))
+ )
+
+
 ;;;
-;;;  srfi-111 & srfi-195 box
+;;; API
 ;;;
 
 ;; We use them in equal? implementation as well, so here we go...
@@ -47,13 +94,17 @@
    [(SCM_NULLP rest) (return (SCM_OBJ (Scm_MakeBox (aref argv 0))))]
    [else (return (SCM_OBJ (Scm_ListToMVBox (Scm_Cons (aref argv 0) rest))))]))
 
-(define-cproc box? (v) ::<boolean> (return (or (SCM_BOXP v) (SCM_MVBOXP v))))
+(define-cproc box? (v) ::<boolean>
+  (return (or (SCM_BOXP v) (SCM_MVBOXP v) (SCM_SHARED_BOX_P v))))
 
 (define-cproc unbox (b)
   (cond
    [(SCM_BOXP b) (return (SCM_BOX_VALUE b))]
    [(SCM_MVBOXP b) (return (Scm_ValuesFromArray (SCM_MVBOX_VALUES b)
                                                 (SCM_MVBOX_SIZE b)))]
+   [(SCM_SHARED_BOX_P b) (return (Scm_ValuesFromArray
+                                  (SCM_SHARED_BOX_VALUES b)
+                                  (SCM_SHARED_BOX_SIZE b)))]
    [else (SCM_TYPE_ERROR b "<box> or <mv-box>") (return SCM_UNDEFINED)]))
 
 (define-cproc set-box! (b :rest vs) ::<void>
@@ -71,13 +122,22 @@
                    b vs))
       (for (() (< i argc) (begin (post++ i) (set! vs (SCM_CDR vs))))
         (set! (aref (SCM_MVBOX_VALUES b) i) (SCM_CAR vs))))]
-   [else (SCM_TYPE_ERROR b "<box> or <mv-box>")]))
+   [(SCM_SHARED_BOX_P b)
+    (let* ([argc::ScmSmallInt (Scm_Length vs)]
+           [i::ScmSmallInt 0])
+      (unless (== argc (SCM_SHARED_BOX_SIZE b))
+        (Scm_Error "Wrong number of values to set to a shared box %S: %S"
+                   b vs))
+      (for (() (< i argc) (begin (post++ i) (set! vs (SCM_CDR vs))))
+        (set! (aref (SCM_SHARED_BOX_VALUES b) i) (SCM_CAR vs))))]
+   [else (SCM_TYPE_ERROR b "<box>, <mv-box>, or <shared-box>")]))
 
 (define-cproc box-arity (b) ::<int>
   (cond
    [(SCM_BOXP b) (return 1)]
    [(SCM_MVBOXP b) (return (SCM_MVBOX_SIZE b))]
-   [else (SCM_TYPE_ERROR b "<box> or <mv-box>")]))
+   [(SCM_SHARED_BOX_P b) (return (SCM_SHARED_BOX_SIZE b))]
+   [else (SCM_TYPE_ERROR b "<box>, <mv-box>, or <shared-box>")]))
 
 (define-cproc unbox-value (b i::<fixnum>)
   (cond
@@ -89,7 +149,12 @@
                  (< i (SCM_MVBOX_SIZE b)))
       (Scm_Error "index out of range for %S: %d" b i))
     (return (aref (SCM_MVBOX_VALUES b) i))]
-   [else (SCM_TYPE_ERROR b "<box> or <mv-box>")]))
+   [(SCM_SHARED_BOX_P b)
+    (unless (and (<= 0 i)
+                 (< i (SCM_SHARED_BOX_SIZE b)))
+      (Scm_Error "index out of range for %S: %d" b i))
+    (return (aref (SCM_SHARED_BOX_VALUES b) i))]
+   [else (SCM_TYPE_ERROR b "<box>, <mv-box>, or <shard-box>")]))
 
 (define-cproc set-box-value! (b i::<fixnum> val) ::<void>
   (cond
@@ -101,71 +166,32 @@
                  (< i (SCM_MVBOX_SIZE b)))
       (Scm_Error "index out of range for %S: %d" b i))
     (set! (aref (SCM_MVBOX_VALUES b) i) val)]
-   [else (SCM_TYPE_ERROR b "<box> or <mv-box>")]))
+   [(SCM_SHARED_BOX_P b)
+    (unless (and (<= 0 i)
+                 (< i (SCM_SHARED_BOX_SIZE b)))
+      (Scm_Error "index out of range for %S: %d" b i))
+    (set! (aref (SCM_SHARED_BOX_VALUES b) i) val)]
+   [else (SCM_TYPE_ERROR b "<box>, <mv-box>, or <shared-box>")]))
 
-;;; EXPERIMENTAL
+;;;
+;;; <shared-box> specific API
+;;;
+;;  for now, we hide these API in a separate module.
 
-;; Counted box is an immutable box with MT-safe counter.
-;; The counter can keep track of, for example, the number of
-;; objects that shares the particular information kept in the box.
-;; Notably, if the counter is one, you can be sure that no other
-;; objects refers to the box so that you can modify the object(s)
-;; kept in it without being worried that you affect the mutation
-;; to others.
-;;
-;; Although the counter operation is MT-safe, it is not an alternative
-;; of mutex.  If the object can be mutated from multiple threads, you have
-;; to protect it with a separate mutex.
-;;
-;; The counter is neither a reference counter in a strict sense; the
-;; references to a counted box can be kept in an environment, for example.
-;;
-;; (This is not essential in core, so some other place like ext/data may
-;; be suitable, but access to the atomicP.h requires a bit of fiddling on
-;; compiler flags.)
-;;
-;; See https://srfi-email.schemers.org/srfi-discuss/msg/16912128/
-;; for the discussion triggered this implementation.
+(define-module gauche.experimental.shared-box)
+(select-module gauche.experimental.shared-box)
 
-(define-module gauche.experimental.counted-box)
-(select-module gauche.experimental.counted-box)
-
-(export <counted-box> counted-box
-        counted-box-unbox counted-box-ref
-        counted-box-arity counted-box-count counted-box-inc!)
-
-(inline-stub
- (.include "gauche/priv/atomicP.h")
-
- (define-ctype ScmCountedBox
-   ::(.struct ScmCountexBoxRec
-              (hdr::ScmHeader
-               counter::ScmAtomicVar
-               numValues::ScmSmallInt
-               values::(.array ScmObj (1)))))
-
- (define-cclass <counted-box> :base :private :no-meta
-   "ScmCountedBox*" "Scm_CountedBoxClass"
-   (c "SCM_CLASS_DEFAULT_CPL")
-   ()
-   (printer (let* ([b::ScmCountedBox* (SCM_COUNTED_BOX obj)]
-                   [cnt::ScmAtomicWord (AO_load (& (-> b counter)))])
-              (Scm_Printf port "#<counted-box[%d]<%d>"
-                          (-> b numValues) cnt)
-              (dotimes [i (-> b numValues)]
-                (Scm_Printf port " %S" (aref (-> b values) i)))
-              (Scm_Printf port ">"))))
- )
+(export shared-box shared-box-count shared-box-inc!)
 
 ;; API
-(define-cproc counted-box (initial-count::<fixnum> :rest values)
+(define-cproc shared-box (initial-count::<fixnum> :rest values)
   (let* ([numVals::ScmSmallInt (Scm_Length values)]
-         [z::ScmCountedBox* (SCM_NEW2 (.type ScmCountedBox*)
-                                      (+ (sizeof (.type ScmCountedBox))
-                                         (* (sizeof (.type ScmObj))
-                                            (- numVals 1))))]
+         [z::ScmSharedBox* (SCM_NEW2 (.type ScmSharedBox*)
+                                     (+ (sizeof (.type ScmSharedBox))
+                                        (* (sizeof (.type ScmObj))
+                                           (- numVals 1))))]
          [i::int 0])
-    (SCM_SET_CLASS z (& Scm_CountedBoxClass))
+    (SCM_SET_CLASS z (& Scm_SharedBoxClass))
     (AO_store (& (-> z counter)) (cast ScmAtomicWord initial-count))
     (set! (-> z numValues) numVals)
     (dolist [v values]
@@ -173,28 +199,14 @@
     (return (SCM_OBJ z))))
 
 ;; API
-(define-cproc counted-box-unbox (cb::<counted-box>)
-  (return (Scm_ValuesFromArray (-> cb values) (-> cb numValues))))
-
-;; API
-(define-cproc counted-box-ref (cb::<counted-box> k::<fixnum>)
-  (when (or (< k 0) (>= k (-> cb numValues)))
-    (Scm_Error "Index out of range: %d" k))
-  (return (aref (-> cb values) k)))
-
-;; API
-(define-cproc counted-box-arity (cb::<counted-box>) ::<fixnum>
-  (return (-> cb numValues)))
-
-;; API
-(define-cproc counted-box-count (cb::<counted-box>)
+(define-cproc shared-box-count (cb::<shared-box>)
   (let* ([v::ScmAtomicWord (AO_load (& (-> cb counter)))])
     ;; NB: We might need an api to convert ScmAtomicWord to Scheme integer.
     (return (Scm_MakeInteger (cast long v)))))
 
 ;; API
-(define-cproc counted-box-inc! (cb::<counted-box>
-                                :optional (delta::<fixnum> 1))
+(define-cproc shared-box-inc! (cb::<shared-box>
+                               :optional (delta::<fixnum> 1))
   (for ()
     (let* ([v::ScmAtomicVar (AO_load (& (-> cb counter)))]
            [vv::ScmAtomicWord (+ v delta)])
