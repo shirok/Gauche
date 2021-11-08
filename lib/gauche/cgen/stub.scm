878c10a2f3f423b3bc7f5f499e394e3774190f84
Shiro Kawai <shiro@acm.org>
2021-11-07 19:13:59 -1000

    Incorporated define-cstruct
    It has been being tested in Gauche-gl.  It allows mapping C structure
    to Scheme world more easily than define-cclass.  Should be considered
    as experimental for now.

--- lib/gauche/cgen/stub.scm
+++ lib/gauche/cgen/stub.scm
@@ -194,6 +194,8 @@
 ;;      (slot-spec ...)
 ;;      property-clause ...
 ;;
+;;   define-cstruct scheme-name c-struct-name (slot-spec ...)
+;;
 ;;   define-cptr scheme-class-name [qualifier] c-typename c-class-var
 ;;               c-pred c-boxer c-unboxer
 ;;               [(flags flag ...)]
@@ -1632,7 +1634,7 @@
 ;;                  [:c-name <c-name>]
 ;;                  [:c-spec <c-spec>]
 ;;                  [:getter <proc-spec>]
-;;                  [:setter <proc-spec>])
+;;                  [:setter <proc-spec>]
 ;;
 ;; <proc-spec> := <c-code> | (c <c-name>) | #f | #t
 ;;
@@ -1692,6 +1694,9 @@
    (type        :init-keyword :type   :init-value '<top>)
    (getter      :init-keyword :getter :init-value #t)
    (setter      :init-keyword :setter :init-value #t)
+   ;; init-cexpr is not currently used by <cclass>; it will eventually
+   ;; be used to auto-generate allocator.
+   (init-cexpr  :init-keyword :init-cexpr :init-value #f)
    ))
 
 (define-form-parser define-cclass (scm-name . args)
@@ -1857,11 +1862,12 @@
                 [c-name (get-keyword :c-name (cdr spec) (get-c-name "" name))]
                 [c-spec (get-keyword :c-spec (cdr spec) #f)]
                 [getter (get-keyword :getter (cdr spec) #t)]
-                [setter (get-keyword :setter (cdr spec) #t)])
+                [setter (get-keyword :setter (cdr spec) #t)]
+                [init   (get-keyword :init   (cdr spec) #f)])
            (make <cslot>
              :cclass cclass :scheme-name name :c-name c-name
              :c-spec c-spec :type (name->type type)
-             :getter getter :setter setter)))
+             :getter getter :setter setter :init-cexpr init)))
        slot-spec))
 
 (define-method slot-getter-name ((slot <cslot>))
@@ -1917,6 +1923,309 @@
 
 (define-method emit-spec-definition ((slot <cslot>))
   (p "  SCM_CLASS_SLOT_SPEC(\""(~ slot'scheme-name)"\", "(slot-getter-name slot)", "(slot-setter-name slot)"),"))
+
+;;===================================================================
+;; CStruct
+;;
+
+;; (define-cstruct scheme-name c-struct-name
+;;   (<slot-spec> ...)
+;;   )
+;;
+;;   This is to generate a stub to access plain old C struct, wrapped
+;;   in a Scheme Object.  The C struct is embedded in the Scheme object,
+;;   and its memory and lifetime are goverened by Gauche GC.
+;;   This isn't a form for a C struct that is allocated and owned by
+;;   the external library.
+;;
+;;   - The C struct is defined elsewhere (e.g. defined by the API).
+;;     We call it c_struct_t here.
+;;
+;;   - The stub creates a Scheme object that embeds the structure:
+;;
+;;       struct {
+;;          SCM_HEADER;
+;;          c_struct_t data;
+;;       };
+;;
+;;   - The Scheme class specified by scheme-name is defined to represent
+;;     the above Scheme structure.
+;;
+;;   - When it is passed from Scheme world to C world, the pointer to
+;;     the data field is passed.
+;;
+;;   - When C world returns it to the Scheme world, the content of the
+;;     C struct is copied into a Scheme-owned structure.
+;;
+;;   <slot-spec> is the following format:
+;;
+;;       <slot-spec> := slot-name::<slot-type> ["<c-spec>"]
+;;
+;;   slot-name is what you see in Scheme world.
+;;
+;;   <slot-type> specifies the type of slot, and how to box/unbox the
+;;   value from C struct.
+;;
+;;       <slot-type> := <stub-type>
+;;                   |  (.array* <stub-type>)
+;;                   |  (.array <stub-type>)
+;;                   |  &<stub-type>
+;;
+;;   The '.array*' type denotes a pointer to an array of <stub-type>.
+;;   The size of the array must e specified by another field of the struct.
+;;   see <c-spec> description below.
+;;
+;;   <c-spec> is a string encodes C-related info.
+;;
+;;       <c-spec> := <c-name>? <c-length>? <c-init>?
+;;       <c-name> ; C field name.  If omitted, slot-name is used.
+;;       <c-length> := '[' <c-name> ']'
+;;                  ; This is used for '.array*' slot, specifies the
+;;                  ; C field that for the length of the array.
+;;       <c-init> := '=' C-literal
+;;                  ; Specifies the initial value.
+;;
+;;   For the '.array*' type, the pointed array is allocated by Gauche
+;;   and subject to GC.
+
+(define-form-parser define-cstruct (scm-name c-struct-name slots . opts)
+  (assume-type scm-name <symbol>)
+  (assume-type c-struct-name <string>)
+  (assume-type slots <list>)
+  (let* ([TYPENAME ($ cgen-safe-name-friendly
+                      $ (cut regexp-replace-all* <>  #/[<>]/ "")
+                      $ symbol->string scm-name)]
+         [ClassName #"Scm_~|TYPENAME|_Class"]
+         [RecName #"Scm_~|TYPENAME|_Rec"]
+         [BoxerName #"Scm_Make_~|TYPENAME|"]
+         [type (make-cgen-type scm-name #"~|c-struct-name|*" #f
+                               #f #f BoxerName)]
+         [initializer (assq 'initializer opts)]
+         [cclass (make <cclass>
+                   :scheme-name scm-name
+                   :c-type #"~|c-struct-name|*"
+                   :c-name ClassName
+                   :qualifiers '()
+                   :cpa '()
+                   :direct-supers '()
+                   :allocator #f
+                   :printer #f
+                   :comparer #f)]
+         [slot-specs (cstruct-grok-slot-specs cclass slots)])
+    (set! (~ cclass'slot-spec)
+          (append-map (cut process-cstruct-slot cclass RecName <>) slot-specs))
+    (cgen-decl "#include <gauche/class.h>")
+    (cgen-decl #"typedef struct {"
+               #"  SCM_HEADER;"
+               #"  ~c-struct-name data;"
+               #"} ~|RecName|;")
+    (cgen-decl #"SCM_CLASS_DECL(~ClassName);")
+    (cgen-decl #"#define ~(cgen-pred-name type)(obj) \
+                         SCM_ISA(obj,&~|ClassName|)")
+    (cgen-decl #"#define ~(cgen-unboxer-name type)(obj) \
+                         &(((~RecName *)(obj))->data)")
+    (cgen-decl #"SCM_EXTERN ScmObj ~(cgen-boxer-name type)(const ~|c-struct-name|*);")
+    (cgen-body #"ScmObj ~(cgen-boxer-name type)(const ~|c-struct-name| *v)"
+               #"{"
+               #"  ~RecName *z = SCM_NEW(~RecName);"
+               #"  SCM_SET_CLASS(z, &~ClassName);"
+               #"  z->data = *v;")
+    (dolist [slot (~ cclass'slot-spec)]
+      (and-let1 init (~ slot'init-cexpr)
+        (cgen-body #"  z->data.~(~ slot'c-name) = ~|init|;")))
+    (cgen-body (if initializer
+                 #"{ ~|c-struct-name| *obj = &z->data;\n~(cadr initializer)\n}"
+                 "")
+               #"  return SCM_OBJ(z);"
+               #"}")
+    (cgen-add! cclass)))
+
+;; returns (<cslot> ...)
+(define (process-cstruct-slot cclass cclass-cname slot-spec)
+  (match-let1 (slot-name type c-field c-length c-init) slot-spec
+    (receive (type getter setter)
+        (match type
+          [('& rtype)
+           (make-embedded-struct-getter-setter cclass cclass-cname
+                                               slot-name rtype
+                                               c-field c-length c-init)]
+          [('.array etype)
+           (make-array-getter-setter cclass cclass-cname
+                                     slot-name etype #f
+                                     c-field c-length c-init)]
+          [('.array* etype)
+           (make-array-getter-setter cclass cclass-cname
+                                     slot-name etype #t
+                                     c-field c-length c-init)]
+          [_ (values type #t #t)])
+      `(,(make <cslot>
+           :cclass cclass :scheme-name slot-name :type (name->type type)
+           :c-name (get-c-name "" c-field)
+           :c-spec #f
+           :getter getter :setter setter
+           :init-cexpr c-init)))))
+
+;; Returns ((slot-name type c-field c-length c-init) ...)
+;; type can be (& type) for embedded types
+(define (cstruct-grok-slot-specs cclass slots)
+  ;; parse slot-name::type.  Returns slot name symbol and type symbol.
+  (define (parse-symbol::type sym)
+    (rxmatch-case (x->string sym)
+      [#/^(.*?)(::(&)?(.*))?$/ (_ name-s _ embed type-s)
+       (let* ([etype (if type-s (string->symbol type-s) '<top>)]
+              [type (if (equal? embed "&") `(& ,etype) etype)])
+         (values (string->symbol name-s) type))]
+      [_ (error <cgen-stub-error> "bad slot name::type:" sym)]))
+  ;; parse c-spec.  Returns c-field, c-length and init
+  ;; cclass and slot-name are only for error message.
+  (define (parse-c-spec slot-name c-spec)
+    (rxmatch-case c-spec
+      [#/^(\w+)?(?:\[(\w+)\])?(?:=(.*))?$/ (_ field length init)
+          (values (or field (x->string slot-name))
+                  (and length (or (string->number length) length))
+                  init)]
+      [else
+       (errorf "Bad c-spec ~s for a slot ~s of ~s" c-spec slot-name
+               (~ cclass'scheme-name))]))
+  (define (grok-1 slots)
+    (match slots
+      [((? symbol? y) . rest)
+       (if (#/::$/ (symbol->string y))
+         (match rest
+           [((and ((or '.array* '.array) etype) type) (? string? c-spec) . rest)
+            (receive (slot-name _) (parse-symbol::type y)
+              (receive (c-field c-length c-init)
+                  (parse-c-spec slot-name c-spec)
+                (values (list slot-name type c-field c-length c-init) rest)))]
+           [_ (error <cgen-stub-error> "bad slot spec in define-cstruct:"
+                     (take* slots 2))])
+         (match rest
+           [((? string? c-spec) . rest)
+            (receive (slot-name type) (parse-symbol::type y)
+              (receive (c-field c-length c-init)
+                  (parse-c-spec slot-name c-spec)
+                (values (list slot-name type
+                              (or c-field (x->string slot-name))
+                              c-length c-init)
+                        rest)))]
+           [_  (receive (slot-name type) (parse-symbol::type y)
+                 (values (list slot-name type (x->string slot-name) #f #f)
+                         rest))]))]
+      [_ (error <cgen-stub-error> "bad slot spec in define-cstruct:" slots)]))
+  (let loop ([slots slots] [r '()])
+    (if (null? slots)
+      (reverse r)
+      (receive (slot rest) (grok-1 slots)
+        (loop rest (cons slot r))))))
+
+;; Handle slot::(.array[*] <elt-type>) "c-name"
+;;   c-name : "c-field[c-length]"
+;;   cclass-cname is the C typename of the wrapper.
+;; Returns stub-type, getter-name, setter-name
+;; TODO: Make c-length field read-only.
+(define (make-array-getter-setter cclass cclass-cname
+                                  slot-name elt-type-name ptr?
+                                  c-field c-length c-init)
+  (define etype (name->type elt-type-name))
+  (define (gen-getter c-field c-length) ; returns getter name
+    (rlet1 getter-name #"~(~ cclass 'c-name)_~|c-field|_GET"
+      (cgen-decl   #"static ScmObj ~|getter-name|(ScmObj);")
+      (cgen-body   #"static ScmObj ~|getter-name|(ScmObj obj_s)"
+                   #"{"
+                   #"  ~|cclass-cname|* obj = (~|cclass-cname|*)obj_s;")
+      (if (string? c-length)
+        (cgen-body #"  if (obj->data.~|c-length| <= 0) return SCM_FALSE;"
+                   #"  ssize_t len = obj->data.~|c-length|;")
+        (cgen-body #"  ssize_t len = ~|c-length|;"))
+      (cgen-body   #"  ScmObj v = Scm_MakeVector(len, SCM_FALSE);"
+                   #"  for (ScmSmallInt i = 0; i < len; i++) {"
+                   #"    ~(~ etype'c-type) e = obj->data.~|c-field|[i];"
+                   #"    SCM_VECTOR_ELEMENT(v, i) ="
+                   #"      ~(cgen-box-expr etype \"e\");"
+                   #"  }"
+                   #"  return SCM_OBJ(v);"
+                   #"}")))
+  (define (gen-setter c-field c-length)
+    (rlet1 setter-name #"~(~ cclass 'c-name)_~|c-field|_SET"
+      (cgen-decl   #"static void ~|setter-name|(ScmObj, ScmObj);")
+      (cgen-body   #"static void ~|setter-name|(ScmObj obj_s, ScmObj val)"
+                   #"{"
+                   #"  ~|cclass-cname|* obj = (~|cclass-cname|*)obj_s;")
+      (when ptr?
+        (cgen-body #"  if (SCM_FALSEP(val)) {"
+                   #"    obj->data.~|c-length| = 0;"
+                   #"    obj->data.~|c-field| = NULL;"
+                   #"    return;"
+                   #"  }"))
+      (cgen-body   #"  if (!SCM_VECTORP(val)) SCM_TYPE_ERROR(val, \"vector\");"
+                   #"  ScmSmallInt vlen = SCM_VECTOR_SIZE(val);")
+      (when (number? c-length)
+        (cgen-body #"  if (vlen != ~|c-length|) {"
+                   #"     Scm_Error(\"Invalid length for ~|cclass-cname|.~|c-field|: %ld (must be ~|c-length|)\", vlen);"
+                   #"  }"))
+      (if ptr?
+        (cgen-body #"  ~(~ etype'c-type) *vs = SCM_NEW_ARRAY(~(~ etype'c-type), vlen);")
+        (cgen-body #"  ~(~ etype'c-type) *vs = obj->data.~|c-field|;"))
+      (cgen-body   #"  for (ScmSmallInt i = 0; i < vlen; i++) {"
+                   #"    ScmObj val_i = SCM_VECTOR_ELEMENT(val,i);"
+                   #"    if (!~(cgen-pred-expr etype \"val_i\")) {"
+                   #"      SCM_TYPE_ERROR(val_i, \"~(~ etype'name)\");"
+                   #"    }"
+                   #"    vs[i] = ~(cgen-unbox-expr etype \"val_i\");"
+                   #"  }")
+      (unless (number? c-length)
+        (cgen-body #"  obj->data.~|c-length| = vlen;"))
+      (when ptr?
+        (cgen-body #"  obj->data.~|c-field| = vs;"))
+      (cgen-body #"}")))
+
+  (values '<vector>
+          `(c ,(gen-getter c-field c-length))
+          `(c ,(gen-setter c-field c-length))))
+
+;; Handle slot::(& etype) "c-name"
+;;   c-name : "c-field"
+;;   cclass-cname is the C typename of the wrapper.
+;; Returns stub-type, getter-name, setter-name
+;; Whether the content of the embedded struct to be copied or not is
+;; up to the boxer of the embedded type.  If the embedded type
+;; is also a cstruct, the content is copied.  (That poses a problem when
+;; the user want to modify its field.  We'll provide setters for
+;; each individual subfields later, but the user code may want to carry
+;; around the embedded structure for modification).
+(define (make-embedded-struct-getter-setter cclass cclass-cname
+                                            slot-name embedded-type-name
+                                            c-field c-length c-init)
+  (define etype (name->type embedded-type-name))
+  (define (gen-getter c-field c-length) ; returns getter name
+    (rlet1 getter-name #"~(~ cclass 'c-name)_~|c-field|_GET"
+      (cgen-decl #"static ScmObj ~|getter-name|(ScmObj);")
+      (cgen-body #"static ScmObj ~|getter-name|(ScmObj obj_s)"
+                 #"{"
+                 #"  ~|cclass-cname|* obj = (~|cclass-cname|*)obj_s;"
+                 #"  ~(~ etype'c-type) e = &(obj->data.~|c-field|);"
+                 #"  return SCM_OBJ(~(cgen-box-expr etype \"e\"));"
+                 #"}")))
+  (define (gen-setter c-field c-length)
+    (rlet1 setter-name #"~(~ cclass 'c-name)_~|c-field|_SET"
+      (cgen-decl #"static void ~|setter-name|(ScmObj, ScmObj);")
+      (cgen-body #"static void ~|setter-name|(ScmObj obj_s, ScmObj val)"
+                 #"{"
+                 #"  ~|cclass-cname|* obj = (~|cclass-cname|*)obj_s;"
+                 #"  if (!~(cgen-pred-expr etype \"val\")) {"
+                 #"    SCM_TYPE_ERROR(val, \"~(~ etype'name)\");"
+                 #"  }"
+                 #"  ~(~ etype'c-type) e = ~(cgen-unbox-expr etype \"val\");"
+                 ;; We need customizable copyer, but for now...
+                 #"  obj->data.~|c-field| = *e;"
+                 #"}")))
+
+  (unless etype
+    (error <cgen-stub-error> "unknown type can't be embedded:"
+           embedded-type-name))
+  (values embedded-type-name
+          `(c ,(gen-getter c-field c-length))
+          `(c ,(gen-setter c-field c-length))))
 
 ;;===================================================================
 ;; Foreign pointers
