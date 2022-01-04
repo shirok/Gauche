0e61ccc0767814efab4463f02a883bc6ee9f2710
Shiro Kawai <shiro@acm.org>
2022-01-04 03:19:10 -1000

    Fix static library and mbedtls-internal issue
    See https://github.com/shirok/Gauche/issues/802

--- src/list-ext-objects.sh
+++ src/list-ext-objects.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 #
-# list-ext-objects $(top_builddir)
+# list-ext-objects $(top_builddir) <extra-dir> ...
 #
 #   Gather list of object files in ext/ to be included in libgauche-static.a
 #
+#   Directories under ext/ are scanned (via `make list-objects`) unconditionally.
+#   Directories given in <extra-dir> are also scanned; they must be relative
+#   to $top_builddir.  The makefile in the directory must recognize
+#   list-objects target.  Currently it may be used to scan internal mbedtls
+#   library.
 
 top_builddir=$1
 
-cd $top_builddir/ext && ${MAKE:=make} list-objects | grep '^/// ' | sed "sx/// xx"
+(cd $top_builddir/ext && ${MAKE:=make} list-objects | grep '^/// ' | sed "sx/// xx")
 
-
+shift
+while [ $# -gt 0 ]; do
+    (cd $top_builddir/$1 && ${MAKE:=make} list-objects | grep '^/// ' | sed "sx/// xx")
+    shift
+done
