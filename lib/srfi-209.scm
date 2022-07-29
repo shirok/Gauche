6bfeb8685bd64b52d4b7b696c40fa3e1a84c1fee
Shiro Kawai <shiro@acm.org>
2022-07-29 13:31:34 -1000

    More srfi-209
    Covered all API, but not tested.

--- lib/srfi-209.scm
+++ lib/srfi-209.scm
@@ -48,7 +48,7 @@
           enum-type-enums enum-type-names enum-type-values
           enum-next enum-prev
           make-enum-comparator
-          enum-type->enum-set enum-set
+          enum-type->enum-set enum-set enum-empty-set
           list->enum-set
           enum-set-projection
           enum-set-copy
@@ -120,12 +120,11 @@
         (hash-table-put! h sym (%make-enum et sym ord val))))
     (set! (~ et'%ordinal->enum)
           (map-to <vector> (^e (hash-table-get h (car e))) elts.))
-    (unless (= (vector-lenngth (~ et'%ordinal->enum))
+    (unless (= (vector-length (~ et'%ordinal->enum))
                (hash-table-num-entries h))
       (error "Duplicate enum name: " elts))
     et))
 
-;; API
 (define (enum-type? obj) (is-a? obj <enum-type>))
 
 (define (enum-type-contains? etype enum)
@@ -133,43 +132,72 @@
   (assume-type enum <enum>)
   (eq? (enum-type enum) etype))
 
-(define (enum-name->enum enum-type name)
-  (assume-type etype <enum-type>)
-  (hash-table-get (~ enum-type'%name->enum) name #f))
+(define (%assume-same-etype etype e0 e1)
+  (assume (eqv? etype (enum-type e1))
+          "Enums don't belong to the same enum type:" e0 e1))
 
-(define (enum-ordinal->enum enum-type n)
-  (assume-type etype <enum-type>)
-  (vector-ref (~ enum-type'%ordinal->enum) n #f))
+(define (enum=? e0 e1 . es)
+  (define etype (enum-type e0))
+  (%assume-same-etype etype e0 e1)
+  (and (eq? e0 e1)
+       (or (null? es)
+           (any (^e (%assume-same-etype etype e0 e)
+                    (eq? e0 e))
+                es))))
 
-(define (enum-name->ordinal enum-type name)
-  (if-let1 e (enum-name->enum enum-type name)
+(define (%enum-compare op e0 e1 es)
+  (define etype (enum-type e0))
+  (%assume-same-etype etype e0 e1)
+  (and (op (enum-ordinal e0) (enum-ordinal e1))
+       (or (null? es)
+           (any (^[ea eb]
+                  (%assume-same-etype etype e0 eb)
+                  (op (enum-ordinal ea) (enum-ordinal eb)))
+                (cons e1 es)
+                es))))
+
+(define (enum<? e0 e1 . es)  (%enum-compare < e0 e1 es))
+(define (enum<=? e0 e1 . es) (%enum-compare <= e0 e1 es))
+(define (enum>? e0 e1 . es)  (%enum-compare > e0 e1 es))
+(define (enum>=? e0 e1 . es) (%enum-compare >= e0 e1 es))
+
+(define (enum-name->enum etype name)
+  (assume-type etype <enum-type>)
+  (hash-table-get (~ etype'%name->enum) name #f))
+
+(define (enum-ordinal->enum etype n)
+  (assume-type etype <enum-type>)
+  (vector-ref (~ etype'%ordinal->enum) n #f))
+
+(define (enum-name->ordinal etype name)
+  (if-let1 e (enum-name->enum etype name)
     (enum-ordinal e)
-    (errorf "enum-type ~s doesn't have enum named ~s" enum-type name)))
+    (errorf "enum-type ~s doesn't have enum named ~s" etype name)))
 
-(define (enum-name->value enum-type name)
-  (if-let1 e (enum-name->enum enum-type name)
+(define (enum-name->value etype name)
+  (if-let1 e (enum-name->enum etype name)
     (enum-value e)
-    (errorf "enum-type ~s doesn't have enum named ~s" enum-type name)))
+    (errorf "enum-type ~s doesn't have enum named ~s" etype name)))
 
-(define (enum-ordinal->name enum-type n)
-  (if-let1 e (enum-ordinal->enum enum-type n)
+(define (enum-ordinal->name etype n)
+  (if-let1 e (enum-ordinal->enum etype n)
     (enum-name e)
-    (errorf "enum-type ~s doesn't have enum with oridnal ~s" enum-type n)))
+    (errorf "enum-type ~s doesn't have enum with oridnal ~s" etype n)))
 
-(define (enum-ordinal->value enum-type n)
-  (if-let1 e (enum-ordinal->enum enum-type n)
+(define (enum-ordinal->value etype n)
+  (if-let1 e (enum-ordinal->enum etype n)
     (enum-value e)
-    (errorf "enum-type ~s doesn't have enum with oridnal ~s" enum-type n)))
+    (errorf "enum-type ~s doesn't have enum with oridnal ~s" etype n)))
 
-(define (enum-type-size enum-type)
-  (assume-type enum-type <enum-type>)
-  (vector-length (~ enum-type'%ordinal->enum)))
+(define (enum-type-size etype)
+  (assume-type etype <enum-type>)
+  (vector-length (~ etype'%ordinal->enum)))
 
-(define (enum-min enum-type)
-  (let1 t (enum-type-size enum-type)
+(define (enum-min etype)
+  (let1 t (enum-type-size etype)
     (when (zero? t)
-      (error "Cannot take enum-min from zero-element enum-type:" enum-type))
-    (~ enum-type'%ordinal->enum 0)))
+      (error "Cannot take enum-min from zero-element enum-type:" etype))
+    (~ etype'%ordinal->enum 0)))
 
 (define (enum-max enum-type)
   (let1 t (enum-type-size enum-type)
@@ -188,25 +216,58 @@
   (assume-type enum-type <enum-type>)
   (map enum-value (enum-type-enums enum-type)))
 
+(define (enum-next enum)
+  (define etype (enum-type enum))
+  (and (< (enum-ordinal enum) (- (enum-type-size etype) 1))
+       (enum-ordinal->enum etype (+ (enum-ordinal enum) 1))))
+
+(define (enum-prev enum)
+  (define etype (enum-type enum))
+  (and (> (enum-ordinal enum) 0)
+       (enum-ordinal->enum etype (- (enum-ordinal enum) 1))))
+
+(define (make-enum-comparator etype)
+  (make-comparator (^e (and (enum? e) (eqv? (enum-type e) etype)))
+                   enum=?
+                   enum<?
+                   default-hash))
 
 ;;
 ;; enum-set
 ;;
 
-(define (enum-type->enum-set enum-type)
+(define (enum-type->enum-set etype)
   (make <enum-type>
-    :enum-type enum-type
-    :members (make-bitvector (enum-type-size enum-type) 1)))
+    :enum-type etype
+    :members (make-bitvector (enum-type-size etype) 1)))
 
 (define (enum-set enum-type . enums)
   (list->enum-set enum-type enums))
+
+;; NB: This is missing from srfi text, but exists in reference impl and tests
+(define (enum-empty-set etype)
+  (assume-type etype <enum-type>)
+  (make <enum-type>
+    :enum-type etype
+    :members (make-bitvector (enum-type-size etype) 0)))
+
+(define (enum-set-type eset)
+  (assume-type eset <enum-set>)
+  (~ eset'enum-type))
+
+(define (enum-set-indexer eset)         ;R6RS
+  (assume-type eset <enum-set>)
+  (let1 etype (enum-set-type eset)
+    (^[name] (and-let1 e (enum-name->enum etype name)
+               (and (enum-set-contains? etype e)
+                    (enum-ordinal e))))))
 
 (define (list->enum-set enum-type enums)
   (let1 members (make-bitvector (enum-type-size enum-type) 0)
     (dolist [e enums]
       (unless (enum-type-contains? enum-type e)
         (errorf "enum ~s isn't a member of given enum type ~s" e enum-type))
-      (bitvetor-set! members (enum-ordinal e) 1))
+      (bitvector-set! members (enum-ordinal e) 1))
     (make <enum-type> :enum-type enum-type :members members)))
 
 ;; NB: srfi isn't clear if enum-set contains an enum whose name
@@ -219,12 +280,28 @@
     (list->enum-set type
                     (enum-set-map->list
                      (^e (enum-name->enum type (enum-name e)))
-                     enums))))
+                     enum-set))))
 
-(define (enum-set-copy enum-set)
+(define (enum-set-copy eset)
   (make <enum-set>
-    :enum-type (~ enum-set'enum-type)
-    :members (bitvector-copy (~ enum-set'members))))
+    :enum-type (~ eset'enum-type)
+    :members (bitvector-copy (~ eset'members))))
+
+(define (make-enumeration names)        ;R6RS
+  (enum-type->enum-set (make-enum-type names)))
+
+(define (enum-set-universe eset)        ;R6RS
+  (assume-type eset <enum-set>)
+  (enum-set (enum-set-type eset)))
+
+(define (enum-set-constructor eset)     ;R6RS
+  (define etype (enum-set-type eset))
+  (^[names] (let1 es (map (^n (or (enum-name->enum etype n)
+                                  (errorf "enum-type ~s doesn't have name: ~s"
+                                          etype n)))
+                          names)
+              (list->enum-set etype es))))
+
 
 (define (enum-set? obj) (is-a? obj <enum-set>))
 
@@ -247,7 +324,7 @@
 ;; NB: srfi is unclear if two sets are not from the same enum-type.
 (define (enum-set-disjoint? enum-set1 enum-set2)
   (assume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
-  (bitvector-zero? (bitvector-and (~ enum-set1'members)
+  (bitvector-none? (bitvector-and (~ enum-set1'members)
                                   (~ enum-set2'members))))
 
 (define (enum-set=? enum-set1 enum-set2)
@@ -275,9 +352,9 @@
        (enum-set>=? enum-set1 enum-set2)))
 
 ;; Two enum sets can belong to different enum types.
-(define (enum-set-subset? enum-set-1 enum-set-2)
+(define (enum-set-subset? enum-set1 enum-set2)
   (enum-set-every? (^e (enum-set-member? (enum-name e) enum-set2))
-                   enum-set-1))
+                   enum-set1))
 
 (define (enum-set-any? pred enum-set)
   (assume-type enum-set <enum-set>)
@@ -333,7 +410,7 @@
 (define (enum-set-delete-all! enum-set . lists)
   (assume-type enum-set <enum-set>)
   (update! (~ enum-set'members)
-           (cut fold (^[enums bv] (%update-members! enum-set <> enums #f))
+           (cut fold (^[enums bv] (%update-members! enum-set bv enums #f))
                 <> lists))
   enum-set)
 
@@ -347,9 +424,12 @@
 (define (enum-set->enum-list enum-set)
   (enum-set-map->list identity enum-set))
 
+(define (enum-set->list enum-set)       ;R6RS
+  (enum-set-map->list enum-name enum-set))
+
 (define (enum-set-count pred enum-set)
   (rlet1 c 0
-    (enum-for-each (^e (when (pred e) (inc! c))) enum-set)))
+    (enum-set-for-each (^e (when (pred e) (inc! c))) enum-set)))
 
 (define (enum-set-filter pred enum-set)
   (define bv (make-bitvector (bitvector-length (~ enum-set'members)) 0))
@@ -361,7 +441,7 @@
     :members bv))
 
 (define (enum-set-remove pred enum-set)
-  (define bv (copy-bitvector (~ enum-set'members)))
+  (define bv (bitvector-copy (~ enum-set'members)))
   ($ enum-set-for-each
      (^e (when (pred e) (bitvector-set! bv (enum-ordinal e) 0)))
      enum-set)
@@ -379,14 +459,14 @@
   (define etype (enum-set-type enum-set))
   ($ bitvector-value-for-each-index
      (^i (proc (enum-ordinal->enum etype i)))
-     (~ enum-set-'members) #t))
+     (~ enum-set'members) #t))
 
 (define (enum-set-fold proc seed enum-set)
   (define etype (enum-set-type enum-set))
   ($ bitvector-value-fold-index
      (^[i s] (proc (enum-ordinal->enum etype i) s))
      seed
-     (~ enum-set-'members) #t))
+     (~ enum-set'members) #t))
 
 (define (enum-set-complement enum-set)
   (make <enum-set>
@@ -398,49 +478,49 @@
   enum-set)
 
 (define (enum-set-union enum-set1 enum-set2)
-  (asssume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
+  (assume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
   (make <enum-set>
     :enum-type (enum-set-type enum-set1)
     :members (bitvector-ior (~ enum-set1'members) (~ enum-set2'members))))
 
 (define (enum-set-union! enum-set1 enum-set2)
-  (asssume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
+  (assume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
   (update! (enum-set1'members)
            (cute bitvector-ior! <> (~ enum-set2'members)))
   enum-set1)
 
 (define (enum-set-intersection enum-set1 enum-set2)
-  (asssume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
+  (assume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
   (make <enum-set>
     :enum-type (enum-set-type enum-set1)
     :members (bitvector-and (~ enum-set1'members) (~ enum-set2'members))))
 
 (define (enum-set-intersection! enum-set1 enum-set2)
-  (asssume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
+  (assume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
   (update! (enum-set1'members)
            (cute bitvector-and! <> (~ enum-set2'members)))
   enum-set1)
 
 (define (enum-set-difference enum-set1 enum-set2)
-  (asssume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
+  (assume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
   (make <enum-set>
     :enum-type (enum-set-type enum-set1)
     :members (bitvector-andc2 (~ enum-set1'members) (~ enum-set2'members))))
 
 (define (enum-set-difference! enum-set1 enum-set2)
-  (asssume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
+  (assume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
   (update! (enum-set1'members)
            (cute bitvector-andc2! <> (~ enum-set2'members)))
   enum-set1)
 
 (define (enum-set-xor enum-set1 enum-set2)
-  (asssume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
+  (assume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
   (make <enum-set>
     :enum-type (enum-set-type enum-set1)
     :members (bitvector-xor (~ enum-set1'members) (~ enum-set2'members))))
 
 (define (enum-set-xor! enum-set1 enum-set2)
-  (asssume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
+  (assume (eqv? (enum-set-type enum-set1) (enum-set-type enum-set2)))
   (update! (enum-set1'members)
            (cute bitvector-xor! <> (~ enum-set2'members)))
   enum-set1)
@@ -457,13 +537,39 @@
         (let ([etype (make-enum-type name-value type-name)])
           (quasirename r
             `(begin
-               (define-syntax type-name
+               (define-syntax ,type-name
                  (er-macro-transformer
                   (^[ff rr cc]
                     (match ff
                       [(_ name) (enum-name->enum ,etype name)]
                       [_ (error "enum type macro must take a symbol:" ff)]))))
-               (define-syntax set-ctor
+               (define-syntax ,set-ctor
+                 (er-macro-transformer
+                  (^[ff rr cc]
+                    (list->enum-set ,etype
+                                    (map (cut enum-name->enum ,etype <>)
+                                         (cdr ff))))))
+               )))]
+       ))))
+
+(define-syntax define-enumeration       ;R6RS
+  (er-macro-transformer
+   (^[f r c]
+     (match f
+       [(_ type-name (name-value ...) set-ctor)
+        (let ([etype (make-enum-type name-value type-name)])
+          (quasirename r
+            `(begin
+               (define-syntax ,type-name
+                 (er-macro-transformer
+                  (^[ff rr cc]
+                    (match ff
+                      [(_ name) (if (enum-name->enum ,etype name)
+                                  name
+                                  (errorf "enum ~s doesn't belong to ~a"
+                                          name ',type-name))]
+                      [_ (error "enum type macro must take a symbol:" ff)]))))
+               (define-syntax ,set-ctor
                  (er-macro-transformer
                   (^[ff rr cc]
                     (list->enum-set ,etype
