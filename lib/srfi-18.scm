7b215b0aab146e1aca7b5292fb6f27e3e6edd943
Shiro Kawai <shiro@acm.org>
2022-10-01 11:59:23 -1000

    Restrict exported symbols from srfi-18
    Just extending gauche.threads includes non-srfi bindings, so we
    explicitly listed exports.

--- lib/srfi-18.scm
+++ lib/srfi-18.scm
@@ -3,4 +3,29 @@
 ;; In Gauche, srfi-18 is covered by gauche.threads
 ;;
 
-(define-module srfi-18 (extend gauche.threads))
+(define-module srfi-18
+  (use gauche.threads)
+  (export current-thread
+
+          thread? make-thread thread-name thread-specific-set! thread-specific
+          thread-start! thread-yield! thread-sleep!
+          thread-join! thread-terminate!
+
+          mutex? make-mutex mutex-name mutex-state
+          mutex-specific-set! mutex-specific
+          mutex-lock! mutex-unlock!
+
+          condition-variable? make-condition-variable condition-variable-name
+          condition-variable-specific condition-variable-specific-set!
+          condition-variable-signal! condition-variable-broadcast!
+
+          current-time time? time->seconds seconds->time
+
+          current-exception-handler     ;re-export from built-in
+          with-exception-handler        ; ditto
+          raise                         ; ditto
+
+          join-timeout-exception? abandoned-mutex-exception?
+          terminated-thread-exception? uncaught-exception?
+          uncaught-exception-reason
+          ))
