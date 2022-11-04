980b863b751c15245140841e7aed12060cc4e13d
Shiro Kawai <shiro@acm.org>
2022-11-04 09:50:34 -1000

    Modify Gasbichler's shift/reset to match builtins as possible

--- lib/gauche/partcont-meta.scm
+++ lib/gauche/partcont-meta.scm
@@ -7,33 +7,51 @@
 ;; http://citeseer.ist.psu.edu/gasbichler02final.html
 ;;
 ;; This is for the reference.  Gauche has built-in support of shift/reset,
-;; and its behavior and this should match.  Sometimes the internal change
-;; introduces subtle bugs, and this module helps to see how it should work.
+;; and its behavior and this should match, except that this version may
+;; execute before/after thunks redundantly.
+;  Sometimes the internal change introduces subtle bugs, and this module
+;; helps to see how it should work.
+;;
+;; Modifications from Gasbichler's paper:
+;;  - Using thread-local instead of a simple global variable for
+;;    *meta-continuation*
+;;  - *meta-continuation* takes thunk instead of values.  It is because the body
+;;    of shift must be evaluated in the dynamic environment of the corresponding
+;;    reset; the original implementation evaluates it in the dynamic
+;;    environment of shift, which is wrong.
+;;    The thunk is later evaluated in %reset.
+;;  - Strictly speaking, nestings of reset must be equal or greater than
+;;    the nesting of shift.  The original implementation throws an error
+;;    if shifts are nested deeper.  In Gauche we have system's implicit
+;;    resets, so it is not necessarily the case.  This implementation
+;;    just evaluates the passed thunk if stale meta-continuation is invoked.
 
 (define-module gauche.partcont-meta
-  (use gauche.parameter)
+  (use gauche.threads)
   (export reset shift call/pc))
 (select-module gauche.partcont-meta)
 
-(define meta-continuation
-  (make-parameter
-   (^ _ (error "stale meta-continuation invoked"))))
+(define *meta-continuation*
+  (make-thread-local
+   (^[thunk] (thunk))
+   'meta-continuation))
 
 (define-syntax reset
   (syntax-rules ()
     [(_ expr ...) (%reset (^[] expr ...))]))
 
 (define (%abort thunk)
-  (receive v (thunk)
-    (apply (meta-continuation) v)))
+  ((tlref *meta-continuation*) thunk))
 
 (define (%reset thunk)
-  (let1 save (meta-continuation)
-    (let/cc k
-      (meta-continuation (^ vals
-                           (meta-continuation save)
-                           (apply k vals)))
-      (%abort thunk))))
+  (let1 save (tlref *meta-continuation*)
+    ((let/cc k
+       (tlset! *meta-continuation*
+               (^[thunk]
+                 (tlset! *meta-continuation* save)
+                 (k thunk)))
+       (receive r (thunk)
+         (%abort (^[] (apply values r))))))))
 
 (define-syntax shift
   (syntax-rules ()
