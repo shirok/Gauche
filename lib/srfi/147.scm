14aeefca1338325aff33fcb6f9c964f9b876b1b1
Shiro Kawai <shiro@acm.org>
2022-12-12 20:27:37 -1000

    Add srfi/147.scm so that (use srfi.147) won't complain

---
+++ lib/srfi/147.scm
@@ -0,0 +1,4 @@
+;; srfi-147 is fully supported in the core; this file is to make
+;; (import (srfi 147)) work.
+(define-module srfi.147
+  (export define-syntax let-syntax letrec-syntax syntax-rules))
