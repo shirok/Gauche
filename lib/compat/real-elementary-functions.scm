c9620852c1f8cf2013387dd308b33df8c2c015d2
Shiro Kawai <shiro@acm.org>
2023-04-29 21:55:22 -1000

    Rename compat.real-trig -> compat.real-elementary-functions
    It's not only trigonometric functions.

--- lib/compat/real-elementary-functions.scm
+++ lib/compat/real-elementary-functions.scm
@@ -1,5 +1,5 @@
 ;;;
-;;; compat.real-trig
+;;; compat.real-elementary-functions
 ;;;
 
 ;; Gauche 0.9.12 and earlier had real-only trigonometric functions
@@ -7,10 +7,10 @@
 ;; 0.9.13 renamed them as real-sin etc. and made them official.  This
 ;; module is to run code that used old undocumented API.
 
-(define-module compat.real-trig
+(define-module compat.real-elementary-functions
   (export %exp %sin %cos %tan %log %sinpi %cospi %tanpi %asin %acos %atan
           %sinh %cosh %tanh %sqrt %expt))
-(select-module compat.real-trig)
+(select-module compat.real-elementary-functions)
 
 (define %exp real-exp)
 (define %sin real-sin)
