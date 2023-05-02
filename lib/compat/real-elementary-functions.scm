3effc0f2aed0cf6e8b0c9ac5fea9b0f3b30b1cef
Shiro Kawai <shiro@acm.org>
2023-05-01 22:46:16 -1000

    Make %log alias of real-ln, add %asinh etc.

--- lib/compat/real-elementary-functions.scm
+++ lib/compat/real-elementary-functions.scm
@@ -9,14 +9,14 @@
 
 (define-module compat.real-elementary-functions
   (export %exp %sin %cos %tan %log %sinpi %cospi %tanpi %asin %acos %atan
-          %sinh %cosh %tanh %sqrt %expt))
+          %sinh %cosh %tanh %asinh %acosh %atanh %sqrt %expt))
 (select-module compat.real-elementary-functions)
 
 (define %exp real-exp)
 (define %sin real-sin)
 (define %cos real-cos)
 (define %tan real-tan)
-(define %log log)                       ;no built-in real-log
+(define %log real-ln)
 (define %sinpi real-sinpi)
 (define %cospi real-cospi)
 (define %tanpi real-tanpi)
@@ -26,5 +26,8 @@
 (define %sinh real-sinh)
 (define %cosh real-cosh)
 (define %tanh real-tanh)
+(define %asinh real-asinh)
+(define %acosh real-acosh)
+(define %atanh real-atanh)
 (define %sqrt real-sqrt)
 (define %expt real-expt)
