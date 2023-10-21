448e811ef553c312eca52ac121062c7018a1b645
Shiro Kawai <shiro@acm.org>
2023-10-20 20:03:40 -1000

    scheme.list - Add missing exports
    We need to export identifiers that are also in scheme.base, for
    a program may only import scheme.list without importing scheme.base.

--- libsrc/scheme/list.scm
+++ libsrc/scheme/list.scm
@@ -20,27 +20,35 @@
 (define-module scheme.list
   ;; The export list includes the procedures defined in #<module gauche>
   ;; in order to make (import (srfi 1)) work for r7rs programs.
-  (export xcons cons* make-list list-tabulate list-copy circular-list iota
-          proper-list? circular-list? dotted-list?
+  (export cons list xcons cons* make-list list-tabulate
+          list-copy circular-list iota
+          pair? null? proper-list? circular-list? dotted-list?
           not-pair? null-list? list=
+          car cdr caar cadr cdar cddr caaar caadr cadar caddr
+          cdaar cdadr cddar cdddr caaaar caaadr caadar caaddr
+          cadaar cadadr caddar cadddr cdaaar cdaadr cdadar cdaddr
+          cddaar cddadr cdddar cddddr list-ref
           first second third fourth fifth sixth seventh eighth ninth tenth
           car+cdr take drop take-right drop-right
           take! drop-right! split-at split-at! last last-pair
-          length+ concatenate reverse append! concatenate! reverse!
+          length length+ append concatenate reverse
+          append! concatenate! reverse!
           append-reverse append-reverse!
           zip unzip1 unzip2 unzip3 unzip4 unzip5 count
-          fold unfold pair-fold reduce fold-right unfold-right
+          map for-each fold unfold pair-fold reduce fold-right unfold-right
           pair-fold-right reduce-right append-map append-map!
           map! pair-for-each filter-map map-in-order
           filter partition remove filter! partition! remove!
-          find find-tail any every list-index take-while drop-while
+          member memq memv find find-tail any every list-index
+          take-while drop-while
           take-while! span break span! break!
           delete delete-duplicates delete! delete-duplicates!
-          alist-cons alist-copy alist-delete alist-delete!
+          assoc assq assv alist-cons alist-copy alist-delete alist-delete!
           lset<= lset= lset-adjoin lset-union lset-union!
           lset-intersection lset-intersection! lset-difference
           lset-difference! lset-xor lset-xor!
           lset-diff+intersection lset-diff+intersection!
+          set-car! set-cdr!
           ))
 (select-module scheme.list)
 
