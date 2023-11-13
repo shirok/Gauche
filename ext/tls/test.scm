e65b0759a2e9fdf1601b63f9a7a112b51863efa8
Shiro Kawai <shiro@acm.org>
2023-11-13 00:45:44 -1000

    Do not run tls test if not supported

--- ext/tls/test.scm
+++ ext/tls/test.scm
@@ -11,27 +11,32 @@
 (use rfc.tls.mbed)
 (test-module 'rfc.tls.mbed)
 
-(test-section "communication")
+(cond-expand
+ [gauche.net.tls
 
-(define (make-server-thread-1 bound-tls)
-  (^[]
-    (guard (e [else (report-error e) #f])
-      (let1 clnt (tls-accept bound-tls)
-        (tls-close clnt)))))
+  (test-section "communication")
 
-(let ((serv (make <mbed-tls> :server-name "localhost"))
-      (serv-thread #f))
-  (unwind-protect
-      (begin
-        (test* "simple communication" #t
-               (is-a? (tls-bind serv #f "8087" 'tcp) <mbed-tls>))
-        ;; Not ready yet
-        ;; (set! serv-thread (make-thread (make-server-thread-1 serv)))
-        ;; (thread-start! serv-thread)
-        ;; (test* "connect" #t
-        ;;        (let1 clnt (make <mbed-tls> :server-name "localhost")
-        ;;           (tls-connect clnt "localhost" "8087" 'tcp)))
-        )
-    (tls-close serv)))
+  (define (make-server-thread-1 bound-tls)
+    (^[]
+      (guard (e [else (report-error e) #f])
+        (let1 clnt (tls-accept bound-tls)
+          (tls-close clnt)))))
+
+  (let ((serv (make <mbed-tls> :server-name "localhost"))
+        (serv-thread #f))
+    (unwind-protect
+        (begin
+          (test* "simple communication" #t
+                 (is-a? (tls-bind serv #f "8087" 'tcp) <mbed-tls>))
+          ;; Not ready yet
+          ;; (set! serv-thread (make-thread (make-server-thread-1 serv)))
+          ;; (thread-start! serv-thread)
+          ;; (test* "connect" #t
+          ;;        (let1 clnt (make <mbed-tls> :server-name "localhost")
+          ;;           (tls-connect clnt "localhost" "8087" 'tcp)))
+          )
+      (tls-close serv)))
+  ]
+ [else])
 
 (test-end)
