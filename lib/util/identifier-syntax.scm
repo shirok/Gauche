33c895a3161acc3caf32a8577beb8bb63dc87b8e
Shiro Kawai <shiro@acm.org>
2024-08-03 21:14:16 -1000

    Add util.identifier-syntax
    We provide R6RS identifier-syntax as a separate module.

---
+++ lib/util/identifier-syntax.scm
@@ -0,0 +1,58 @@
+;;;
+;;; util.identifier-syntax - R6RS identifier-syntax
+;;;
+;;;   Copyright (c) 2024  Shiro Kawai  <shiro@acm.org>
+;;;
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;
+;;;   3. Neither the name of the authors nor the names of its contributors
+;;;      may be used to endorse or promote products derived from this
+;;;      software without specific prior written permission.
+;;;
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+(define-module util.identifier-syntax
+  (export identifier-syntax))
+(select-module util.identifier-syntax)
+
+;; Provides R6RS identifier-syntax
+;;
+;;  We make it in a separate library, for we generally discourage
+;;  the use of id-macro.  Also we don't like the syntax of
+;;  identifier-syntax as in R6RS; it has two different syntax,
+;;  which seems arbitrary.
+
+(define-syntax identifier-syntax
+  (syntax-rules (set!)
+    [(_ template)
+     (make-id-transformer
+      (syntax-rules (set!)
+        [(set! _ expr)
+         (syntax-error "Id macro can't be used with set!")]
+        [_ template]))]
+    [(_ ((_ template1)
+         ((set! _ pat2) template2)))
+     (make-id-transformer
+      (syntax-rules (set!)
+        [(set! _ pat2) template2]
+        [_ template1]))]))
