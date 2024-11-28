ccda20567914ce863efb0a69011534198f1d8420
Shiro Kawai <shiro@acm.org>
2024-11-28 11:17:48 -1000

    Update copyright text of srfi reference implementations
    A couple of them now adopt standard MIT license.

--- lib/srfi/7.scm
+++ lib/srfi/7.scm
@@ -2,19 +2,25 @@
 ;; Gauche module stuff added by Alex Shinn.
 
 ;; Copyright (C) Richard Kelsey (1999). All Rights Reserved.
-
-;; This document and translations of it may be copied and furnished to
-;; others, and derivative works that comment on or otherwise explain it or
-;; assist in its implementation may be prepared, copied, published and
-;; distributed, in whole or in part, without restriction of any kind,
-;; provided that the above copyright notice and this paragraph are included
-;; on all such copies and derivative works. However, this document itself
-;; may not be modified in any way, such as by removing the copyright notice
-;; or references to the Scheme Request For Implementation process or
-;; editors, except as needed for the purpose of developing SRFIs in which
-;; case the procedures for copyrights defined in the SRFI process must be
-;; followed, or as required to translate it into languages other than
-;; English.
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining
+;; a copy of this software and associated documentation files (the
+;; "Software"), to deal in the Software without restriction, including
+;; without limitation the rights to use, copy, modify, merge, publish,
+;; distribute, sublicense, and/or sell copies of the Software, and to
+;; permit persons to whom the Software is furnished to do so, subject to
+;; the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+;; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+;; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ;; [SK] the SRFI-7 reference implementation using cond-expand expands
 ;; a form that ends with (begin), although the empty begin isn't defined
