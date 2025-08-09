661ed7ea55a8428f80f5f67d6968f6b13f79e4d4
Shiro Kawai <shiro@acm.org>
2025-08-08 18:00:33 -1000

    text.multicolumn - Support packed style

--- lib/text/multicolumn.scm
+++ lib/text/multicolumn.scm
@@ -34,6 +34,7 @@
 (define-module text.multicolumn
   (use util.match)
   (use text.tree)
+  (use scheme.division)
   (export display-multicolumn layout-multicolumn))
 (select-module text.multicolumn)
 
@@ -46,12 +47,14 @@
                                   (minimum-width 8) ; minimum column width
                                   (max-columns 4)
                                   (order 'column)
-                                  (indent 0))
+                                  (indent 0)
+                                  (style 'even))
   (define indent-string (make-string indent #\space))
   (dolist [line (layout-multicolumn strs
                                     :width (- width indent)
                                     :minimum-width minimum-width
-                                    :max-columns max-columns :order order)]
+                                    :max-columns max-columns :order order
+                                    :style style)]
     (display indent-string)
     (for-each write-tree line)
     (newline)))
@@ -59,22 +62,69 @@
 ;; underlying utility of display-multicolumn.
 ;; Returns a list of string-list; each string-list is for a line.
 (define (layout-multicolumn strs
-                            :key (width 80) ; total width
-                                 (minimum-width 8) ; minimum column width
+                            :key (width 80)
+                                 (minimum-width 8)
                                  (max-columns 4)
-                                 (order 'column))
+                                 (order 'column)
+                                 (style 'even))
+  (define num-items (length strs))
+  [define max-col-width
+    (max (+ 2 (apply max 0 (map string-length strs)))
+         minimum-width)]
+  (define (make-tr-matrix rows)         ; return transposed matrix
+    (case order
+      [(row) ($ map list
+                   $* slices strs (ceiling->exact (/ num-items rows)) #t "")]
+      [(column) (slices strs rows #t "")]))
+  (define (column-widths tr-matrix)
+    (map (^[ss] (apply max (map (^s (+ 2 (string-length s))) ss))) tr-matrix))
+
+  ;; Packers - Returns tr-matrix and column-widths
+  ;;
+  ;; even-packer - Use uniform column width
+  (define (even-packer rows)
+    (let1 trm (make-tr-matrix rows)
+      (values trm
+              (map (constantly max-col-width) (iota (length (car trm)))))))
+  ;; dense-packer - Try more rows if possible.
+  (define (dense-packer rows)
+    (let try ([rows rows])
+      (let* ([tr-matrix (make-tr-matrix rows)]
+             [col-widths (column-widths tr-matrix)]
+             [cols (length col-widths)]
+             [total-width (apply + col-widths)])
+        (cond [(> total-width width) (values #f #f)]
+              [(and (< cols max-columns)
+                    (<= total-width width)
+                    (> rows 1))
+               (receive (trm cws) (try (- rows 1))
+                 (if trm
+                   (values trm cws)
+                   (values tr-matrix col-widths)))]
+              [else (values tr-matrix col-widths)]))))
+
+  (define (pad-items col-widths row)
+    (let rec ([row row]
+              [widths col-widths])
+      ;; We avoid trailing spaces
+      (match row
+        [(w) (if (equal? w "") '() (list w))]
+        [(w . ws) (let1 s (rec ws (cdr widths))
+                    (if (null? s)
+                      (if (equal? w "") '() (list w))
+                      (cons (format "~va" (car widths) w) s)))])))
+
+  (define (transpose tr-matrix)
+    (apply map list tr-matrix))
+
   (if (null? strs)
     '()
-    (let* ([col-width (max (+ 2 (apply max (map string-length strs)))
-                           minimum-width)]
-           [cols (clamp (quotient width col-width) 1 max-columns)]
-           [rows (quotient (+ (length strs) cols -1) cols)])
-      (define (layout-1 words)
-        (let rec ([words words])
-          (match words
-            [(w) (list w)]
-            [(w #f . _) (list w)]         ; #f is padding
-            [(w . ws) (cons (format "~va" col-width w) (rec ws))])))
-      (ecase order
-        [(column) ($ map (^ ss (layout-1 ss)) $* slices strs rows #t #f)]
-        [(row) ($ map layout-1 $ slices strs cols)]))))
+    (let* ([min-cols (clamp (floor-quotient width max-col-width)
+                            1 max-columns)]
+           [max-rows (quotient (+ num-items min-cols -1) min-cols)]
+           [widths (map (^_ max-col-width) (iota min-cols))])
+      (receive (tr-matrix col-widths)
+          (ecase style
+            [(even) (even-packer max-rows)]
+            [(packed) (dense-packer max-rows)])
+        ($ map (cut pad-items col-widths <>) $ transpose tr-matrix)))))
