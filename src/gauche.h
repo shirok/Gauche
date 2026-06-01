6393c7f9fb337af18df2b0e1d7fb19db8528bdb8
Shiro Kawai <shiro@acm.org>
2026-06-01 06:44:52 -1000

    Support call-with-composable-continuation

--- src/gauche.h
+++ src/gauche.h
@@ -656,6 +656,7 @@ SCM_EXTERN ScmObj Scm_VMCall(ScmObj *args, int argcnt, void *data);
 SCM_EXTERN ScmObj Scm_VMCallCC(ScmObj proc);
 SCM_EXTERN ScmObj Scm_VMCallCCWithTag(ScmObj proc, ScmObj promptTag);
 SCM_EXTERN ScmObj Scm_VMCallPC(ScmObj proc);
+SCM_EXTERN ScmObj Scm_VMCallPCWithTag(ScmObj proc, ScmObj promptTag);
 SCM_EXTERN ScmObj Scm_VMReset(ScmObj proc);
 SCM_EXTERN void   Scm_VMPushDynamicHandlers(ScmObj, ScmObj, ScmObj);
 SCM_EXTERN ScmObj Scm_VMDynamicWind(ScmObj pre, ScmObj body, ScmObj post);
