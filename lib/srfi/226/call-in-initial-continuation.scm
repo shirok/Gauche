784cace8d244abfb1686c9b242126c02b4ac9977
Shiro Kawai <shiro@acm.org>
2026-06-05 11:03:01 -1000

    Export implemented APIs in srfi.226.*

--- lib/srfi/226/call-in-initial-continuation.scm
+++ lib/srfi/226/call-in-initial-continuation.scm
@@ -3,9 +3,10 @@
 ;;;
 
 (define-module srfi.226.call-in-initial-continuation
+  (use gauche.threads)
   (export &uncaught-exception
           make-uncaught-exception-condition
           uncaught-exception-condition?
           uncaught-exception-condition-reason
-          ;; call-in-initial-continuation
+          call-in-initial-continuation
    ))
