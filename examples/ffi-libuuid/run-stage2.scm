9bf514f0ca5ff4bfdcdd5891fda80be3474de0a2
Shiro Kawai <shiro@acm.org>
2026-06-11 00:01:57 -1000

    Improve ffi-libuuid/configure
    Use "uuid_get_template" for library test fn of libuuid.  "uuid_clear"
    is included in default runtime on macos.

--- examples/ffi-libuuid/run-stage2.scm
+++ examples/ffi-libuuid/run-stage2.scm
@@ -9,9 +9,10 @@
 (cf-init-gauche-extension)
 (cf-check-headers '("uuid/uuid.h"))
 (define *have-libuuid* #f)
-(cf-search-libs "uuid_clear" '("uuid")
-                :if-found (^z  ; can be #f, if uuid_clear is available by default
-                            (set! *have-libuuid* (boolean z))))
+(cf-search-libs "uuid_get_template" '("uuid")
+                :if-found (^z  ; can be #f, if fn is available by default
+                           (cf-msg-notice "cf-search-libs: ~s" z)
+                           (set! *have-libuuid* (boolean z))))
 
 ;; If libuuid isn't available, just skip the test.
 (unless *have-libuuid*
